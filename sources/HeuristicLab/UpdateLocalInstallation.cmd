5d65807fb68b1b7013ec5bde3225fa8519e37bd7
mkofler <>
2008-03-05 10:35:51 +0000

    Fixed small issue with UpdateLocalInstallation batch file (added rmdir/mkdir for plugins directory).
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@43 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab/UpdateLocalInstallation.cmd
+++ sources/HeuristicLab/UpdateLocalInstallation.cmd
@@ -1,5 +1,7 @@
 set target=C:\Program Files\HeuristicLab 3.0
 
+rmdir "%target%\plugins" /s /q 
+mkdir "%target%\plugins"
 copy "HeuristicLab.exe" "%target%"
 copy "HeuristicLab.exe.config" "%target%"
 copy "HeuristicLab.PluginInfrastructure.dll" "%target%"
