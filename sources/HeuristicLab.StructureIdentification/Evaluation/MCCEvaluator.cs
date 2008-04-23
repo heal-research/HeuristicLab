eb1cd48daeb689c4d5db47f9bb7e947dccf0740b
Gabriel Kronberger <gabriel.kronberger@fh-hagenberg.at>
2008-04-23 09:23:28 +0000

    added matthews correlation coefficient evaluator for classification problems (ticket #69)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@169 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ sources/HeuristicLab.StructureIdentification/Evaluation/MCCEvaluator.cs
@@ -0,0 +1,86 @@
+#region License Information
+/* HeuristicLab
+ * Copyright (C) 2002-2008 Heuristic and Evolutionary Algorithms Laboratory (HEAL)
+ *
+ * This file is part of HeuristicLab.
+ *
+ * HeuristicLab is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HeuristicLab is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HeuristicLab. If not, see <http://www.gnu.org/licenses/>.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HeuristicLab.Core;
+using HeuristicLab.Data;
+using HeuristicLab.Operators;
+using HeuristicLab.Functions;
+using HeuristicLab.DataAnalysis;
+
+namespace HeuristicLab.StructureIdentification {
+  public class MCCEvaluator : GPEvaluatorBase {
+    public override string Description {
+      get {
+        return @"TASK";
+      }
+    }
+
+    double limit = 50.0;
+    public MCCEvaluator()
+      : base() {
+    }
+
+    public override double Evaluate(IScope scope, IFunctionTree functionTree, int targetVariable, Dataset dataset) {
+      double[] estimated = new double[dataset.Rows];
+      double[] original = new double[dataset.Rows];
+      double positive = 0;
+      double negative = 0;
+      double targetMean = dataset.GetMean(targetVariable);
+      for(int sample = 0; sample < dataset.Rows; sample++) {
+        double est = functionTree.Evaluate(dataset, sample);
+        double orig = dataset.GetValue(sample, targetVariable);
+        if(double.IsNaN(est) || double.IsInfinity(est)) {
+          est = targetMean + maximumPunishment;
+        } else if(est > targetMean + maximumPunishment) {
+          est = targetMean + maximumPunishment;
+        } else if(est < targetMean - maximumPunishment) {
+          est = targetMean - maximumPunishment;
+        }
+        estimated[sample] = est;
+        original[sample] = orig;
+        if(orig >= limit) positive++;
+        else negative++;
+      }
+      Array.Sort(estimated, original);
+      double best_mcc = 0.0;
+      double tp = 0;
+      double fn = positive;
+      double tn = negative;
+      double fp = 0;
+      for(int i = original.Length-1; i >= 0 ; i--) {
+        if(original[i] >= limit) {
+          tp++; fn--;
+        } else {
+          tn--; fp++;
+        }
+        double mcc = (tp * tn - fp * fn) / Math.Sqrt((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn));
+        if(best_mcc < mcc) {
+          best_mcc = mcc;
+        }
+      }
+      return best_mcc;
+    }
+  }
+}
