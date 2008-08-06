b7fa0edad81a1cde4125e6593d7ff25160968b8d
Gabriel Kronberger <gabriel.kronberger@fh-hagenberg.at>
2008-08-06 14:59:39 +0000

    improved StructIdProblemInjector (user can remove variables which shouldn't be injected)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@461 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.StructureIdentification/StructIdProblemInjector.cs
+++ sources/HeuristicLab.StructureIdentification/StructIdProblemInjector.cs
@@ -37,20 +37,35 @@ namespace HeuristicLab.StructureIdentification {
     public StructIdProblemInjector()
       : base() {
       AddVariableInfo(new VariableInfo("Dataset", "Dataset", typeof(Dataset), VariableKind.New));
+      GetVariableInfo("Dataset").Local = true;
       AddVariable(new Variable("Dataset", new Dataset()));
+
       AddVariableInfo(new VariableInfo("TargetVariable", "TargetVariable", typeof(IntData), VariableKind.New));
+      GetVariableInfo("TargetVariable").Local = true;
       AddVariable(new Variable("TargetVariable", new IntData()));
+
       AddVariableInfo(new VariableInfo("TrainingSamplesStart", "TrainingSamplesStart", typeof(IntData), VariableKind.New));
+      GetVariableInfo("TrainingSamplesStart").Local = true;
       AddVariable(new Variable("TrainingSamplesStart", new IntData()));
+
       AddVariableInfo(new VariableInfo("TrainingSamplesEnd", "TrainingSamplesEnd", typeof(IntData), VariableKind.New));
+      GetVariableInfo("TrainingSamplesEnd").Local = true;
       AddVariable(new Variable("TrainingSamplesEnd", new IntData()));
+
       AddVariableInfo(new VariableInfo("ValidationSamplesStart", "ValidationSamplesStart", typeof(IntData), VariableKind.New));
+      GetVariableInfo("ValidationSamplesStart").Local = true;
       AddVariable(new Variable("ValidationSamplesStart", new IntData()));
+
       AddVariableInfo(new VariableInfo("ValidationSamplesEnd", "ValidationSamplesEnd", typeof(IntData), VariableKind.New));
+      GetVariableInfo("ValidationSamplesEnd").Local = true;
       AddVariable(new Variable("ValidationSamplesEnd", new IntData()));
+
       AddVariableInfo(new VariableInfo("TestSamplesStart", "TestSamplesStart", typeof(IntData), VariableKind.New));
+      GetVariableInfo("TestSamplesStart").Local = true;
       AddVariable(new Variable("TestSamplesStart", new IntData()));
+
       AddVariableInfo(new VariableInfo("TestSamplesEnd", "TestSamplesEnd", typeof(IntData), VariableKind.New));
+      GetVariableInfo("TestSamplesEnd").Local = true;
       AddVariable(new Variable("TestSamplesEnd", new IntData()));
     }
 
@@ -59,14 +74,11 @@ namespace HeuristicLab.StructureIdentification {
     }
 
     public override IOperation Apply(IScope scope) {
-      scope.AddVariable(new Variable(scope.TranslateName("Dataset"), (IItem)GetVariable("Dataset").Value.Clone()));
-      scope.AddVariable(new Variable(scope.TranslateName("TargetVariable"), (IItem)GetVariable("TargetVariable").Value.Clone()));
-      scope.AddVariable(new Variable(scope.TranslateName("TrainingSamplesStart"), (IItem)GetVariable("TrainingSamplesStart").Value.Clone()));
-      scope.AddVariable(new Variable(scope.TranslateName("TrainingSamplesEnd"), (IItem)GetVariable("TrainingSamplesEnd").Value.Clone()));
-      scope.AddVariable(new Variable(scope.TranslateName("ValidationSamplesStart"), (IItem)GetVariable("ValidationSamplesStart").Value.Clone()));
-      scope.AddVariable(new Variable(scope.TranslateName("ValidationSamplesEnd"), (IItem)GetVariable("ValidationSamplesEnd").Value.Clone()));
-      scope.AddVariable(new Variable(scope.TranslateName("TestSamplesStart"), (IItem)GetVariable("TestSamplesStart").Value.Clone()));
-      scope.AddVariable(new Variable(scope.TranslateName("TestSamplesEnd"), (IItem)GetVariable("TestSamplesEnd").Value.Clone()));
+      foreach(VariableInfo info in VariableInfos) {
+        if(info.Local) {
+          scope.AddVariable(new Variable(info.ActualName, (IItem)GetVariable(info.FormalName).Value.Clone()));
+        }
+      }
       return null;
     }
   }
