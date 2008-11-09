d46761d3fed21ab274db23713402beb375936d1e
kgrading <>
2008-11-09 23:44:02 +0000

    implemented a basic logger according to #335
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@729 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Hive.Client.Common/Logging.cs
+++ sources/HeuristicLab.Hive.Client.Common/Logging.cs
@@ -6,13 +6,35 @@ using System.Diagnostics;
 
 namespace HeuristicLab.Hive.Client.Common {
   public class Logging {      
-    public static void Info(String source, String message) {
+    private static Logging instance = null;
+    private EventLog eventLogger = null;
+
+    public static Logging getInstance() {
+      if (instance == null)
+        instance = new Logging();
+      return instance;
     }
 
-    public static void Error(String source, String message) {
+    private Logging() {
+      eventLogger = new EventLog("Hive Client Core");    
     }
 
-    public static void Error(String source, String message, Exception e) {
+    public void Info(String source, String message) {
+      eventLogger.Source = source;
+      eventLogger.WriteEntry(message);      
+      eventLogger.Close();
+    }
+
+    public void Error(String source, String message) {
+      eventLogger.Source = source;
+      eventLogger.WriteEntry(message, EventLogEntryType.Error);
+      eventLogger.Close();
+    }
+
+    public void Error(String source, String message, Exception e) {
+      eventLogger.Source = source;
+      eventLogger.WriteEntry(message +"\n" + e.ToString(), EventLogEntryType.Error);
+      eventLogger.Close();
     }
   }
 }
--- sources/HeuristicLab.Hive.Client.Core/CoreApplication.cs
+++ sources/HeuristicLab.Hive.Client.Core/CoreApplication.cs
@@ -4,19 +4,15 @@ using System.Linq;
 using System.Text;
 using HeuristicLab.PluginInfrastructure;
 using System.Diagnostics;
+using HeuristicLab.Hive.Client.Common;
 
 namespace HeuristicLab.Hive.Client.Core {
   [ClassInfo(Name = "Hive Client Core", Description = "Hive Client Core baseclass", AutoRestart = true)]
   public class CoreApplication: ApplicationBase {
     public override void Run() {
-      //EventLog logger = new EventLog();
-      //logger.Log = "Hive Client Core";
-      //logger.Source = "CoreApplication";
-      //logger.WriteEntry("Program has Started");
-      //logger.Close();
-      
-      this.ToString();
-      Console.WriteLine("awesome");
+      Logging.getInstance().Info(this.Name, "Info Message");
+      Logging.getInstance().Error(this.Name, "Error Message");
+      Logging.getInstance().Error(this.Name, "Exception Message", new Exception("Exception"));
     }
   }
 }
