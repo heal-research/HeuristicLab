c9b3e090a46ab389177fcfbcb54e24cc3773d453
cbahner <>
2008-11-09 20:55:21 +0000

    #320 first persistence mechanism (values)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@727 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Visualization/ChartDataRowsModel.cs
+++ sources/HeuristicLab.Visualization/ChartDataRowsModel.cs
@@ -25,7 +25,7 @@ namespace HeuristicLab.Visualization{
 
     public void AddDataRow(int id){
 
-      double[] vals = new double[]{};
+      double[] vals = new double[10]{4.0, 2.2, 0.2, 4, 18.2333, 1.1, 7, 99.99, 8, 2.6};
 
       ChartDataRowsModelColumn col = new ChartDataRowsModelColumn(id, vals);
 
@@ -34,10 +34,18 @@ namespace HeuristicLab.Visualization{
 
     public void PushData(int dataRowId, double value){
 
-      columns[dataRowId].Values[columns[dataRowId].Values.Length+1] = value;
+      //double[] vals = new double[columns[dataRowId].Values.Length+1];
+      //for (int i = 0; i < columns[dataRowId].Values.Length; i++ ){
+      //  vals[i] = columns[dataRowId].Values[i];
+
+      //}
+        
+      //vals[vals.Length] = value;
+      //columns[dataRowId].Values = vals;
     }
 
     public override IView CreateView() {
+      //return new LineChart(this); when LineChart is implemented
       return new IntDataView(test);
     }
 
@@ -62,9 +70,17 @@ namespace HeuristicLab.Visualization{
       XmlNode node = base.GetXmlNode(name, document, persistedObjects);
 
       foreach (ChartDataRowsModelColumn column in Columns){
-        XmlNode child = document.CreateNode(XmlNodeType.Element, "column", null);
-        child.InnerText = "xx";
-        node.AppendChild(child);
+        XmlNode columnElement = document.CreateNode(XmlNodeType.Element, "column", null);
+
+        XmlAttribute idAttr = document.CreateAttribute("id");
+        idAttr.Value = (column.ColumnId).ToString();
+        columnElement.Attributes.Append(idAttr);
+
+        for (int i = 0; i < column.Values.Length; i++){
+          columnElement.InnerText += column.Values[i].ToString() + ";";
+        }
+          //columnElement.InnerText = "xx";
+          node.AppendChild(columnElement);
       }
       
       //node.InnerText = "test1";
