3dc6295792e15a52e7e2dcd11bc333e7f35e3b3a
kgrading <>
2008-11-11 12:03:41 +0000

    implemented a basic first version of the Timer (#346)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@733 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Hive.Client.Core/CoreApplication.cs
+++ sources/HeuristicLab.Hive.Client.Core/CoreApplication.cs
@@ -5,14 +5,32 @@ using System.Text;
 using HeuristicLab.PluginInfrastructure;
 using System.Diagnostics;
 using HeuristicLab.Hive.Client.Common;
+using System.Timers;
+using System.Threading;
 
 namespace HeuristicLab.Hive.Client.Core {
   [ClassInfo(Name = "Hive Client Core", Description = "Hive Client Core baseclass", AutoRestart = true)]
   public class CoreApplication: ApplicationBase {
     public override void Run() {
+ 
       Logging.getInstance().Info(this.Name, "Info Message");
       Logging.getInstance().Error(this.Name, "Error Message");
       Logging.getInstance().Error(this.Name, "Exception Message", new Exception("Exception"));
+      
+      Heartbeat beat = new Heartbeat();
+      beat.Interval = 1000;
+      beat.StartHeartbeat();
+      DoRubbish();
+      Console.WriteLine("done");
+      Thread.Sleep(99999);
+    }
+
+    private void DoRubbish() {
+      for (int w = 0; w < 20000000; w++)
+        for (int x = 0; x < 20000000; x++)
+          for (int y = 0; y < 20000000; y++) {
+          }
+
     }
   }
 }
---
+++ sources/HeuristicLab.Hive.Client.Core/Heartbeat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace HeuristicLab.Hive.Client.Core {
+  public class Heartbeat {
+    public double Interval { get; set; }
+    
+    private Timer heartbeatTimer = null;
+        
+    public Heartbeat() {
+      Interval = 100;
+    }
+
+    public Heartbeat(double interval) {
+      Interval = interval;      
+    }
+
+    public void StartHeartbeat() {
+      heartbeatTimer = new System.Timers.Timer();
+      heartbeatTimer.Interval = this.Interval;
+      heartbeatTimer.AutoReset = true;
+      heartbeatTimer.Elapsed += new ElapsedEventHandler(heartbeatTimer_Elapsed);
+      heartbeatTimer.Start();            
+    }
+
+    void heartbeatTimer_Elapsed(object sender, ElapsedEventArgs e) {
+      Console.WriteLine("tick");
+    }
+
+  }
+}
