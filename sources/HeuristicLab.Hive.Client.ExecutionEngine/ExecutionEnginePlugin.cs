30cff5c7e45499a09a7eda9dea270c4345448e4d
kgrading <>
2008-11-15 14:13:19 +0000

    changed the plugin name to the correct value (#358)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@762 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Hive.Client.ExecutionEngine/ExecutionEnginePlugin.cs
+++ sources/HeuristicLab.Hive.Client.ExecutionEngine/ExecutionEnginePlugin.cs
@@ -5,10 +5,10 @@ using System.Text;
 using HeuristicLab.PluginInfrastructure;
 
 namespace HeuristicLab.Hive.Client.ExecutionEngine {
-  [ClassInfo(Name = "HeuristicLab.Hive.ExecutionEngine-3.2")]
-  [PluginFile(Filename = "HeuristicLab.Hive.ExecutionEngine-3.2.dll", Filetype = PluginFileType.Assembly)]
-  [Dependency(Dependency = "HeuristicLab.Core-3.2")]
+  [ClassInfo(Name = "HeuristicLab.Hive.Client.ExecutionEngine-3.2")]
+  [PluginFile(Filename = "HeuristicLab.Hive.Client.ExecutionEngine-3.2.dll", Filetype = PluginFileType.Assembly)]
   [Dependency(Dependency = "HeuristicLab.Hive.Client.Core-3.2")]
-  public class ExecutionEnginePlugin {
+  [Dependency(Dependency = "HeuristicLab.Core-3.2")]
+  public class ExecutionEnginePlugin: PluginBase {
   }
 }
