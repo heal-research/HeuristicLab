6b34e7438d7761abd7d7857e5d5691eeb4394352
whackl <>
2008-11-20 14:35:40 +0000

    Added Status class (#374)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@789 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ sources/HeuristicLab.Hive.Client.Common/Status.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HeuristicLab.Hive.Client.Common {
+  public static class Status {
+    public static int CurrentJobs { get; set; }
+    public static int CurrentUsedCores { get; set; }
+    public static DateTime LoginTime { get; set; }
+    public static bool LoggedIn { get; set; }
+
+    static Status() {
+      CurrentJobs = 0;
+      CurrentUsedCores = 0;
+      LoggedIn = false;
+    }
+  }
+}
