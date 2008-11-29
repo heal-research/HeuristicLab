8a6cf6570b03f2f6f1a51d9c02d9ba94986fc4d5
shofstad <>
2008-11-29 12:43:00 +0000

    changes on LegendForm (#407)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@873 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Visualization/LegendShape.cs
+++ sources/HeuristicLab.Visualization/LegendShape.cs
@@ -1,13 +1,27 @@
+using System.Collections.Generic;
 using System.Drawing;
 
 namespace HeuristicLab.Visualization {
+  public class LegendItem {
+    public LegendItem(string label, Color color) {
+      Label = label;
+      Color = color;
+    }
+
+    public string Label { get; set; }
+    public Color Color { get; set; }
+  }
+
+
   public class LegendShape : IShape {
     private readonly Color color;
+
+    private readonly IList<LegendItem> legendItems = new List<LegendItem>();
     private readonly RectangleD rect;
 
     public LegendShape(double x1, double y1, double x2, double y2, double z, Color color) {
       rect = new RectangleD(x1, y1, x2, y2);
-      this.Z = z;
+      Z = z;
       this.color = color;
     }
 
@@ -30,5 +44,17 @@ namespace HeuristicLab.Visualization {
     }
 
     #endregion
+
+    public void AddLegendItem(LegendItem item) {
+      legendItems.Add(item);
+    }
+
+    public void RemoveLegendItem(LegendItem item) {
+      legendItems.Remove(item);
+    }
+
+    public void ClearLegendItems() {
+      legendItems.Clear();
+    }
   }
 }
