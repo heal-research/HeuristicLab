4272042aab8e73faf183c303bf119badfd5f0721
Stefan Vonolfen <stefan.vonolfen@gmail.com>
2008-12-11 14:19:38 +0000

    Added IJobAdapter (#372)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@961 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ sources/HeuristicLab.Hive.Server.Core/InternalInterfaces/DataAccess/IJobAdapter.cs
@@ -0,0 +1,55 @@
+﻿#region License Information
+/* HeuristicLab
+ * Copyright (C) 2002-2008 Heuristic and Evolutionary Algorithms Laboratory (HEAL)
+ *
+ * This file is part of HeuristicLab.
+ *
+ * HeuristicLab is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HeuristicLab is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HeuristicLab. If not, see <http://www.gnu.org/licenses/>.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HeuristicLab.Hive.Contracts.BusinessObjects;
+
+namespace HeuristicLab.Hive.Server.Core.InternalInterfaces.DataAccess {
+  interface IJobAdapter {
+    /// <summary>
+    /// Save or update the job
+    /// </summary>
+    /// <param name="client"></param>
+    void UpdateJob(Job job);
+
+    /// <summary>
+    /// Get the job with the specified ID
+    /// </summary>
+    /// <param name="clientId"></param>
+    /// <returns></returns>
+    ClientInfo GetJobById(long id);
+
+    /// <summary>
+    /// Get all jobs
+    /// </summary>
+    /// <returns></returns>
+    ICollection<Job> GetAllJobs();
+
+    /// <summary>
+    /// Deletes the job
+    /// </summary>
+    /// <param name="client"></param>
+    bool DeleteJob(Job job);
+  }
+}
