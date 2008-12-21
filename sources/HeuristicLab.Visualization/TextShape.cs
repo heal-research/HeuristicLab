14a572dee619aff63e3bcdc6311889b93675e8a4
shofstad <>
2008-12-21 18:51:05 +0000

    Legend implementation finished (#407)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@1046 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ sources/HeuristicLab.Visualization/TextShape.cs
@@ -0,0 +1,61 @@
+﻿using System.Drawing;
+
+namespace HeuristicLab.Visualization {
+  public class TextShape : IShape {
+    private Font font;
+    private Brush brush;
+    private Color color;
+    private string text;
+    private double x;
+    private double y;
+
+    public TextShape(double x, double y, string text) : this(x, y, text, 8) {}
+
+    public TextShape(double x, double y, string text, int fontSize) {
+      this.x = x;
+      this.y = y;
+      this.text = text;
+      font = new Font("Arial", fontSize);
+
+      Color = Color.Blue;
+    }
+
+    public string Text {
+      get { return text; }
+      set { text = value; }
+    }
+
+    public double X {
+      get { return x; }
+      set { x = value; }
+    }
+
+    public double Y {
+      get { return y; }
+      set { y = value; }
+    }
+
+    public Color Color {
+      get { return color; }
+      set {
+        color = value;
+        brush = new SolidBrush(color);
+      }
+    }
+
+    #region IShape Members
+
+    public void Draw(Graphics graphics, Rectangle viewport, RectangleD clippingArea) {
+      int screenX = Transform.ToScreenX(x, viewport, clippingArea);
+      int screenY = Transform.ToScreenY(y, viewport, clippingArea);
+
+      graphics.DrawString(text, font, brush, screenX, screenY);
+    }
+
+    public RectangleD BoundingBox {
+      get { return RectangleD.Empty; }
+    }
+
+    #endregion
+  }
+}
--- sources/HeuristicLab.Visualization/XAxis.cs
+++ sources/HeuristicLab.Visualization/XAxis.cs
@@ -2,61 +2,6 @@ using System.Collections.Generic;
 using System.Drawing;
 
 namespace HeuristicLab.Visualization {
-  // TODO move to own file
-  public class TextShape : IShape {
-    private double x;
-    private double y;
-    private string text;
-
-    private Font font;
-    private Color color;
-    private Brush brush;
-
-    public TextShape(double x, double y, string text) : this(x, y, text, 8) {}
-
-    public TextShape(double x, double y, string text, int fontSize) {
-      this.x = x;
-      this.y = y;
-      this.text = text;
-      this.font = new Font("Arial", fontSize);
-
-      this.Color = Color.Blue;
-    }
-
-    public void Draw(Graphics graphics, Rectangle viewport, RectangleD clippingArea) {
-      int screenX = Transform.ToScreenX(x, viewport, clippingArea);
-      int screenY = Transform.ToScreenY(y, viewport, clippingArea);
-
-      graphics.DrawString(text, font, brush, screenX, screenY);
-    }
-
-    public RectangleD BoundingBox {
-      get { return RectangleD.Empty; }
-    }
-
-    public string Text {
-      get { return text; }
-      set { text = value; }
-    }
-
-    public double X {
-      get { return x; }
-      set { x = value; }
-    }
-
-    public double Y {
-      get { return y; }
-      set { y = value; }
-    }
-
-    public Color Color {
-      get { return color; }
-      set {
-        this.color = value;
-        this.brush = new SolidBrush(color);
-      }
-    }
-  }
 
   public class XAxis : WorldShape {
     private readonly IDictionary<int, TextShape> labels = new Dictionary<int, TextShape>();
