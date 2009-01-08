8ddb66e06eab50888e9fe5d195b48786267befc1
Stefan Vonolfen <stefan.vonolfen@gmail.com>
2009-01-08 16:21:28 +0000

    Implemented DB Settings FW (#372)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@1100 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ sources/HeuristicLab.Hive.Server.ADODataAccess/DBSettings.cs
@@ -0,0 +1,63 @@
+﻿#region License Information
+/* HeuristicLab
+ * Copyright (C) 2002-2008 Heuristic and Evolutionary Algorithms Laboratory (HEAL)
+ *
+ * This file is part of HeuristicLab.
+ *
+ * HeuristicLab is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HeuristicLab is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HeuristicLab. If not, see <http://www.gnu.org/licenses/>.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HeuristicLab.Hive.Server.ADODataAccess.Properties {
+
+  internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+
+    private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
+
+    public static Settings Default {
+      get {
+        return defaultInstance;
+      }
+    }
+
+    private static String pw = "hive";
+    private static String un = "hive";
+    private static String address = "10.22.20.84";
+    private static String catalog = "HiveServer";
+
+    public static String GetConnString(String adress, String catalog, String un, String pw) {
+      return "Data Source=" + adress + ";Initial Catalog=" + catalog + ";MultipleActiveResultSets=True;" +
+        "Persist Security Info=True;User ID=" + un + ";Password=" + pw;
+    }
+
+    [global::System.Configuration.ApplicationScopedSettingAttribute()]
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+    [global::System.Configuration.DefaultSettingValueAttribute(""))]
+    public string HiveServerConnectionString {
+      get {
+        return ((string)(this["HiveServerConnectionString"]));
+      }
+
+      private set {
+        this["HiveServerConnectionString"] = value;
+      }
+    }
+  }
+}
