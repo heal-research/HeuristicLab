fe77ca6fa55386c236ee86b6dacd54a3ea54779f
Stefan Vonolfen <stefan.vonolfen@gmail.com>
2009-01-14 15:22:16 +0000

    updated cached data adapter (#372)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@1123 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Hive.Server.ADODataAccess/ICachedDataAdapter.cs
+++ sources/HeuristicLab.Hive.Server.ADODataAccess/ICachedDataAdapter.cs
@@ -23,9 +23,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.CompilerServices;
 
 namespace HeuristicLab.Hive.Server.ADODataAccess {
   internal interface ICachedDataAdapter {
+    [MethodImpl(MethodImplOptions.Synchronized)]
     void SyncWithDb();
   }
 }
