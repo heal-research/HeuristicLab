bd8da19a261d9569825e346be55e571e474419e3
Stefan Vonolfen <stefan.vonolfen@gmail.com>
2009-01-15 15:02:57 +0000

    Refactored settings (#372)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@1143 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Hive.Server/MainForm.Designer.cs
+++ sources/HeuristicLab.Hive.Server/MainForm.Designer.cs
@@ -41,9 +41,9 @@
           this.label1.AutoSize = true;
           this.label1.Location = new System.Drawing.Point(18, 18);
           this.label1.Name = "label1";
-          this.label1.Size = new System.Drawing.Size(115, 13);
+          this.label1.Size = new System.Drawing.Size(183, 13);
           this.label1.TabIndex = 0;
-          this.label1.Text = "Hive Server running @";
+          this.label1.Text = "Hive Server Client Facade running @";
           // 
           // lblAddress1
           // 
