78e2d2c0f6c81d832d2ee59cd594d962aa0ddbfb
dwagner <>
2009-03-03 19:12:03 +0000

    Added Test for Single Value Lines (#502)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@1243 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Visualization.Test/LineChartTests.cs
+++ sources/HeuristicLab.Visualization.Test/LineChartTests.cs
@@ -112,6 +112,58 @@ namespace HeuristicLab.Visualization.Test {
       f.ShowDialog();
     }
 
+
+    [Test]
+    public void TestSingleValueDataRows() {
+      LineChartTestForm f = new LineChartTestForm(model);
+
+      IDataRow row1 = new DataRow();
+      IDataRow row2 = new DataRow();
+      IDataRow row3 = new DataRow();
+
+      row1.Color = Color.Red;
+      row2.Color = Color.Green;
+      row3.Color = Color.Blue;
+
+      row1.Thickness = 3;
+      row2.Thickness = 4;
+      row3.Thickness = 5;
+
+      row1.Label = "SingleValue";
+      row2.Label = "Gertschi";
+      row3.Label = "Maxi";
+
+      row1.Style = DrawingStyle.Solid;
+      row2.Style = DrawingStyle.Solid;
+      row3.Style = DrawingStyle.Dashed;
+
+      row1.LineType = DataRowType.SingleValue;
+      row2.LineType = DataRowType.SingleValue;
+      row1.AddValue(12);
+
+      model.AddDataRow(row1);
+      model.AddDataRow(row2);
+      model.AddDataRow(row3);
+
+      
+     
+      row2.AddValue(5);
+     
+
+      row3.AddValue(2);
+      row3.AddValue(5);
+      row3.AddValue(9);
+      row3.AddValue(1);
+      row3.AddValue(3);
+
+
+    
+
+      f.ShowDialog();
+    }
+
+
+
     [Test]
     public void TestMainForm() {
       MainForm f = new MainForm();
