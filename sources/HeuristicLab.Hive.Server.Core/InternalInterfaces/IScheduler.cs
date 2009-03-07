21633db3ce4910dac3f67f4799f0f0dfed8ada19
msteinbi <>
2009-03-07 12:07:39 +0000

    Added the 2 missing files (#510) Sorry for that!
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@1284 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ sources/HeuristicLab.Hive.Server.Core/InternalInterfaces/IScheduler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HeuristicLab.Hive.Contracts.BusinessObjects;
+
+namespace HeuristicLab.Hive.Server.Core.InternalInterfaces {
+  public interface IScheduler {
+    bool ExistsJobForClient(HeartBeatData hbData);
+    Job GetNextJobForClient(Guid clientId);
+  }
+}
---
+++ sources/HeuristicLab.Hive.Server.Scheduler/DefaultScheduler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HeuristicLab.Hive.Server.Core.InternalInterfaces;
+
+namespace HeuristicLab.Hive.Server.Scheduler {
+  class DefaultScheduler : IScheduler {
+    #region IScheduler Members
+
+    public bool ExistsJobForClient(HeuristicLab.Hive.Contracts.BusinessObjects.HeartBeatData hbData) {
+      throw new NotImplementedException();
+    }
+
+    public HeuristicLab.Hive.Contracts.BusinessObjects.Job GetNextJobForClient(Guid clientId) {
+      throw new NotImplementedException();
+    }
+
+    #endregion
+  }
+}
