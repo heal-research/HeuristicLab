169c7d242de5f1c0b04e4b0cdd5ccb986687d03f
cbahner <>
2009-03-11 16:46:49 +0000

    #519 tests for aggregators
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@1326 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Visualization.Test/LineChartTests.cs
+++ sources/HeuristicLab.Visualization.Test/LineChartTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Drawing;
+using System.Threading;
 using NUnit.Framework;
 
 namespace HeuristicLab.Visualization.Test {
@@ -96,6 +97,188 @@ namespace HeuristicLab.Visualization.Test {
 
       f.ShowDialog();
     }
+
+    [Test]
+    public void TestAggregator() {
+      LineChartTestForm f = new LineChartTestForm(model);
+
+      IDataRow row1 = new DataRow();
+      row1.Label = "row";
+      row1.Color = Color.Red;
+      row1.Thickness = 3;
+      row1.Style = DrawingStyle.Solid;
+
+      model.AddDataRow(row1);
+
+
+      IAggregator aggregator = new MinAggregator();
+      aggregator.Label = "MinAggregator";
+      aggregator.Color = Color.Pink;
+      aggregator.Thickness = 5;
+      aggregator.Style = DrawingStyle.Solid;
+      aggregator.AddValue(2);
+      aggregator.LineType = DataRowType.SingleValue;
+
+      IDataRow lineTest = new DataRow("testline");
+      lineTest.Color = Color.DarkSalmon;
+      lineTest.Thickness = 2;
+      lineTest.Style = DrawingStyle.Dashed;
+      lineTest.LineType = DataRowType.SingleValue;
+      model.AddDataRow(lineTest);
+      lineTest.AddValue(9);
+      lineTest.AddValue(2);
+      lineTest.AddValue(3);
+      lineTest.AddValue(4);
+
+      aggregator.AddWatch(row1);
+
+      model.AddDataRow(aggregator);
+
+
+      row1.AddValue(10);
+      row1.AddValue(5);
+      row1.AddValue(7);
+      row1.AddValue(3);
+      row1.AddValue(10);
+      row1.AddValue(2);
+
+      f.ShowDialog();
+    }
+
+
+    public class Worker {
+      // This method will be called when the thread is started.
+      private ChartDataRowsModel model;
+      public Worker(ChartDataRowsModel model) {
+        this.model = model;
+      }
+      
+      public void DoWork() {
+
+        IDataRow row1 = new DataRow();
+        row1.Color = Color.Red;
+        row1.Thickness = 2;
+        row1.Label = "Simon";
+        row1.Style = DrawingStyle.Solid;
+        model.AddDataRow(row1);
+
+        IDataRow row2 = new DataRow();
+        row2.Color = Color.Red;
+        row2.Thickness = 3;
+        row2.Label = "Simon";
+        row2.Style = DrawingStyle.Solid;
+        model.AddDataRow(row2);
+
+
+        IAggregator aggregator = new MinAggregator();
+        aggregator.Label = "MinAggregator";
+        aggregator.Color = Color.Pink;
+        aggregator.Thickness = 3;
+        aggregator.Style = DrawingStyle.Solid;
+        aggregator.LineType = DataRowType.SingleValue;
+        aggregator.AddWatch(row1);
+        model.AddDataRow(aggregator);
+
+        IAggregator maxAggregator = new MaxAggregator();
+        maxAggregator.Label = "MaxAggregator";
+        maxAggregator.Color = Color.DeepSkyBlue;
+        maxAggregator.Thickness = 3;
+        maxAggregator.Style = DrawingStyle.Solid;
+        maxAggregator.LineType = DataRowType.SingleValue;
+        maxAggregator.AddWatch(row1);
+        model.AddDataRow(maxAggregator);
+        
+        
+        IAggregator avgAggregator = new AvgAggregator();
+        avgAggregator.Label = "AvgAggregator";
+        avgAggregator.Color = Color.Violet;
+        avgAggregator.Thickness = 3;
+        avgAggregator.Style = DrawingStyle.Solid;
+        avgAggregator.LineType = DataRowType.SingleValue;
+        avgAggregator.AddWatch(row1);
+        model.AddDataRow(avgAggregator);
+
+        IAggregator multiAvgAggregator = new AvgAggregator();
+        multiAvgAggregator.Label = "MultiAvgAggregator";
+        multiAvgAggregator.Color = Color.DarkOliveGreen;
+        multiAvgAggregator.Thickness = 3;
+        multiAvgAggregator.Style = DrawingStyle.Solid;
+        multiAvgAggregator.LineType = DataRowType.SingleValue;
+        multiAvgAggregator.AddWatch(row1);
+        multiAvgAggregator.AddWatch(row2);
+        model.AddDataRow(multiAvgAggregator);
+
+        IAggregator multiMaxAggregator = new MaxAggregator();
+        multiMaxAggregator.Label = "MultiMaxAggregator";
+        multiMaxAggregator.Color = Color.DarkKhaki;
+        multiMaxAggregator.Thickness = 3;
+        multiMaxAggregator.Style = DrawingStyle.Solid;
+        multiMaxAggregator.LineType = DataRowType.SingleValue;
+        multiMaxAggregator.AddWatch(row1);
+        multiMaxAggregator.AddWatch(row2);
+        model.AddDataRow(multiMaxAggregator);
+
+        IAggregator multiMinAggregator = new MinAggregator();
+        multiMinAggregator.Label = "MultiMinAggregator";
+        multiMinAggregator.Color = Color.DarkRed;
+        multiMinAggregator.Thickness = 3;
+        multiMinAggregator.Style = DrawingStyle.Solid;
+        multiMinAggregator.LineType = DataRowType.SingleValue;
+        multiMinAggregator.AddWatch(row1);
+        multiMinAggregator.AddWatch(row2);
+        model.AddDataRow(multiMinAggregator);
+
+        IAggregator multiLineAvgAggregator = new AvgLineAggregator();
+        multiLineAvgAggregator.Label = "MultiLineAvgAggregator";
+        multiLineAvgAggregator.Color = Color.Red;
+        multiLineAvgAggregator.Thickness = 4;
+        multiLineAvgAggregator.Style = DrawingStyle.Solid;
+        multiLineAvgAggregator.LineType = DataRowType.Normal;
+        multiLineAvgAggregator.AddWatch(row1);
+        multiLineAvgAggregator.AddWatch(row2);
+        multiLineAvgAggregator.AddValue(0);
+        model.AddDataRow(multiLineAvgAggregator);
+
+        double i = 0;
+        double newY;
+
+        Random rand = new Random();
+        while (!_shouldStop && i <= 24) {
+          i += 0.2;
+          newY = Math.Sin(i);
+          System.Console.WriteLine("working");
+          //row1.AddValue(rand.NextDouble() * 10);
+          row1.AddValue(newY * 10);
+          row2.AddValue(i*2-15);
+          System.Threading.Thread.Sleep(100);
+        }
+        Console.WriteLine("worker thread: terminating gracefully.");
+      }
+      public void RequestStop() {
+        _shouldStop = true;
+      }
+      // Volatile is used as hint to the compiler that this data
+      // member will be accessed by multiple threads.
+      private volatile bool _shouldStop;
+    }
+
+
+    [Test]
+    public void TestAggregator2() {
+      LineChartTestForm f = new LineChartTestForm(model);
+   
+      // Create the thread object. This does not start the thread.
+      Worker workerObject = new Worker(model);
+      Thread workerThread = new Thread(workerObject.DoWork);
+
+      // Start the worker thread.
+      workerThread.Start();
+
+      f.ShowDialog();
+      workerObject.RequestStop();
+    }
+
+    
 
     [Test]
     public void TestAutoZoomInConstructor() {
