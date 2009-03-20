3a9e21972bd5f43b44dfe95d49895a686d11468a
Stefan Vonolfen <stefan.vonolfen@gmail.com>
2009-03-20 12:43:54 +0000

    Added security interfaces (#532)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@1378 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ sources/HeuristicLab.Security.Contracts/Interfaces/IPermissionManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeuristicLab.Security.Contracts.Interfaces {
+  interface IPermissionManager {
+    Guid Authenticate(String userName, String password);
+    bool CheckPermission(Guid sessionId, Guid permissionToken,
+      Guid enitityId);
+    void EndSession(Guid sessionId);
+  }
+}
---
+++ sources/HeuristicLab.Security.Contracts/Interfaces/ISecurityManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HeuristicLab.Security.Contracts.BusinessObjects;
+
+namespace HeuristicLab.Security.Contracts.Interfaces {
+  interface ISecurityManager {
+    User AddNewUser(User user);
+    bool RemoveUser(long userId);
+    User UpdateUser(User user);
+
+    UserGroup AddNewUserGroup(UserGroup group);
+    bool RemoveUserGroup(long groupId);
+    UserGroup UpdateUserGroup(UserGroup group);
+ 
+    bool AddPermissionOwnerToGroup(long groupId, long permissionOwnerId);
+    bool RemovePermissionOwnerFromGroup(long groupId, long permissionOwnerId);
+
+    bool GrantPermission(long permissionOwnerId, Guid permissionToken);
+    bool RevokePermission(long permissionOwnerId, Guid permissionToken);
+  }
+}
