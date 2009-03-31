d2f5c3c393fbd2f99a9ed237f20b8dc83eadf280
Erik Pitzer <erik.pitzer@fh-hagenberg.at>
2009-03-31 13:39:35 +0000

    Include plug-in dependency. (r1469) (#524)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@1471 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Persistence/HeuristicLabPersistencePlugin.cs
+++ sources/HeuristicLab.Persistence/HeuristicLabPersistencePlugin.cs
@@ -4,6 +4,7 @@ namespace HeuristicLab.Persistence {
 
   [ClassInfo(Name = "HeuristicLab.Persistence-3.3")]
   [PluginFile(Filename = "HeuristicLab.Persistence-3.3.dll", Filetype = PluginFileType.Assembly)]
+  [Dependency(Dependency = "HeuristicLab.Logging-1.0")]
   public class HeuristicLabPersistencePlugin : PluginBase {}
 
 }
