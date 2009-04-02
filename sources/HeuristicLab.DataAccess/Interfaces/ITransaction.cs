3f667cd3442b3fc05fd24764ed4d241fc13c37db
Stefan Vonolfen <stefan.vonolfen@gmail.com>
2009-04-02 13:40:13 +0000

    Removed System.Data dependency from HeuristicLab.DataAccess project(#527)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@1493 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.DataAccess/Interfaces/ITransaction.cs
+++ sources/HeuristicLab.DataAccess/Interfaces/ITransaction.cs
@@ -22,7 +22,6 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Data.Common;
 
 namespace HeuristicLab.DataAccess.Interfaces {
   public interface ITransaction {
