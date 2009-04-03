88c29c4c5ae748ed654af218caaaa88db06b6cc2
mbecirov <>
2009-04-03 11:03:15 +0000

    #528 WcfSettings provides common settings for WCF Services
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@1502 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ sources/HeuristicLab.Hive.Contracts/WcfSettings.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel.Channels;
+using System.ServiceModel;
+using System.Security.Cryptography.X509Certificates;
+using System.Net;
+
+namespace HeuristicLab.Hive.Contracts {
+  public static class WcfSettings {
+
+    public static Binding GetBinding() {
+      NetTcpBinding binding = new NetTcpBinding(SecurityMode.Message);
+      return binding;
+    }
+
+    public static string GetActiveIP() {
+      return System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties().GetActiveTcpConnections()[0].LocalEndPoint.Address.ToString();
+    }
+
+    public static int GetDefaultPort() {
+      return 9000;
+    }
+  }
+
+  // WARNUNG: Dieser Code wird nur für Testzertifikate benötigt, wie sie beispielsweise von makecert erstellt werden. 
+  // Sie sollten diesen Code nicht in einer Produktionsumgebung verwenden.
+  public class PermissiveCertificatePolicy {
+    string subjectName;
+    static PermissiveCertificatePolicy currentPolicy;
+    PermissiveCertificatePolicy(string subjectName) {
+      this.subjectName = subjectName;
+      ServicePointManager.ServerCertificateValidationCallback +=
+          new System.Net.Security.RemoteCertificateValidationCallback(RemoteCertValidate);
+    }
+
+    public static void Enact(string subjectName) {
+      currentPolicy = new PermissiveCertificatePolicy(subjectName);
+    }
+
+    bool RemoteCertValidate(object sender, X509Certificate cert, X509Chain chain, System.Net.Security.SslPolicyErrors error) {
+      if (cert.Subject == subjectName) {
+        return true;
+      }
+
+      return false;
+    }
+  }
+}
