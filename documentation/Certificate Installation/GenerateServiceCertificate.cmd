d7d25e0576827637c6107653c694c81c5b3a2ad2
mbecirov <>
2009-04-16 16:08:11 +0000

    #528 Command for creating service certificates
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@1583 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ documentation/Certificate Installation/GenerateServiceCertificate.cmd
@@ -0,0 +1,19 @@
+@echo off
+REM Use this tool to create a testcertificate on the server:
+REM ========================================================
+REM Options:
+REM -sr	 specifies the Store Location -> must be the same as used in WcfSettings.cs!
+REM -ss  specifies the Store Sub Location -> must be the same as used in WcfSettings.cs!
+REM -n   certificate name, don't change!
+REM -sk  specifies the primary use
+REM ========================================================
+cls
+echo This batch will create and install a new service certificate
+echo for the HIVE Server!
+echo You must have administrator rights in order to access the 
+echo local machine certification store, otherwise you will get
+echo an error message. In that case try to change the store location
+echo to CurrentUser and also change the store location in WcfSettings to CurrentUser.
+pause
+makecert –sr LocalMachine –ss My –n CN=HIVE-Server –sk HTTPS-Key
+pause
