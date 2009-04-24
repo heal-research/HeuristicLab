6a47e47f459e72043b2b11ed705ff468008f551e
Erik Pitzer <erik.pitzer@fh-hagenberg.at>
2009-04-24 13:00:50 +0000

    Separate TypeExtensions into its own file. (#548)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@1658 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ sources/HeuristicLab.Persistence.GUI/3.3/TypeExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HeuristicLab.Persistence.GUI {
+
+  internal static class TypeExtensions {
+
+    public static string SimpleFullName(this Type type) {
+      StringBuilder sb = new StringBuilder();
+      SimpleFullName(type, sb);
+      return sb.ToString();
+    }
+
+    private static void SimpleFullName(Type type, StringBuilder sb) {
+      if (type.IsGenericType) {
+        sb.Append(type.Name, 0, type.Name.LastIndexOf('`'));
+        sb.Append("<");
+        foreach (Type t in type.GetGenericArguments()) {
+          SimpleFullName(t, sb);
+          sb.Append(", ");
+        }
+        sb.Remove(sb.Length - 2, 2);
+        sb.Append(">");
+      } else {
+        sb.Append(type.Name);
+      }
+    }
+
+  }
+  
+}
