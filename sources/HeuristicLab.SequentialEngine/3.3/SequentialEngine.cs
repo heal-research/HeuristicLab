bccbf709f443da8a82b862c5d2e24eab003d08ff
Erik Pitzer <erik.pitzer@fh-hagenberg.at>
2009-04-27 14:31:41 +0000

    Branch HL.SequentialEngine to 3.3 (preparation for new persistence library) (#603)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@1677 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ sources/HeuristicLab.SequentialEngine/3.3/SequentialEngine.cs
@@ -0,0 +1,96 @@
+#region License Information
+/* HeuristicLab
+ * Copyright (C) 2002-2008 Heuristic and Evolutionary Algorithms Laboratory (HEAL)
+ *
+ * This file is part of HeuristicLab.
+ *
+ * HeuristicLab is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HeuristicLab is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HeuristicLab. If not, see <http://www.gnu.org/licenses/>.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using HeuristicLab.Core;
+using System.Threading;
+
+namespace HeuristicLab.SequentialEngine {
+  /// <summary>
+  /// Represents an engine that executes its steps sequentially, also if they could be executed 
+  /// in parallel.
+  /// </summary>
+  public class SequentialEngine : EngineBase, IEditable {
+    private IOperator currentOperator;
+
+    /// <summary>
+    /// Creates a new instance of <see cref="SequentialEngineEditor"/>.
+    /// </summary>
+    /// <returns>The created view as <see cref="SequentialEngineEditor"/>.</returns>
+    public override IView CreateView() {
+      return new SequentialEngineEditor(this);
+    }
+
+    /// <summary>
+    /// Creates a new instance of <see cref="SequentialEngineEditor"/>.
+    /// </summary>
+    /// <returns>The created editor as <see cref="SequentialEngineEditor"/>.</returns>
+    public virtual IEditor CreateEditor() {
+      return new SequentialEngineEditor(this);
+    }
+
+    /// <summary>
+    /// Aborts the engine execution.
+    /// </summary>
+    /// <remarks>Calls <see cref="EngineBase.Abort"/> of base class <see cref="EngineBase"/> and
+    /// <see cref="IOperator.Abort"/> of the current <see cref="IOperator"/>.</remarks>
+    public override void Abort() {
+      base.Abort();
+      if (currentOperator != null)
+        currentOperator.Abort();
+    }
+
+    /// <summary>
+    /// Deals with the next operation, if it is an <see cref="AtomicOperation"/> it is executed,
+    /// if it is a <see cref="CompositeOperation"/> its single operations are pushed on the execution stack.
+    /// </summary>
+    /// <remarks>If an error occurs during the execution the operation is aborted and the operation
+    /// is pushed on the stack again.<br/>
+    /// If the execution was successful <see cref="EngineBase.OnOperationExecuted"/> is called.</remarks>
+    protected override void ProcessNextOperation() {
+      IOperation operation = myExecutionStack.Pop();
+      if (operation is AtomicOperation) {
+        AtomicOperation atomicOperation = (AtomicOperation)operation;
+        IOperation next = null;
+        try {
+          currentOperator = atomicOperation.Operator;
+          next = atomicOperation.Operator.Execute(atomicOperation.Scope);
+        }
+        catch (Exception ex) {
+          // push operation on stack again
+          myExecutionStack.Push(atomicOperation);
+          Abort();
+          ThreadPool.QueueUserWorkItem(delegate(object state) { OnExceptionOccurred(ex);});
+        }
+        if (next != null)
+          myExecutionStack.Push(next);
+        OnOperationExecuted(atomicOperation);
+        if (atomicOperation.Operator.Breakpoint) Abort();
+      } else if (operation is CompositeOperation) {
+        CompositeOperation compositeOperation = (CompositeOperation)operation;
+        for (int i = compositeOperation.Operations.Count - 1; i >= 0; i--)
+          myExecutionStack.Push(compositeOperation.Operations[i]);
+      }
+    }
+  }
+}
---
+++ sources/HeuristicLab.SequentialEngine/3.3/SequentialEngineEditor.cs
@@ -0,0 +1,62 @@
+#region License Information
+/* HeuristicLab
+ * Copyright (C) 2002-2008 Heuristic and Evolutionary Algorithms Laboratory (HEAL)
+ *
+ * This file is part of HeuristicLab.
+ *
+ * HeuristicLab is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HeuristicLab is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HeuristicLab. If not, see <http://www.gnu.org/licenses/>.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using HeuristicLab.Core;
+
+namespace HeuristicLab.SequentialEngine {
+  /// <summary>
+  /// Visual representation of a <see cref="SequentialEngine"/>.
+  /// </summary>
+  public partial class SequentialEngineEditor : EngineBaseEditor {
+    /// <summary>
+    /// Gets or set the engine to represent visually.
+    /// </summary>
+    /// <remarks>Uses property <see cref="EngineBaseEditor.Engine"/> of base class 
+    /// <see cref="EngineBaseEditor"/>. No own data storage present.</remarks>
+    public SequentialEngine SequentialEngine {
+      get { return (SequentialEngine)Engine; }
+      set { base.Engine = value; }
+    }
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="SequentialEngineEditor"/>.
+    /// </summary>
+    public SequentialEngineEditor() {
+      InitializeComponent();
+    }
+    /// <summary>
+    /// Initializes a new instance of <see cref="SequentialEngineEditor"/> with the given
+    /// <paramref name="sequentialEngine"/>.
+    /// </summary>
+    /// <param name="sequentialEngine">The engine to display.</param>
+    public SequentialEngineEditor(SequentialEngine sequentialEngine)
+      : this() {
+      SequentialEngine = sequentialEngine;
+    }
+  }
+}
