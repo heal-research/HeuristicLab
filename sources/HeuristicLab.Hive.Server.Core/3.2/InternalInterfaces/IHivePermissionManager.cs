607275655d588c0fa450ec255c795223b8e755a6
mbecirov <>
2009-05-08 12:15:30 +0000

    #586 Interface for HivePermissionManager
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@1769 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ sources/HeuristicLab.Hive.Server.Core/3.2/InternalInterfaces/IHivePermissionManager.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace HeuristicLab.Hive.Server.Core.InternalInterfaces {
+  public interface IHivePermissionManager {
+    bool CheckPermission(Guid sessionID, Guid actionID, Guid entityId);
+    Guid Login(string username, string password);
+  }
+}
--- sources/HeuristicLab.Hive.Server.Core/3.2/ServerConsoleFacade.cs
+++ sources/HeuristicLab.Hive.Server.Core/3.2/ServerConsoleFacade.cs
@@ -27,6 +27,7 @@ using HeuristicLab.Hive.Contracts.Interfaces;
 using HeuristicLab.Hive.Contracts.BusinessObjects;
 using HeuristicLab.Hive.Contracts;
 using HeuristicLab.Security.Contracts.Interfaces;
+using HeuristicLab.Hive.Server.Core.InternalInterfaces;
 
 namespace HeuristicLab.Hive.Server.Core {
   public class ServerConsoleFacade: IServerConsoleFacade {
@@ -36,138 +37,137 @@ namespace HeuristicLab.Hive.Server.Core {
     private IJobManager jobManager = 
       ServiceLocator.GetJobManager();
 
-    //private IPermissionManager permManager = ServiceLocator.GetPermissionManager();
-
-    #region IServerConsoleFacade Members
+    private IHivePermissionManager secMan = ServiceLocator.GetHivePermissionManager();
 
     public Guid sessionID = Guid.Empty;
 
     public Response Login(string username, string password) {
       Response resp = new Response();
-      /*
-      sessionID = permManager.Authenticate(username, password);
+      sessionID = secMan.Login(username, password);
       if (sessionID == Guid.Empty)
         resp.Success = false;
       else {
-       
         resp.Success = true;
         resp.StatusMessage =
           ApplicationConstants.RESPONSE_SERVERCONSOLE_LOGIN_SUCCESS;
       }
-       */
-      resp.Success = true;
-      resp.StatusMessage =
-        ApplicationConstants.RESPONSE_SERVERCONSOLE_LOGIN_SUCCESS;
       return resp;
     }
 
-    #endregion
-
-    #region IClientManager Members
 
     public ResponseList<ClientInfo> GetAllClients() {
-      if (hasPermission(PermissiveSecurityAction.List_AllClients))
+      if (HasPermission(PermissiveSecurityAction.List_AllClients))
         return clientManager.GetAllClients();
       else
         throw new PermissionException();
     }
 
     public ResponseList<ClientGroup> GetAllClientGroups() {
-      if (hasPermission(PermissiveSecurityAction.List_AllClientGroups))
+      if (HasPermission(PermissiveSecurityAction.List_AllClientGroups))
         return clientManager.GetAllClientGroups();
       else
         throw new PermissionException();
     }
 
     public ResponseList<UpTimeStatistics> GetAllUpTimeStatistics() {
-      if (hasPermission(PermissiveSecurityAction.Show_Statistics))
+      if (HasPermission(PermissiveSecurityAction.Show_Statistics))
         return clientManager.GetAllUpTimeStatistics();
       else
         throw new PermissionException();
     }
 
     public Response AddClientGroup(ClientGroup clientGroup) {
-      if (hasPermission(PermissiveSecurityAction.Add_ClientGroup))
+      if (HasPermission(PermissiveSecurityAction.Add_ClientGroup))
         return clientManager.AddClientGroup(clientGroup);
       else
         throw new PermissionException();
     }
 
     public Response AddResourceToGroup(Guid clientGroupId, Resource resource) {
-      if (hasPermission(PermissiveSecurityAction.Add_Resource))
+      if (HasPermission(PermissiveSecurityAction.Add_Resource))
         return clientManager.AddResourceToGroup(clientGroupId, resource);
       else
         throw new PermissionException();
     }
 
     public Response DeleteResourceFromGroup(Guid clientGroupId, Guid resourceId) {
-      if (hasPermission(PermissiveSecurityAction.Delete_Resource))
+      if (HasPermission(PermissiveSecurityAction.Delete_Resource))
         return clientManager.DeleteResourceFromGroup(clientGroupId, resourceId);
       else
         throw new PermissionException();
     }
 
-    #endregion
-
-    #region IJobManager Members
 
     public ResponseList<HeuristicLab.Hive.Contracts.BusinessObjects.Job> GetAllJobs() {
-      if (hasPermission(PermissiveSecurityAction.Get_AllJobs))
+      if (HasPermission(PermissiveSecurityAction.Get_AllJobs))
         return jobManager.GetAllJobs();
       else
         throw new PermissionException();
     }
 
     public ResponseObject<Job> AddNewJob(Job job) {
-      if (hasPermission(PermissiveSecurityAction.Add_Job))
+      if (hasPermission(PermissiveSecurityAction.Add_Job, job.Client.Id))
         return jobManager.AddNewJob(job);
       else
         throw new PermissionException();
     }
 
     public ResponseObject<JobResult> GetLastJobResultOf(Guid jobId, bool requested) {
-      if (hasPermission(PermissiveSecurityAction.Get_LastJobResult))
+      if (HasPermission(PermissiveSecurityAction.Get_LastJobResult))
         return jobManager.GetLastJobResultOf(jobId, requested);
       else
         throw new PermissionException();
     }
 
     public ResponseObject<List<JobResult>> GetAllJobResults(Guid jobId) {
-      if (hasPermission(PermissiveSecurityAction.Get_AllJobResults))
+      if (HasPermission(PermissiveSecurityAction.Get_AllJobResults))
         return jobManager.GetAllJobResults(jobId);
       else
         throw new PermissionException();
     }
 
     public Response RemoveJob(Guid jobId) {
-      if (hasPermission(PermissiveSecurityAction.Remove_Job))
+      if (HasPermission(PermissiveSecurityAction.Remove_Job))
         return jobManager.RemoveJob(jobId);
       else
         throw new PermissionException();
     }
 
     public Response RequestSnapshot(Guid jobId) {
-      if (hasPermission(PermissiveSecurityAction.Request_Snapshot))
+      if (HasPermission(PermissiveSecurityAction.Request_Snapshot))
         return jobManager.RequestSnapshot(jobId);
       else
         throw new PermissionException();
     }
 
     public Response AbortJob(Guid jobId) {
-      if (hasPermission(PermissiveSecurityAction.Abort_Job))
+      if (HasPermission(PermissiveSecurityAction.Abort_Job))
         return jobManager.AbortJob(jobId);
       else
         throw new PermissionException();
     }
 
-    private bool hasPermission(Guid action) {
-      return true;
-      /*
-      if (sessionID == Guid.Empty)
-        throw new Exception("sessionID is not set! Please check if user is successfully logged on!");
-      return permManager.CheckPermission(sessionID, action, Guid.Empty);
-       */
+    public ResponseObject<List<ClientGroup>> GetAllGroupsOfResource(Guid resourceId) {
+      if (HasPermission(PermissiveSecurityAction.Get_AllGroupsOfResource))
+        return clientManager.GetAllGroupsOfResource(resourceId);
+      else
+        throw new PermissionException();      
     }
+
+  /*
+    private bool HasPermission(Guid action) {
+      return (sessionID == Guid.Empty) ? false : secMan.CheckPermission(sessionID, action, Guid.Empty);
+    }
+
+    private bool HasPermission(Guid action, Guid entityId) {
+      return (sessionID == Guid.Empty) ? false : secMan.CheckPermission(sessionID, action, entityId);
+    }
+   */
+
+    [Obsolete("Only for testing!")]
+    private bool HasPermission(Guid g) { return true; }
+    [Obsolete("Only for testing!")]
+    private bool hasPermission(Guid g, Guid f) { return true; }
 
     public class PermissionException : Exception {
       public PermissionException()
@@ -181,10 +181,6 @@ namespace HeuristicLab.Hive.Server.Core {
 
     }
 
-    public ResponseObject<List<ClientGroup>> GetAllGroupsOfResource(Guid resourceId) {
-      return clientManager.GetAllGroupsOfResource(resourceId);
-    }
 
-    #endregion
   }
 }
