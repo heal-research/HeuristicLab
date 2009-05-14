538bc18a0ebffe16c685d553520ae85120bd959f
Michael Kommenda <michael.kommenda@fh-ooe.at>
2009-05-14 13:22:38 +0000

    SupportVectorEvaluator and SVMHelper added (ticket #619)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@1808 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ sources/HeuristicLab.SupportVectorMachines/3.2/SVMHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HeuristicLab.Core;
+using HeuristicLab.Data;
+using HeuristicLab.DataAnalysis;
+
+namespace HeuristicLab.SupportVectorMachines {
+  public class SVMHelper {
+    public static SVM.Problem CreateSVMProblem(Dataset dataset, ItemList<IntData> allowedFeatures, int targetVariable, int start, int end) {
+      int rowCount = end - start;
+      double[] samples = dataset.Samples;
+      
+      SVM.Node[][] nodes = new SVM.Node[rowCount][];
+      List<SVM.Node> tempRow;
+      double value;
+      for (int row = 0; row < rowCount; row++) {
+        tempRow = new List<SVM.Node>();
+        for (int col = 0; col < allowedFeatures.Count; col++) {
+          value = samples[(start + row) * dataset.Columns + allowedFeatures[col].Data];
+          if(!double.IsNaN(value))
+            tempRow.Add(new SVM.Node(allowedFeatures[col].Data, value));
+        }
+        nodes[row] = tempRow.ToArray();
+      }
+
+      double[] targetVector = new double[rowCount];
+      for (int i = 0; i < rowCount; i++)
+        targetVector[i] = samples[(start + i) * dataset.Columns + targetVariable];
+
+      return new SVM.Problem(rowCount, targetVector, nodes, allowedFeatures.Max(x => x.Data));
+    }
+  }
+}
---
+++ sources/HeuristicLab.SupportVectorMachines/3.2/SupportVectorEvaluator.cs
@@ -0,0 +1,76 @@
+﻿#region License Information
+/* HeuristicLab
+ * Copyright (C) 2002-2009 Heuristic and Evolutionary Algorithms Laboratory (HEAL)
+ *
+ * This file is part of HeuristicLab.
+ *
+ * HeuristicLab is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HeuristicLab is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HeuristicLab. If not, see <http://www.gnu.org/licenses/>.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HeuristicLab.Core;
+using HeuristicLab.Data;
+using HeuristicLab.DataAnalysis;
+
+namespace HeuristicLab.SupportVectorMachines {
+  public class SupportVectorEvaluator : OperatorBase {
+
+    public SupportVectorEvaluator()
+      : base() {
+      //Dataset infos
+      AddVariableInfo(new VariableInfo("Dataset", "Dataset with all samples on which to apply the function", typeof(Dataset), VariableKind.In));
+      AddVariableInfo(new VariableInfo("AllowedFeatures", "List of indexes of allowed features", typeof(ItemList<IntData>), VariableKind.In));
+      AddVariableInfo(new VariableInfo("TargetVariable", "Index of the column of the dataset that holds the target variable", typeof(IntData), VariableKind.In));
+      AddVariableInfo(new VariableInfo("SamplesStart", "Start index of samples in dataset to evaluate", typeof(IntData), VariableKind.In));
+      AddVariableInfo(new VariableInfo("SamplesEnd", "End index of samples in dataset to evaluate", typeof(IntData), VariableKind.In));
+
+      AddVariableInfo(new VariableInfo("SVMModel", "Represent the model learned by the SVM", typeof(ObjectData), VariableKind.In));
+      AddVariableInfo(new VariableInfo("SVMRangeTransform", "The applied transformation during the learning the model", typeof(ObjectData), VariableKind.In));
+
+      AddVariableInfo(new VariableInfo("Values", "Target vs predicted values", typeof(ItemList), VariableKind.New | VariableKind.Out));
+
+    }
+
+
+    public override IOperation Apply(IScope scope) {
+      Dataset dataset = GetVariableValue<Dataset>("Dataset", scope, true);
+      ItemList<IntData> allowedFeatures = GetVariableValue<ItemList<IntData>>("AllowedFeatures", scope, true);
+      int targetVariable = GetVariableValue<IntData>("TargetVariable", scope, true).Data;
+      int start = GetVariableValue<IntData>("SamplesStart", scope, true).Data;
+      int end = GetVariableValue<IntData>("SamplesEnd", scope, true).Data;
+
+      SVM.Model model = (SVM.Model)GetVariableValue<ObjectData>("SVMModel", scope, true).Data;
+      SVM.RangeTransform rangeTransform = (SVM.RangeTransform)GetVariableValue<ObjectData>("SVMRangeTransform", scope, true).Data;
+
+      SVM.Problem problem = SVMHelper.CreateSVMProblem(dataset, allowedFeatures, targetVariable, start, end);
+      SVM.Problem scaledProblem = SVM.Scaling.Scale(problem, rangeTransform);
+
+      ItemList predictedValues = new ItemList();
+      ItemList row;
+      for (int i = 0; i < end - start; i++) {
+        row = new ItemList();
+        row.Add(new DoubleData(SVM.Prediction.Predict(model, scaledProblem.X[i])));
+        row.Add(new DoubleData(dataset.Samples[(start + i) * dataset.Columns + targetVariable]));
+        predictedValues.Add(row);
+      }
+
+      scope.AddVariable(new HeuristicLab.Core.Variable(scope.TranslateName("Values"), predictedValues));
+      return null;
+    }
+  }
+}
