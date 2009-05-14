e359e9255e7a42b6defdb3a9318bc5bc32b0b0fb
Michael Kommenda <michael.kommenda@fh-ooe.at>
2009-05-14 15:09:56 +0000

    added SimpleR2Evaluator for SVMs + test engine (ticket #619)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@1814 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.SupportVectorMachines/3.2/SimpleR2Evaluator.cs
+++ sources/HeuristicLab.SupportVectorMachines/3.2/SimpleR2Evaluator.cs
@@ -7,31 +7,47 @@ using HeuristicLab.Data;
 using HeuristicLab.DataAnalysis;
 
 namespace HeuristicLab.SupportVectorMachines {
-  public class SimpleMSEEvaluator : OperatorBase{
+  public class SimpleR2Evaluator : OperatorBase{
 
-    public SimpleMSEEvaluator()
+    public SimpleR2Evaluator()
       : base() {
       AddVariableInfo(new VariableInfo("Values", "Target vs predicted values", typeof(ItemList), VariableKind.In));
-      AddVariableInfo(new VariableInfo("MSE", "Mean squarred error", typeof(DoubleData), VariableKind.New | VariableKind.Out));
+      AddVariableInfo(new VariableInfo("R2", "Coefficient of determination", typeof(DoubleData), VariableKind.New | VariableKind.Out));
     }
 
     public override IOperation Apply(IScope scope) {
       ItemList values = GetVariableValue<ItemList>("Values", scope, true);
+
+      double targetMean = 0;
       double sse = 0;
-      double cnt = 0;      
+      double cnt = 0;
       foreach (ItemList row in values) {
         double estimated = ((DoubleData)row[0]).Data;
         double target = ((DoubleData)row[1]).Data;
         if (!double.IsNaN(estimated) && !double.IsInfinity(estimated) &&
             !double.IsNaN(target) && !double.IsInfinity(target)) {
+          targetMean += target;
           double error = estimated - target;
           sse += error * error;
           cnt++;
         }
       }
+      targetMean /= cnt;
 
-      double mse = sse / cnt;
-      scope.AddVariable(new Variable(scope.TranslateName("MSE"),new DoubleData(mse)));
+      double targetDeviationTotalSumOfSquares = 0;
+      foreach (ItemList row in values) {
+        double target = ((DoubleData)row[1]).Data;
+        if (!double.IsNaN(target) && !double.IsInfinity(target)) {
+          target = target - targetMean;
+          target = target * target;
+          targetDeviationTotalSumOfSquares += target;
+        }
+      }
+      double quality = 1 - sse / targetDeviationTotalSumOfSquares;
+      if (quality > 1)
+        throw new InvalidProgramException();
+
+      scope.AddVariable(new HeuristicLab.Core.Variable(scope.TranslateName("R2"), new DoubleData(quality)));
       return null;
     }
   }
