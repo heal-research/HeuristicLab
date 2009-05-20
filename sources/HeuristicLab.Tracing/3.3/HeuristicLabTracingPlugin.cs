83d7a27688b283945c322273170e73bffd664609
Erik Pitzer <erik.pitzer@fh-hagenberg.at>
2009-05-20 10:02:41 +0000

    Create branch for Tracing-3.3 (#603)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@1860 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ sources/HeuristicLab.Tracing/3.3/HeuristicLabTracingPlugin.cs
@@ -0,0 +1,12 @@
+﻿using HeuristicLab.PluginInfrastructure;
+
+namespace HeuristicLab.Tracing {
+
+  [ClassInfo(Name = "HeuristicLab.Tracing-3.2")]
+  [PluginFile(Filename = "HeuristicLab.Tracing-3.2.dll", Filetype = PluginFileType.Assembly)]
+  [PluginFile(Filename = "log4net.dll", Filetype = PluginFileType.Assembly)]
+  [PluginFile(Filename = "log4net licence.txt", Filetype = PluginFileType.License)]
+  [PluginFile(Filename = "HeuristicLab.log4net.xml", Filetype = PluginFileType.Data)]
+  public class HeuristicLabPersistencePlugin : PluginBase {}
+
+}
---
+++ sources/HeuristicLab.Tracing/3.3/Logger.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using HeuristicLab.Tracing.Properties;
+using log4net;
+using System.Diagnostics;
+using log4net.Config;
+using System.IO;
+
+namespace HeuristicLab.Tracing {
+
+  public class Logger {
+
+    private static bool IsConfigured = false;
+
+    private static void Configure() {
+      if ( IsConfigured ) return;
+      IsConfigured = true;
+      if (string.IsNullOrEmpty(Settings.Default.TracingLog4netConfigFile)) {
+        Settings.Default.TracingLog4netConfigFile = 
+          Path.Combine(
+            PluginInfrastructure.Properties.Settings.Default.PluginDir,
+            "HeuristicLab.log4net.xml");
+      }
+      XmlConfigurator.ConfigureAndWatch(
+        new FileInfo(Settings.Default.TracingLog4netConfigFile));
+      Info("logging initialized " + DateTime.Now);
+    }
+
+    public static ILog GetDefaultLogger(int nParents) {
+      Configure();
+      StackFrame frame = new StackFrame(nParents + 1);
+      return LogManager.GetLogger(frame.GetMethod().DeclaringType);      
+    }
+
+    public static ILog GetDefaultLogger() {
+      Configure();
+      StackFrame frame = new StackFrame(1);
+      return LogManager.GetLogger(frame.GetMethod().DeclaringType);
+    }
+
+    public static void Debug(object message) {
+      GetDefaultLogger(1).Debug(message);
+    }
+
+    public static void Info(object message) {
+      GetDefaultLogger(1).Info(message);
+    }
+
+    public static void Warn(object message) {
+      GetDefaultLogger(1).Warn(message);
+    }
+
+    public static void Error(object message) {      
+      GetDefaultLogger(1).Error(message);
+    }
+
+    public static void Fatal(object message) {
+      GetDefaultLogger(1).Fatal(message);
+    }
+
+    public static void Debug(Type type, object message) {
+      Configure();
+      LogManager.GetLogger(type).Debug(message);
+    }
+
+    public static void Info(Type type, object message) {
+      Configure();
+      LogManager.GetLogger(type).Info(message);      
+    }
+
+    public static void Warn(Type type, object message) {
+      Configure();
+      LogManager.GetLogger(type).Warn(message);      
+    }
+
+    public static void Error(Type type, object message) {
+      Configure();
+      LogManager.GetLogger(type).Error(message);
+    }
+
+    public static void Fatal(Type type, object message) {
+      Configure();
+      LogManager.GetLogger(type).Fatal(message);
+    }
+
+    public static void Debug(object message, Exception exception) {
+      GetDefaultLogger(1).Debug(message, exception);
+    }
+
+    public static void Info(object message, Exception exception) {
+      GetDefaultLogger(1).Info(message, exception);
+    }
+
+    public static void Warn(object message, Exception exception) {
+      GetDefaultLogger(1).Warn(message, exception);
+    }
+
+    public static void Error(object message, Exception exception) {
+      GetDefaultLogger(1).Error(message, exception);
+    }
+
+    public static void Fatal(object message, Exception exception) {
+      GetDefaultLogger(1).Fatal(message, exception);
+    }
+
+    public static void Debug(Type type, object message, Exception exception) {
+      Configure();
+      LogManager.GetLogger(type).Debug(message, exception);
+    }
+
+    public static void Info(Type type, object message, Exception exception) {
+      Configure();
+      LogManager.GetLogger(type).Info(message, exception);
+    }
+
+    public static void Warn(Type type, object message, Exception exception) {
+      Configure();
+      LogManager.GetLogger(type).Warn(message, exception);
+    }
+
+    public static void Error(Type type, object message, Exception exception) {
+      Configure();
+      LogManager.GetLogger(type).Error(message, exception);
+    }
+
+    public static void Fatal(Type type, object message, Exception exception) {
+      Configure();
+      LogManager.GetLogger(type).Fatal(message, exception);
+    }
+
+  }
+}
