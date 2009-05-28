65189d91955314280d442066ae946be1042216d7
kgrading <>
2009-05-28 15:24:12 +0000

    fixed custom logging in #622
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@1935 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Tracing/3.2/HiveLogger.cs
+++ sources/HeuristicLab.Tracing/3.2/HiveLogger.cs
@@ -1,14 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using HeuristicLab.Tracing.Properties;
-using System.IO;
+using log4net;
+using System.Diagnostics;
 using log4net.Config;
+using System.IO;
 
 namespace HeuristicLab.Tracing {
-  public class HiveLogger: Logger {
-    protected new static void Configure() {
+  public class HiveLogger {
+    protected static bool IsConfigured = false;
+    protected static void Configure() {
       if (IsConfigured) return;
       IsConfigured = true;
       if (string.IsNullOrEmpty(Settings.Default.TracingLog4netConfigFile)) {
@@ -21,5 +21,108 @@ namespace HeuristicLab.Tracing {
         new FileInfo(Settings.Default.TracingLog4netConfigFile));
       Info("Hive Logging initialized " + DateTime.Now);
     }
+
+    public static ILog GetDefaultLogger(int nParents) {
+      Configure();
+      StackFrame frame = new StackFrame(nParents + 1);
+      return LogManager.GetLogger(frame.GetMethod().DeclaringType);      
+    }
+
+    public static ILog GetDefaultLogger() {
+      Configure();
+      StackFrame frame = new StackFrame(1);
+      return LogManager.GetLogger(frame.GetMethod().DeclaringType);
+    }
+
+    public static void Debug(object message) {
+      GetDefaultLogger(1).Debug(message);
+    }
+
+    public static void Info(object message) {
+      GetDefaultLogger(1).Info(message);
+    }
+
+    public static void Warn(object message) {
+      GetDefaultLogger(1).Warn(message);
+    }
+
+    public static void Error(object message) {      
+      GetDefaultLogger(1).Error(message);
+    }
+
+    public static void Fatal(object message) {
+      GetDefaultLogger(1).Fatal(message);
+    }
+
+    public static void Debug(Type type, object message) {
+      Configure();
+      LogManager.GetLogger(type).Debug(message);
+    }
+
+    public static void Info(Type type, object message) {
+      Configure();
+      LogManager.GetLogger(type).Info(message);      
+    }
+
+    public static void Warn(Type type, object message) {
+      Configure();
+      LogManager.GetLogger(type).Warn(message);      
+    }
+
+    public static void Error(Type type, object message) {
+      Configure();
+      LogManager.GetLogger(type).Error(message);
+    }
+
+    public static void Fatal(Type type, object message) {
+      Configure();
+      LogManager.GetLogger(type).Fatal(message);
+    }
+
+    public static void Debug(object message, Exception exception) {
+      GetDefaultLogger(1).Debug(message, exception);
+    }
+
+    public static void Info(object message, Exception exception) {
+      GetDefaultLogger(1).Info(message, exception);
+    }
+
+    public static void Warn(object message, Exception exception) {
+      GetDefaultLogger(1).Warn(message, exception);
+    }
+
+    public static void Error(object message, Exception exception) {
+      GetDefaultLogger(1).Error(message, exception);
+    }
+
+    public static void Fatal(object message, Exception exception) {
+      GetDefaultLogger(1).Fatal(message, exception);
+    }
+
+    public static void Debug(Type type, object message, Exception exception) {
+      Configure();
+      LogManager.GetLogger(type).Debug(message, exception);
+    }
+
+    public static void Info(Type type, object message, Exception exception) {
+      Configure();
+      LogManager.GetLogger(type).Info(message, exception);
+    }
+
+    public static void Warn(Type type, object message, Exception exception) {
+      Configure();
+      LogManager.GetLogger(type).Warn(message, exception);
+    }
+
+    public static void Error(Type type, object message, Exception exception) {
+      Configure();
+      LogManager.GetLogger(type).Error(message, exception);
+    }
+
+    public static void Fatal(Type type, object message, Exception exception) {
+      Configure();
+      LogManager.GetLogger(type).Fatal(message, exception);
+    }
+
   }
 }
