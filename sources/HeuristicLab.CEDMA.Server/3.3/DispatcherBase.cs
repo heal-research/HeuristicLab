heal-research/HeuristicLab

commit de596aa155fcf6e22f45af81c99862514e64b93e

Refactoring: changed CEDMA backend to use a single data set per RDF results database.  #656 (CEDMA server should handle only one data set (problem) at a time)
git-svn-id: https://src.heuristiclab.com/svn/core/trunk@2000 2abd9481-f8db-48e9-bd25-06bc13291c1b

sources/HeuristicLab.CEDMA.Server/3.3/DispatcherBase.cs
@@ -64,7 +64,6 @@ namespace HeuristicLab.CEDMA.Server {
       IAlgorithm selectedAlgorithm = SelectAlgorithm(dataSetEntity, targetVariable, dataSet.Problem.LearningTask);
       string targetVariableName = dataSet.Problem.GetVariableName(targetVariable);
 
-
       if (selectedAlgorithm != null) {
         SetProblemParameters(selectedAlgorithm, dataSet.Problem, targetVariable);
       }
@@ -76,7 +75,7 @@ namespace HeuristicLab.CEDMA.Server {
     public abstract IAlgorithm SelectAlgorithm(Entity dataSet, int targetVariable, LearningTask learningTask);
 
     private void SetProblemParameters(IAlgorithm algo, Problem problem, int targetVariable) {
-      algo.Dataset = problem.DataSet;
+      algo.Dataset = problem.Dataset;
       algo.TargetVariable = targetVariable;
       algo.ProblemInjector.GetVariable("TrainingSamplesStart").GetValue<IntData>().Data = problem.TrainingSamplesStart;
       algo.ProblemInjector.GetVariable("TrainingSamplesEnd").GetValue<IntData>().Data = problem.TrainingSamplesEnd;
@@ -93,7 +92,7 @@ namespace HeuristicLab.CEDMA.Server {
         algo.ProblemInjector.GetVariable("MaxTimeOffset").GetValue<IntData>().Data = problem.MaxTimeOffset;
       } else if (problem.LearningTask == LearningTask.Classification) {
         ItemList<DoubleData> classValues = algo.ProblemInjector.GetVariable("TargetClassValues").GetValue<ItemList<DoubleData>>();
-        foreach (double classValue in GetDifferentClassValues(problem.DataSet, targetVariable)) classValues.Add(new DoubleData(classValue));
+        foreach (double classValue in GetDifferentClassValues(problem.Dataset, targetVariable)) classValues.Add(new DoubleData(classValue));
       }
     }
 

sources/HeuristicLab.CEDMA.Core/3.3/Console.cs
@@ -36,14 +36,14 @@ namespace HeuristicLab.CEDMA.Core {
       get { return serverUri; }
     }
 
-    private DataSetList dataSetList;
-    public DataSetList DataSetList {
-      get { return dataSetList; }
+    private DataSet dataSet;
+    public DataSet DataSet {
+      get { return dataSet; }
     }
 
     public Console()
       : base() {
-      dataSetList = new DataSetList();
+      dataSet = new DataSet();
     }
 
     public IEditor CreateEditor() {
@@ -70,7 +70,13 @@ namespace HeuristicLab.CEDMA.Core {
     #endregion
 
     internal void Connect(string serverUri) {
-      DataSetList.Store = new StoreProxy(serverUri);
+      IStore store = new StoreProxy(serverUri);
+      var variableBindings = store.Query("?Dataset <" + Ontology.PredicateInstanceOf + "> <" + Ontology.TypeDataSet + "> .", 0, 10).ToArray();
+      if (variableBindings.Length > 0) {
+        dataSet = new DataSet(store, (Entity)variableBindings[0].Get("Dataset"));
+      } else {
+        dataSet.Store = store;
+      }
     }
   }
 }

sources/HeuristicLab.CEDMA.Core/3.3/ConsoleEditor.cs
@@ -41,7 +41,6 @@ namespace HeuristicLab.CEDMA.Core {
     public ConsoleEditor(Console console) {
       InitializeComponent();
       this.console = console;
-      serverPanel.Enabled = false;
     }
 
     #region autogenerated code
@@ -110,9 +109,9 @@ namespace HeuristicLab.CEDMA.Core {
         connectButton.Enabled = false;
         serverPanel.Enabled = true;
         serverPanel.Controls.Clear();
-        Control dataSetListView = (Control)console.DataSetList.CreateView();
-        dataSetListView.Dock = DockStyle.Fill;
-        serverPanel.Controls.Add(dataSetListView);
+        Control dataSetView = (Control)console.DataSet.CreateView();
+        dataSetView.Dock = DockStyle.Fill;
+        serverPanel.Controls.Add(dataSetView);
       } catch(CommunicationException ex) {
         MessageBox.Show("Exception while trying to connect to " + uriTextBox.Text + "\n" + ex.Message);
       }

sources/HeuristicLab.CEDMA.Core/3.3/DataSet.cs
@@ -60,10 +60,16 @@ namespace HeuristicLab.CEDMA.Core {
       }
     }
 
+    private bool activated;
+    public bool Activated {
+      get { return activated; }
+    }
+
     public DataSet()
       : base() {
       guid = Guid.NewGuid();
       name = "Data set";
+      activated = false;
     }
 
     public DataSet(IStore store, Entity dataSetEntity)
@@ -71,6 +77,7 @@ namespace HeuristicLab.CEDMA.Core {
       Store = store;
       guid = new Guid(dataSetEntity.Uri.Remove(0, Ontology.CedmaNameSpace.Length));
       name = guid.ToString();
+      activated = true;
     }
 
     public void Activate() {
@@ -78,6 +85,7 @@ namespace HeuristicLab.CEDMA.Core {
       Store.Add(new Statement(myEntity, Ontology.PredicateInstanceOf, Ontology.TypeDataSet));
       Store.Add(new Statement(myEntity, Ontology.PredicateSerializedData, new Literal(Convert.ToBase64String(PersistenceManager.SaveToGZip(problem)))));
       Store.Add(new Statement(myEntity, Ontology.PredicateName, new Literal(name)));
+      activated = true;
     }
 
     public IView CreateView() {
@@ -86,7 +94,6 @@ namespace HeuristicLab.CEDMA.Core {
 
     internal Results GetResults() {
       Results results = new Results(Store);
-      results.FilterDataSet(new Entity(Ontology.CedmaNameSpace + Guid));
       return results;
     }
   }

sources/HeuristicLab.CEDMA.Core/3.3/DataSetView.cs
@@ -57,6 +57,19 @@ namespace HeuristicLab.CEDMA.Core {
       editorGroupBox.Controls.Add(problemControl);
       PopulateViewComboBox();
       resultsButton.Enabled = viewComboBox.SelectedItem != null;
+      if (dataSet.Activated) {
+        activateButton.Enabled = false;
+        editorGroupBox.Enabled = false;
+        viewComboBox.Enabled = true;
+        resultsButton.Enabled = true;
+        progressBar.Enabled = true;
+      } else {
+        activateButton.Enabled = true;
+        editorGroupBox.Enabled = true;
+        viewComboBox.Enabled = false;
+        resultsButton.Enabled = false;
+        progressBar.Enabled = false;
+      }
     }
 
     private void PopulateViewComboBox() {

sources/HeuristicLab.CEDMA.Core/3.3/HeuristicLab.CEDMA.Core-3.3.csproj
@@ -102,13 +102,6 @@
     </Compile>
     <Compile Include="Problem.cs" />
     <Compile Include="DataSet.cs" />
-    <Compile Include="DataSetList.cs" />
-    <Compile Include="DataSetListView.cs">
-      <SubType>UserControl</SubType>
-    </Compile>
-    <Compile Include="DataSetListView.Designer.cs">
-      <DependentUpon>DataSetListView.cs</DependentUpon>
-    </Compile>
     <Compile Include="DataSetView.cs">
       <SubType>UserControl</SubType>
     </Compile>
@@ -161,9 +154,6 @@
     <EmbeddedResource Include="ConsoleEditor.resx">
       <DependentUpon>ConsoleEditor.cs</DependentUpon>
       <SubType>Designer</SubType>
-    </EmbeddedResource>
-    <EmbeddedResource Include="DataSetListView.resx">
-      <DependentUpon>DataSetListView.cs</DependentUpon>
     </EmbeddedResource>
     <EmbeddedResource Include="DataSetView.resx">
       <DependentUpon>DataSetView.cs</DependentUpon>

sources/HeuristicLab.CEDMA.Core/3.3/Problem.cs
@@ -44,12 +44,8 @@ namespace HeuristicLab.CEDMA.Core {
   ///  * regression, time-series or classification problem
   /// </summary>
   public class Problem : ItemBase {
-    private string name;
-    public string Name {
-      get { return name; }
-    }
     private HeuristicLab.DataAnalysis.Dataset dataset;
-    public HeuristicLab.DataAnalysis.Dataset DataSet {
+    public HeuristicLab.DataAnalysis.Dataset Dataset {
       get { return dataset; }
     }
 

sources/HeuristicLab.CEDMA.Core/3.3/ProblemView.cs
@@ -43,7 +43,7 @@ namespace HeuristicLab.CEDMA.Core {
 
     protected override void UpdateControls() {
       base.UpdateControls();
-      datasetView.Dataset = problem.DataSet;
+      datasetView.Dataset = problem.Dataset;
       trainingSamplesStartTextBox.Text = problem.TrainingSamplesStart.ToString();
       trainingSamplesEndTextBox.Text = problem.TrainingSamplesEnd.ToString();
       validationSamplesStartTextBox.Text = problem.ValidationSamplesStart.ToString();
@@ -60,9 +60,9 @@ namespace HeuristicLab.CEDMA.Core {
       }
       targetsListBox.Items.Clear();
       inputsListBox.Items.Clear();
-      for (int i = 0; i < problem.DataSet.Columns; i++) {
-        targetsListBox.Items.Add(problem.DataSet.GetVariableName(i), problem.AllowedTargetVariables.Contains(i));
-        inputsListBox.Items.Add(problem.DataSet.GetVariableName(i), problem.AllowedInputVariables.Contains(i));
+      for (int i = 0; i < problem.Dataset.Columns; i++) {
+        targetsListBox.Items.Add(problem.Dataset.GetVariableName(i), problem.AllowedTargetVariables.Contains(i));
+        inputsListBox.Items.Add(problem.Dataset.GetVariableName(i), problem.AllowedInputVariables.Contains(i));
       }
     }
 
@@ -88,7 +88,7 @@ namespace HeuristicLab.CEDMA.Core {
           ShowErrorMessageBox(ex);
         }
         if (success) {
-          Dataset dataset = (Dataset)problem.DataSet;
+          Dataset dataset = (Dataset)problem.Dataset;
           dataset.Rows = parser.Rows;
           dataset.Columns = parser.Columns;
           for (int i = 0; i < parser.VariableNames.Length; i++) {
@@ -97,7 +97,7 @@ namespace HeuristicLab.CEDMA.Core {
           dataset.Name = parser.ProblemName;
           dataset.Samples = new double[dataset.Rows * dataset.Columns];
           Array.Copy(parser.Samples, dataset.Samples, dataset.Columns * dataset.Rows);
-          datasetView.Dataset = problem.DataSet;
+          datasetView.Dataset = problem.Dataset;
 
           problem.TrainingSamplesStart = parser.TrainingSamplesStart;
           problem.ValidationSamplesEnd = parser.TrainingSamplesStart;
@@ -115,7 +115,6 @@ namespace HeuristicLab.CEDMA.Core {
           Refresh();
         }
       }
-
     }
 
     private void targetsListBox_ItemCheck(object sender, ItemCheckEventArgs e) {
@@ -145,7 +144,7 @@ namespace HeuristicLab.CEDMA.Core {
         int testStart = int.Parse(testSamplesStartTextBox.Text);
         int testEnd = int.Parse(testSamplesEndTextBox.Text);
         if (trainingStart < 0 || validationStart < 0 || testStart < 0 ||
-          trainingEnd >= problem.DataSet.Rows || validationEnd >= problem.DataSet.Rows || testEnd >= problem.DataSet.Rows ||
+          trainingEnd >= problem.Dataset.Rows || validationEnd >= problem.Dataset.Rows || testEnd >= problem.Dataset.Rows ||
           trainingStart >= trainingEnd ||
           validationStart >= validationEnd ||
           testStart >= testEnd ||

commit 2de9f0da66dccca35d1c8a8c7b3de9aebe641054

Fixed a few bugs introduced with r2000.  #656 (CEDMA server should handle only one data set (problem) at a time)
git-svn-id: https://src.heuristiclab.com/svn/core/trunk@2012 2abd9481-f8db-48e9-bd25-06bc13291c1b

sources/HeuristicLab.CEDMA.Server/3.3/DispatcherBase.cs
@@ -39,40 +39,31 @@ using HeuristicLab.Modeling;
 namespace HeuristicLab.CEDMA.Server {
   public abstract class DispatcherBase : IDispatcher {
     private IStore store;
+    private DataSet dataset;
+    
     public DispatcherBase(IStore store) {
       this.store = store;
     }
 
     public IAlgorithm GetNextJob() {
-      // find and select a dataset
-      var dataSetVar = new HeuristicLab.CEDMA.DB.Interfaces.Variable("DataSet");
-      var dataSetQuery = new Statement[] {
-        new Statement(dataSetVar, Ontology.PredicateInstanceOf, Ontology.TypeDataSet)
-      };
-
-      Entity[] datasets = store.Query("?DataSet <" + Ontology.PredicateInstanceOf.Uri + "> <" + Ontology.TypeDataSet.Uri + "> .", 0, 100)
-        .Select(x => (Entity)x.Get("DataSet"))
-        .ToArray();
-
-      // no datasets => do nothing
-      if (datasets.Length == 0) return null;
-
-      Entity dataSetEntity = SelectDataSet(datasets);
-      DataSet dataSet = new DataSet(store, dataSetEntity);
-
-      int targetVariable = SelectTargetVariable(dataSetEntity, dataSet.Problem.AllowedTargetVariables.ToArray());
-      IAlgorithm selectedAlgorithm = SelectAlgorithm(dataSetEntity, targetVariable, dataSet.Problem.LearningTask);
-      string targetVariableName = dataSet.Problem.GetVariableName(targetVariable);
+      if (dataset == null) {
+        var datasetEntities = store.Query("?DataSet <" + Ontology.PredicateInstanceOf.Uri + "> <" + Ontology.TypeDataSet.Uri + "> .", 0, 1)
+          .Select(x => (Entity)x.Get("DataSet"));
+        if (datasetEntities.Count() == 0) return null;
+        dataset = new DataSet(store, datasetEntities.ElementAt(0));
+      }
+      int targetVariable = SelectTargetVariable(dataset.Problem.AllowedTargetVariables.ToArray());
+      IAlgorithm selectedAlgorithm = SelectAlgorithm(targetVariable, dataset.Problem.LearningTask);
+      string targetVariableName = dataset.Problem.GetVariableName(targetVariable);
 
       if (selectedAlgorithm != null) {
-        SetProblemParameters(selectedAlgorithm, dataSet.Problem, targetVariable);
+        SetProblemParameters(selectedAlgorithm, dataset.Problem, targetVariable);
       }
       return selectedAlgorithm;
     }
 
-    public abstract Entity SelectDataSet(Entity[] datasets);
-    public abstract int SelectTargetVariable(Entity dataSet, int[] targetVariables);
-    public abstract IAlgorithm SelectAlgorithm(Entity dataSet, int targetVariable, LearningTask learningTask);
+    public abstract int SelectTargetVariable(int[] targetVariables);
+    public abstract IAlgorithm SelectAlgorithm(int targetVariable, LearningTask learningTask);
 
     private void SetProblemParameters(IAlgorithm algo, Problem problem, int targetVariable) {
       algo.Dataset = problem.Dataset;

sources/HeuristicLab.CEDMA.Server/3.3/GridExecuter.cs
@@ -52,8 +52,10 @@ namespace HeuristicLab.CEDMA.Server {
       get { return TimeSpan.FromMilliseconds(100); }
     }
 
-    public GridExecuter(IDispatcher dispatcher, IStore store, string gridUrl) : base(dispatcher, store) {
+    public GridExecuter(IDispatcher dispatcher, IStore store, string gridUrl)
+      : base(dispatcher, store) {
       this.jobManager = new JobManager(gridUrl);
+      activeAlgorithms = new Dictionary<WaitHandle, IAlgorithm>();
       jobManager.Reset();
     }
 
@@ -114,7 +116,7 @@ namespace HeuristicLab.CEDMA.Server {
         string[] retVal = new string[activeAlgorithms.Count];
         int i = 0;
         foreach (IAlgorithm a in activeAlgorithms.Values) {
-          retVal[i++] = "Target-Variable: " + a.TargetVariable + " " + a.Description;
+          retVal[i++] = a.Name + " " + a.Dataset.GetVariableName(a.TargetVariable);
         }
         return retVal;
       }

sources/HeuristicLab.CEDMA.Server/3.3/HiveExecuter.cs
@@ -119,7 +119,7 @@ namespace HeuristicLab.CEDMA.Server {
         string[] retVal = new string[activeAlgorithms.Count];
         int i = 0;
         foreach (IAlgorithm a in activeAlgorithms.Values) {
-          retVal[i++] = "Target-Variable: " + a.TargetVariable + " " + a.Description;
+          retVal[i++] = a.Name + " " + a.Dataset.GetVariableName(a.TargetVariable);
         }
         return retVal;
       }

sources/HeuristicLab.CEDMA.Server/3.3/SimpleDispatcher.cs
@@ -40,47 +40,47 @@ namespace HeuristicLab.CEDMA.Server {
   public class SimpleDispatcher : DispatcherBase {
     private Random random;
     private IStore store;
-    private Dictionary<Entity, Dictionary<int, List<string>>> finishedAndDispatchedRuns;
+    private Dictionary<int, List<string>> finishedAndDispatchedRuns;
 
     public SimpleDispatcher(IStore store)
       : base(store) {
       this.store = store;
       random = new Random();
-      finishedAndDispatchedRuns = new Dictionary<Entity, Dictionary<int, List<string>>>();
+      finishedAndDispatchedRuns = new Dictionary<int, List<string>>();
       PopulateFinishedRuns();
     }
 
-    public override IAlgorithm SelectAlgorithm(Entity dataSetEntity, int targetVariable, LearningTask learningTask) {
+    public override IAlgorithm SelectAlgorithm(int targetVariable, LearningTask learningTask) {
       DiscoveryService ds = new DiscoveryService();
       IAlgorithm[] algos = ds.GetInstances<IAlgorithm>();
       IAlgorithm selectedAlgorithm = null;
       switch (learningTask) {
         case LearningTask.Regression: {
             var regressionAlgos = algos.Where(a => (a as IClassificationAlgorithm) == null && (a as ITimeSeriesAlgorithm) == null);
-            selectedAlgorithm = ChooseDeterministic(dataSetEntity, targetVariable, regressionAlgos) ?? ChooseStochastic(regressionAlgos);
+            selectedAlgorithm = ChooseDeterministic(targetVariable, regressionAlgos) ?? ChooseStochastic(regressionAlgos);
             break;
           }
         case LearningTask.Classification: {
             var classificationAlgos = algos.Where(a => (a as IClassificationAlgorithm) != null);
-            selectedAlgorithm = ChooseDeterministic(dataSetEntity, targetVariable, classificationAlgos) ?? ChooseStochastic(classificationAlgos);
+            selectedAlgorithm = ChooseDeterministic(targetVariable, classificationAlgos) ?? ChooseStochastic(classificationAlgos);
             break;
           }
         case LearningTask.TimeSeries: {
             var timeSeriesAlgos = algos.Where(a => (a as ITimeSeriesAlgorithm) != null);
-            selectedAlgorithm = ChooseDeterministic(dataSetEntity, targetVariable, timeSeriesAlgos) ?? ChooseStochastic(timeSeriesAlgos);
+            selectedAlgorithm = ChooseDeterministic(targetVariable, timeSeriesAlgos) ?? ChooseStochastic(timeSeriesAlgos);
             break;
           }
       }
       if (selectedAlgorithm != null) {
-        AddDispatchedRun(dataSetEntity, targetVariable, selectedAlgorithm.Name);
+        AddDispatchedRun(targetVariable, selectedAlgorithm.Name);
       }
       return selectedAlgorithm;
     }
 
-    private IAlgorithm ChooseDeterministic(Entity dataSetEntity, int targetVariable, IEnumerable<IAlgorithm> algos) {
+    private IAlgorithm ChooseDeterministic(int targetVariable, IEnumerable<IAlgorithm> algos) {
       var deterministicAlgos = algos
         .Where(a => (a as IStochasticAlgorithm) == null)
-        .Where(a => AlgorithmFinishedOrDispatched(dataSetEntity, targetVariable, a.Name) == false);
+        .Where(a => AlgorithmFinishedOrDispatched(targetVariable, a.Name) == false);
 
       if (deterministicAlgos.Count() == 0) return null;
       return deterministicAlgos.ElementAt(random.Next(deterministicAlgos.Count()));
@@ -92,47 +92,45 @@ namespace HeuristicLab.CEDMA.Server {
       return stochasticAlgos.ElementAt(random.Next(stochasticAlgos.Count()));
     }
 
-    public override Entity SelectDataSet(Entity[] datasets) {
-      return datasets[random.Next(datasets.Length)];
-    }
-
-    public override int SelectTargetVariable(Entity dataSet, int[] targetVariables) {
+    public override int SelectTargetVariable(int[] targetVariables) {
       return targetVariables[random.Next(targetVariables.Length)];
     }
 
     private void PopulateFinishedRuns() {
+      var datasetEntity = store
+        .Query(
+        "?Dataset <" + Ontology.PredicateInstanceOf + "> <" + Ontology.TypeDataSet + "> .", 0, 1)
+        .Select(x => (Entity)x.Get("Dataset")).ElementAt(0);
+      DataSet ds = new DataSet(store, datasetEntity);
+
       var result = store
-        .Query("?DataSet <" + Ontology.PredicateInstanceOf.Uri + "> <" + Ontology.TypeDataSet.Uri + "> ." + Environment.NewLine +
-        "?DataSet <" + Ontology.PredicateHasModel + "> ?Model ." + Environment.NewLine +
+        .Query(
         "?Model <" + Ontology.TargetVariable + "> ?TargetVariable ." + Environment.NewLine +
         "?Model <" + Ontology.AlgorithmName + "> ?AlgoName .",
         0, 1000)
-        .Select(x => new Resource[] { (Entity)x.Get("DataSet"), (Literal)x.Get("TargetVariable"), (Literal)x.Get("AlgoName") });
+        .Select(x => new Resource[] { (Literal)x.Get("TargetVariable"), (Literal)x.Get("AlgoName") });
 
       foreach (Resource[] row in result) {
-        Entity dataset = (Entity)row[0];
-        int targetVariable = (int)((Literal)row[1]).Value;
-        string algoName = (string)((Literal)row[2]).Value;
-        if (!AlgorithmFinishedOrDispatched(dataset, targetVariable, algoName))
-          AddDispatchedRun(dataset, targetVariable, algoName);
+        string targetVariable = (string)((Literal)row[0]).Value;
+        string algoName = (string)((Literal)row[1]).Value;
+
+        int targetVariableIndex = ds.Problem.Dataset.GetVariableIndex(targetVariable);
+        if (!AlgorithmFinishedOrDispatched(targetVariableIndex, algoName))
+          AddDispatchedRun(targetVariableIndex, algoName);
       }
     }
 
-    private void AddDispatchedRun(Entity dataSetEntity, int targetVariable, string algoName) {
-      if (!finishedAndDispatchedRuns.ContainsKey(dataSetEntity)) {
-        finishedAndDispatchedRuns[dataSetEntity] = new Dictionary<int, List<string>>();
+    private void AddDispatchedRun(int targetVariable, string algoName) {
+      if (!finishedAndDispatchedRuns.ContainsKey(targetVariable)) {
+        finishedAndDispatchedRuns[targetVariable] = new List<string>();
       }
-      if (!finishedAndDispatchedRuns[dataSetEntity].ContainsKey(targetVariable)) {
-        finishedAndDispatchedRuns[dataSetEntity][targetVariable] = new List<string>();
-      }
-      finishedAndDispatchedRuns[dataSetEntity][targetVariable].Add(algoName);
+      finishedAndDispatchedRuns[targetVariable].Add(algoName);
     }
 
-    private bool AlgorithmFinishedOrDispatched(Entity dataSetEntity, int targetVariable, string algoName) {
+    private bool AlgorithmFinishedOrDispatched(int targetVariable, string algoName) {
       return
-        finishedAndDispatchedRuns.ContainsKey(dataSetEntity) &&
-        finishedAndDispatchedRuns[dataSetEntity].ContainsKey(targetVariable) &&
-        finishedAndDispatchedRuns[dataSetEntity][targetVariable].Contains(algoName);
+        finishedAndDispatchedRuns.ContainsKey(targetVariable) &&
+        finishedAndDispatchedRuns[targetVariable].Contains(algoName);
     }
   }
 }

sources/HeuristicLab.DataAnalysis/3.2/Dataset.cs
@@ -119,15 +119,22 @@ namespace HeuristicLab.DataAnalysis {
       return variableNames[variableIndex];
     }
 
+    public int GetVariableIndex(string variableName) {
+      for (int i = 0; i < variableNames.Length; i++) {
+        if (variableNames[i].Equals(variableName)) return i;
+      }
+      throw new ArgumentException("The variable name " + variableName + " was not found.");
+    }
+
     public void SetVariableName(int variableIndex, string name) {
       variableNames[variableIndex] = name;
     }
-
 
     public override IView CreateView() {
       return new DatasetView(this);
     }
 
+    #region persistence
     public override object Clone(IDictionary<Guid, object> clonedObjects) {
       Dataset clone = new Dataset();
       clonedObjects.Add(Guid, clone);
@@ -255,6 +262,7 @@ namespace HeuristicLab.DataAnalysis {
       }
       return xs;
     }
+    #endregion
 
     public double GetMean(int column) {
       return GetMean(column, 0, Rows);