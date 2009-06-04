f0de91c935616ae3b37c800bc919de25d84fe233
kgrading <>
2009-06-04 15:39:39 +0000

    added webservices (#669)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@2010 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ sources/HeuristicLab.Hive.Client.Core/3.2/ConfigurationManager/UptimeManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Calendar;
+
+namespace HeuristicLab.Hive.Client.Core.ConfigurationManager {
+  public class UptimeManager {
+
+    public List<Appointment> Appointments { get; set; }
+
+    private static UptimeManager instance = null;
+    public static UptimeManager Instance {
+      get {
+        if (instance == null) {
+          instance = new UptimeManager();
+        }
+        return instance;
+      }
+    }
+
+    
+  }
+}
