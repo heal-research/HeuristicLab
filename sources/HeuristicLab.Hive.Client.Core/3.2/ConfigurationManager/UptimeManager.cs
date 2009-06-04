e41e506ac85fe35b36f2e7c77b0ed4eacf9d8957
kgrading <>
2009-06-04 16:04:01 +0000

    added uptimemanager (#669)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@2011 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Hive.Client.Core/3.2/ConfigurationManager/UptimeManager.cs
+++ sources/HeuristicLab.Hive.Client.Core/3.2/ConfigurationManager/UptimeManager.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Calendar;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace HeuristicLab.Hive.Client.Core.ConfigurationManager {
   public class UptimeManager {
 
     public List<Appointment> Appointments { get; set; }
+    private static String path = System.IO.Directory.GetCurrentDirectory()+"\\plugins\\Hive.Client.Jobs\\";
 
     private static UptimeManager instance = null;
     public static UptimeManager Instance {
@@ -19,6 +22,26 @@ namespace HeuristicLab.Hive.Client.Core.ConfigurationManager {
       }
     }
 
-    
+    private void PersistToHDD() {
+      XmlSerializer s = new XmlSerializer(typeof(List<Appointment>));
+      using (TextWriter w = new StreamWriter(path + "calendar.xml")) {
+        s.Serialize(w, Appointments);  
+      }
+    }
+
+    private void RestoreFromHDD() {
+      XmlSerializer s = new XmlSerializer(typeof(List<Appointment>));
+      using (TextReader r = new StreamReader(path + "calendar.xml")) {
+        Appointments = (List<Appointment>)s.Deserialize(r);        
+      }           
+    }
+
+    public bool isOnline(DateTime time) {
+      foreach (Appointment app in Appointments)
+        if ((time >= app.StartDate) &&
+            (time <= app.EndDate))
+          return true;
+      return false;
+    }
   }
 }
