45a7d567a9f5fbee0b0e0ce7b0b6d5017a9717f1
Stefan Vonolfen <stefan.vonolfen@gmail.com>
2009-06-04 13:49:07 +0000

    Implementd ClientConfigAdapter (#372)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@2002 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ sources/HeuristicLab.Hive.Server.ADODataAccess/3.2/ClientConfigAdapter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HeuristicLab.DataAccess.ADOHelper;
+using HeuristicLab.Hive.Contracts.BusinessObjects;
+using HeuristicLab.Hive.Server.DataAccess;
+using HeuristicLab.Hive.Server.ADODataAccess.TableAdapterWrapper;
+
+namespace HeuristicLab.Hive.Server.ADODataAccess {
+  class ClientConfigAdapter :
+    DataAdapterBase<
+      dsHiveServerTableAdapters.ClientConfigTableAdapter,
+      ClientConfig,
+      dsHiveServer.ClientConfigRow>,
+    IClientConfigAdapter {
+    public ClientConfigAdapter(): 
+      base(new ClientConfigAdapterWrapper()) {
+    }
+
+    protected override dsHiveServer.ClientConfigRow 
+      ConvertObj(ClientConfig config, dsHiveServer.ClientConfigRow row) {
+      if (row != null && config != null) {
+        row.ClientConfigId = config.Id;
+        row.HeartBeatIntervall = config.HeartBeatIntervall;
+        row.UpDownTimeCalendar = config.UpDownTimeCalendar;
+
+        return row;
+      } else
+        return null;
+    }
+
+    protected override ClientConfig ConvertRow(dsHiveServer.ClientConfigRow row, ClientConfig config) {
+      if (config != null && row != null) {
+        config.Id = row.ClientConfigId;
+
+        if (!row.IsHeartBeatIntervallNull())
+          config.HeartBeatIntervall = row.HeartBeatIntervall;
+        else
+          config.HeartBeatIntervall = 0;
+
+        if (!row.IsUpDownTimeCalendarNull())
+          config.UpDownTimeCalendar = row.UpDownTimeCalendar;
+        else
+          config.UpDownTimeCalendar = null;
+
+        return config;
+      } else
+        return null;
+    }
+  }
+}
---
+++ sources/HeuristicLab.Hive.Server.ADODataAccess/3.2/TableAdapterWrapper/ClientConfigAdapterWrapper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HeuristicLab.Hive.Contracts.BusinessObjects;
+using HeuristicLab.DataAccess.ADOHelper;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+namespace HeuristicLab.Hive.Server.ADODataAccess.TableAdapterWrapper {
+  class ClientConfigAdapterWrapper :
+    TableAdapterWrapperBase<
+        dsHiveServerTableAdapters.ClientConfigTableAdapter,
+    ClientConfig,
+    dsHiveServer.ClientConfigRow> {
+    public override void UpdateRow(dsHiveServer.ClientConfigRow row) {
+      TransactionalAdapter.Update(row);
+    }
+
+    public override dsHiveServer.ClientConfigRow
+      InsertNewRow(ClientConfig config) {
+      dsHiveServer.ClientConfigDataTable data =
+        new dsHiveServer.ClientConfigDataTable();
+
+      dsHiveServer.ClientConfigRow row = data.NewClientConfigRow();
+      row.ClientConfigId = config.Id;
+      data.AddClientConfigRow(row);
+
+      return row;
+    }
+
+    public override IEnumerable<dsHiveServer.ClientConfigRow>
+      FindById(Guid id) {
+      return TransactionalAdapter.GetDataById(id);
+    }
+
+    public override IEnumerable<dsHiveServer.ClientConfigRow>
+      FindAll() {
+      return TransactionalAdapter.GetData();
+    }
+
+    protected override void SetConnection(DbConnection connection) {
+      adapter.Connection = connection as SqlConnection;
+    }
+
+    protected override void SetTransaction(DbTransaction transaction) {
+      adapter.Transaction = transaction as SqlTransaction;
+    }
+  }
+}
