heal-research/HeuristicLab

commit f66c47d9794c9dfa969a0a735ab056ab820a91da

Improved schema for variable impacts. #644
git-svn-id: https://src.heuristiclab.com/svn/core/trunk@2047 2abd9481-f8db-48e9-bd25-06bc13291c1b

sources/HeuristicLab.CEDMA.Server/3.3/ExecuterBase.cs
@@ -70,33 +70,39 @@ namespace HeuristicLab.CEDMA.Server {
 
     protected void StoreResults(IAlgorithm finishedAlgorithm) {
       Entity modelEntity = new Entity(Ontology.CedmaNameSpace + Guid.NewGuid());
-      // store.Add(new Statement(finishedAlgorithm.DataSetEntity, Ontology.PredicateHasModel, modelEntity));
-      StoreModelAttribute(modelEntity, Ontology.TargetVariable, finishedAlgorithm.Model.TargetVariable);
-      StoreModelAttribute(modelEntity, Ontology.AlgorithmName, finishedAlgorithm.Description);
+      store.Add(new Statement(modelEntity, Ontology.InstanceOf, Ontology.TypeModel));
+      StoreEntityAttribute(modelEntity, Ontology.TargetVariable, finishedAlgorithm.Model.TargetVariable);
+      StoreEntityAttribute(modelEntity, Ontology.AlgorithmName, finishedAlgorithm.Description);
       
       IModel model = finishedAlgorithm.Model;
-      StoreModelAttribute(modelEntity, Ontology.TrainingMeanSquaredError, model.TrainingMeanSquaredError);
-      StoreModelAttribute(modelEntity, Ontology.ValidationMeanSquaredError, model.ValidationMeanSquaredError);
-      StoreModelAttribute(modelEntity, Ontology.TestMeanSquaredError, model.TestMeanSquaredError);
-      StoreModelAttribute(modelEntity, Ontology.TrainingCoefficientOfDetermination, model.TrainingCoefficientOfDetermination);
-      StoreModelAttribute(modelEntity, Ontology.ValidationCoefficientOfDetermination, model.ValidationCoefficientOfDetermination);
-      StoreModelAttribute(modelEntity, Ontology.TestCoefficientOfDetermination, model.TestCoefficientOfDetermination);
-      StoreModelAttribute(modelEntity, Ontology.TrainingVarianceAccountedFor, model.TrainingVarianceAccountedFor);
-      StoreModelAttribute(modelEntity, Ontology.ValidationVarianceAccountedFor, model.ValidationVarianceAccountedFor);
-      StoreModelAttribute(modelEntity, Ontology.TestVarianceAccountedFor, model.TestVarianceAccountedFor);
-      StoreModelAttribute(modelEntity, Ontology.TrainingMeanAbsolutePercentageError, model.TrainingMeanAbsolutePercentageError);
-      StoreModelAttribute(modelEntity, Ontology.ValidationMeanAbsolutePercentageError, model.ValidationMeanAbsolutePercentageError);
-      StoreModelAttribute(modelEntity, Ontology.TestMeanAbsolutePercentageError, model.TestMeanAbsolutePercentageError);
-      StoreModelAttribute(modelEntity, Ontology.TrainingMeanAbsolutePercentageOfRangeError, model.TrainingMeanAbsolutePercentageOfRangeError);
-      StoreModelAttribute(modelEntity, Ontology.ValidationMeanAbsolutePercentageOfRangeError, model.ValidationMeanAbsolutePercentageOfRangeError);
-      StoreModelAttribute(modelEntity, Ontology.TestMeanAbsolutePercentageOfRangeError, model.TestMeanAbsolutePercentageOfRangeError);
+      StoreEntityAttribute(modelEntity, Ontology.TrainingMeanSquaredError, model.TrainingMeanSquaredError);
+      StoreEntityAttribute(modelEntity, Ontology.ValidationMeanSquaredError, model.ValidationMeanSquaredError);
+      StoreEntityAttribute(modelEntity, Ontology.TestMeanSquaredError, model.TestMeanSquaredError);
+      StoreEntityAttribute(modelEntity, Ontology.TrainingCoefficientOfDetermination, model.TrainingCoefficientOfDetermination);
+      StoreEntityAttribute(modelEntity, Ontology.ValidationCoefficientOfDetermination, model.ValidationCoefficientOfDetermination);
+      StoreEntityAttribute(modelEntity, Ontology.TestCoefficientOfDetermination, model.TestCoefficientOfDetermination);
+      StoreEntityAttribute(modelEntity, Ontology.TrainingVarianceAccountedFor, model.TrainingVarianceAccountedFor);
+      StoreEntityAttribute(modelEntity, Ontology.ValidationVarianceAccountedFor, model.ValidationVarianceAccountedFor);
+      StoreEntityAttribute(modelEntity, Ontology.TestVarianceAccountedFor, model.TestVarianceAccountedFor);
+      StoreEntityAttribute(modelEntity, Ontology.TrainingMeanAbsolutePercentageError, model.TrainingMeanAbsolutePercentageError);
+      StoreEntityAttribute(modelEntity, Ontology.ValidationMeanAbsolutePercentageError, model.ValidationMeanAbsolutePercentageError);
+      StoreEntityAttribute(modelEntity, Ontology.TestMeanAbsolutePercentageError, model.TestMeanAbsolutePercentageError);
+      StoreEntityAttribute(modelEntity, Ontology.TrainingMeanAbsolutePercentageOfRangeError, model.TrainingMeanAbsolutePercentageOfRangeError);
+      StoreEntityAttribute(modelEntity, Ontology.ValidationMeanAbsolutePercentageOfRangeError, model.ValidationMeanAbsolutePercentageOfRangeError);
+      StoreEntityAttribute(modelEntity, Ontology.TestMeanAbsolutePercentageOfRangeError, model.TestMeanAbsolutePercentageOfRangeError);
 
       for (int i = 0; i < finishedAlgorithm.Dataset.Columns; i++) {
         try {
-          double qualImpact = model.GetVariableQualityImpact(finishedAlgorithm.Dataset.GetVariableName(i));
-          Entity impactEntity = new Entity(Ontology.CedmaNameSpace + Guid.NewGuid());
-          store.Add(new Statement(impactEntity, Ontology.PredicateInstanceOf, Ontology.TypeVariableQualityImpact));
-          store.Add(new Statement(modelEntity, impactEntity, new Literal(qualImpact)));
+          string variableName = finishedAlgorithm.Dataset.GetVariableName(i);
+          double qualImpact = model.GetVariableQualityImpact(variableName);
+          double evalImpact = model.GetVariableEvaluationImpact(variableName);
+
+          Entity inputVariableEntity = new Entity(Ontology.CedmaNameSpace + Guid.NewGuid());
+          store.Add(new Statement(inputVariableEntity, Ontology.InstanceOf, Ontology.TypeVariableImpact));
+          store.Add(new Statement(modelEntity, Ontology.HasInputVariable, inputVariableEntity));
+          StoreEntityAttribute(inputVariableEntity, Ontology.EvaluationImpact, evalImpact);
+          StoreEntityAttribute(inputVariableEntity, Ontology.QualityImpact, qualImpact);
+          StoreEntityAttribute(inputVariableEntity, Ontology.Name, variableName);
         }
         catch (ArgumentException) {
           // ignore
@@ -104,11 +110,11 @@ namespace HeuristicLab.CEDMA.Server {
       }
 
       byte[] serializedModel = PersistenceManager.SaveToGZip(model.Data);
-      store.Add(new Statement(modelEntity, Ontology.PredicateSerializedData, new Literal(Convert.ToBase64String(serializedModel))));
+      store.Add(new Statement(modelEntity, Ontology.SerializedData, new Literal(Convert.ToBase64String(serializedModel))));
     }
 
-    private void StoreModelAttribute(Entity model, Entity predicate, object value) {
-      store.Add(new Statement(model, predicate, new Literal(value)));
+    private void StoreEntityAttribute(Entity entity, Entity predicate, object value) {
+      store.Add(new Statement(entity, predicate, new Literal(value)));
     }
 
     public abstract string[] GetJobs();

sources/HeuristicLab.CEDMA.Charting/3.3/BubbleChart.cs
@@ -265,7 +265,7 @@ namespace HeuristicLab.CEDMA.Charting {
       if (button == MouseButtons.Left) {
         ResultsEntry entry = GetResultsEntry(point);
         if (entry != null) {
-          string serializedData = (string)entry.Get(Ontology.PredicateSerializedData.Uri.Replace(Ontology.CedmaNameSpace, ""));
+          string serializedData = (string)entry.Get(Ontology.SerializedData.Uri.Replace(Ontology.CedmaNameSpace, ""));
           var model = (IItem)PersistenceManager.RestoreFromGZip(Convert.FromBase64String(serializedData));
           PluginManager.ControlManager.ShowControl(model.CreateView());
         }

sources/HeuristicLab.CEDMA.Core/3.3/Console.cs
@@ -71,7 +71,7 @@ namespace HeuristicLab.CEDMA.Core {
 
     internal void Connect(string serverUri) {
       IStore store = new StoreProxy(serverUri);
-      var variableBindings = store.Query("?Dataset <" + Ontology.PredicateInstanceOf + "> <" + Ontology.TypeDataSet + "> .", 0, 10).ToArray();
+      var variableBindings = store.Query("?Dataset <" + Ontology.InstanceOf + "> <" + Ontology.TypeDataSet + "> .", 0, 10).ToArray();
       if (variableBindings.Length > 0) {
         dataSet = new DataSet(store, (Entity)variableBindings[0].Get("Dataset"));
       } else {

sources/HeuristicLab.CEDMA.Core/3.3/DataSet.cs
@@ -49,7 +49,7 @@ namespace HeuristicLab.CEDMA.Core {
         // lazy loading of problem from DB
         if (problem == null) {
           var persistedData = Store.Query(
-            "<" + Ontology.CedmaNameSpace + Guid + "> <" + Ontology.PredicateSerializedData.Uri + "> ?SerializedData .", 0, 10);
+            "<" + Ontology.CedmaNameSpace + Guid + "> <" + Ontology.SerializedData.Uri + "> ?SerializedData .", 0, 10);
           if (persistedData.Count() == 1) {
             Literal persistedLiteral = (Literal)persistedData.First().Get("SerializedData");
             this.problem = (Problem)PersistenceManager.RestoreFromGZip(Convert.FromBase64String((string)persistedLiteral.Value));
@@ -82,9 +82,9 @@ namespace HeuristicLab.CEDMA.Core {
 
     public void Activate() {
       Entity myEntity = new Entity(Ontology.CedmaNameSpace + Guid);
-      Store.Add(new Statement(myEntity, Ontology.PredicateInstanceOf, Ontology.TypeDataSet));
-      Store.Add(new Statement(myEntity, Ontology.PredicateSerializedData, new Literal(Convert.ToBase64String(PersistenceManager.SaveToGZip(problem)))));
-      Store.Add(new Statement(myEntity, Ontology.PredicateName, new Literal(name)));
+      Store.Add(new Statement(myEntity, Ontology.InstanceOf, Ontology.TypeDataSet));
+      Store.Add(new Statement(myEntity, Ontology.SerializedData, new Literal(Convert.ToBase64String(PersistenceManager.SaveToGZip(problem)))));
+      Store.Add(new Statement(myEntity, Ontology.Name, new Literal(name)));
       activated = true;
     }
 

sources/HeuristicLab.CEDMA.Core/3.3/Results.cs
@@ -86,7 +86,7 @@ namespace HeuristicLab.CEDMA.Core {
       if (store == null) yield break;
       entries = new List<ResultsEntry>();
       do {
-        var allBindings = store.Query("?Dataset <" + Ontology.PredicateHasModel + "> ?Model ." + Environment.NewLine +
+        var allBindings = store.Query("?Dataset <" + Ontology.HasModel + "> ?Model ." + Environment.NewLine +
           "?Model ?Attribute ?Value .", page, PAGE_SIZE);
         var allModelBindings = allBindings.GroupBy(x => (Entity)x.Get("Model"));
         resultsReturned = allBindings.Count;
@@ -124,11 +124,11 @@ namespace HeuristicLab.CEDMA.Core {
 
     private void LoadModelAttributes() {
       this.ordinalVariables =
-        store.Query("?ModelAttribute <" + Ontology.PredicateInstanceOf + "> <" + Ontology.TypeOrdinalAttribute + "> .", 0, 100)
+        store.Query("?ModelAttribute <" + Ontology.InstanceOf + "> <" + Ontology.TypeOrdinalAttribute + "> .", 0, 100)
         .Select(s => ((Entity)s.Get("ModelAttribute")).Uri.Replace(Ontology.CedmaNameSpace, ""))
         .ToArray();
       this.categoricalVariables =
-        store.Query("?ModelAttribute <" + Ontology.PredicateInstanceOf + "> <" + Ontology.TypeCategoricalAttribute + "> .", 0, 100)
+        store.Query("?ModelAttribute <" + Ontology.InstanceOf + "> <" + Ontology.TypeCategoricalAttribute + "> .", 0, 100)
         .Select(s => ((Entity)s.Get("ModelAttribute")).Uri.Replace(Ontology.CedmaNameSpace, ""))
         .ToArray();
     }

sources/HeuristicLab.CEDMA.Core/3.3/TableResultsView.cs
@@ -67,7 +67,7 @@ namespace HeuristicLab.CEDMA.Core {
       if (e.Button == MouseButtons.Left && e.Clicks == 2) {
         DataGridView.HitTestInfo hitInfo = dataGridView.HitTest(e.X, e.Y);
         ResultsEntry entry = (ResultsEntry)dataGridView.Rows[hitInfo.RowIndex].Tag;
-        string serializedData =  (string)entry.Get(Ontology.PredicateSerializedData.Uri.Replace(Ontology.CedmaNameSpace, ""));
+        string serializedData =  (string)entry.Get(Ontology.SerializedData.Uri.Replace(Ontology.CedmaNameSpace, ""));
         var model = (IItem)PersistenceManager.RestoreFromGZip(Convert.FromBase64String(serializedData));
         PluginManager.ControlManager.ShowControl(model.CreateView());
       }

sources/HeuristicLab.CEDMA.DB.Interfaces/3.3/Ontology.cs
@@ -62,33 +62,40 @@ namespace HeuristicLab.CEDMA.DB.Interfaces {
     #endregion
 
     #region meta-data predicates
-    public static Entity PredicateName {
+    public static Entity Name {
       get { return new Entity(CedmaNameSpace + "Name"); }
     }
     /// <summary>
     /// Each entity can have a serialized XML representation that can be loaded in HL.
     /// </summary>
-    public static Entity PredicateSerializedData {
+    public static Entity SerializedData {
       get { return new Entity(CedmaNameSpace + "SerializedData"); }
     }
     /// <summary>
     /// Link from data set to model.
     /// A data set can have multiple models.
     /// </summary>
-    public static Entity PredicateHasModel {
+    public static Entity HasModel {
       get { return new Entity(CedmaNameSpace + "Model"); }
     }
     /// <summary>
     /// Link from model to algorithm.
     /// An algorithm can generate multiple models, each model is generated by exactly one algorithm.
     /// </summary>
-    public static Entity PredicateGeneratedBy {
+    public static Entity GeneratedBy {
       get { return new Entity(CedmaNameSpace + "GeneratedBy"); }
+    }
+    /// <summary>
+    /// Link from model to input variable impact
+    /// Each model can have multiple input variables and each of the input variables can have a different impact on the model.
+    /// </summary>
+    public static Entity HasInputVariable {
+      get { return new Entity(CedmaNameSpace + "InputVariable"); }
     }
     #endregion
 
     #region types and type relations
-    public static Entity PredicateInstanceOf {
+    public static Entity InstanceOf {
       get { return new Entity(CedmaNameSpace + "InstanceOf"); }
     }
     public static Entity TypeDataSet {
@@ -101,16 +108,13 @@ namespace HeuristicLab.CEDMA.DB.Interfaces {
       get { return new Entity(CedmaNameSpace + "Model"); }
     }
     public static Entity TypeVariableImpact {
-      get { return new Entity(CedmaNameSpace + "VariableQualityImpact"); }
+      get { return new Entity(CedmaNameSpace + "VariableImpact"); }
     }
     #endregion
 
-    #region default attributes
+    #region attributes for models
     public static Entity TargetVariable {
       get { return new Entity(CedmaNameSpace + "TargetVariable"); }
-    }
-    public static Entity AlgorithmName {
-      get { return new Entity(CedmaNameSpace + "AlgorithmName"); }
     }
     public static Entity TrainingMeanSquaredError {
       get { return new Entity(CedmaNameSpace + "TrainingMeanSquaredError"); }
@@ -185,54 +189,65 @@ namespace HeuristicLab.CEDMA.DB.Interfaces {
       get { return new Entity(CedmaNameSpace + "EvaluatedSolutions"); }
     }
     #endregion
+    #region attributes for input variables
+    public static Entity EvaluationImpact {
+      get { return new Entity(CedmaNameSpace + "EvaluationImpact"); }
+    }
+    public static Entity QualityImpact {
+      get { return new Entity(CedmaNameSpace + "QualityImpact"); }
+    }
+    #endregion
 
     public static ICollection<Statement> InitialStatements {
       get {
         return new List<Statement> {
-          new Statement(TargetVariable, PredicateInstanceOf, TypeCategoricalAttribute),
-          new Statement(AlgorithmName, PredicateInstanceOf, TypeCategoricalAttribute),
-          new Statement(TrainingMeanSquaredError, PredicateInstanceOf, TypeOrdinalAttribute),
-          new Statement(TrainingMeanSquaredError, PredicateInstanceOf, TypeQualityAttribute),
-          new Statement(ValidationMeanSquaredError, PredicateInstanceOf, TypeOrdinalAttribute),
-          new Statement(ValidationMeanSquaredError, PredicateInstanceOf, TypeQualityAttribute),
-          new Statement(TestMeanSquaredError, PredicateInstanceOf, TypeOrdinalAttribute),
-          new Statement(TestMeanSquaredError, PredicateInstanceOf, TypeQualityAttribute),
-          new Statement(TrainingMeanAbsolutePercentageError, PredicateInstanceOf, TypeOrdinalAttribute),
-          new Statement(TrainingMeanAbsolutePercentageError, PredicateInstanceOf, TypeQualityAttribute),
-          new Statement(ValidationMeanAbsolutePercentageError, PredicateInstanceOf, TypeOrdinalAttribute),
-          new Statement(ValidationMeanAbsolutePercentageError, PredicateInstanceOf, TypeQualityAttribute),
-          new Statement(TestMeanAbsolutePercentageError, PredicateInstanceOf, TypeOrdinalAttribute),
-          new Statement(TestMeanAbsolutePercentageError, PredicateInstanceOf, TypeQualityAttribute),
-          new Statement(TrainingMeanAbsolutePercentageOfRangeError, PredicateInstanceOf, TypeOrdinalAttribute),
-          new Statement(TrainingMeanAbsolutePercentageOfRangeError, PredicateInstanceOf, TypeQualityAttribute),
-          new Statement(ValidationMeanAbsolutePercentageOfRangeError, PredicateInstanceOf, TypeOrdinalAttribute),
-          new Statement(ValidationMeanAbsolutePercentageOfRangeError, PredicateInstanceOf, TypeQualityAttribute),
-          new Statement(TestMeanAbsolutePercentageOfRangeError, PredicateInstanceOf, TypeOrdinalAttribute),
-          new Statement(TestMeanAbsolutePercentageOfRangeError, PredicateInstanceOf, TypeQualityAttribute),
-          new Statement(TrainingCoefficientOfDetermination, PredicateInstanceOf, TypeOrdinalAttribute),
-          new Statement(TrainingCoefficientOfDetermination, PredicateInstanceOf, TypeQualityAttribute),
-          new Statement(ValidationCoefficientOfDetermination, PredicateInstanceOf, TypeOrdinalAttribute),
-          new Statement(ValidationCoefficientOfDetermination, PredicateInstanceOf, TypeQualityAttribute),
-          new Statement(TestCoefficientOfDetermination, PredicateInstanceOf, TypeOrdinalAttribute),
-          new Statement(TestCoefficientOfDetermination, PredicateInstanceOf, TypeQualityAttribute),
-          new Statement(TrainingAccuracy, PredicateInstanceOf, TypeOrdinalAttribute),
-          new Statement(TrainingAccuracy, PredicateInstanceOf, TypeQualityAttribute),
-          new Statement(ValidationAccuracy, PredicateInstanceOf, TypeOrdinalAttribute),
-          new Statement(ValidationAccuracy, PredicateInstanceOf, TypeQualityAttribute),
-          new Statement(TestAccuracy, PredicateInstanceOf, TypeOrdinalAttribute),
-          new Statement(TestAccuracy, PredicateInstanceOf, TypeQualityAttribute),
-          new Statement(TrainingTheilsInequalityCoefficient, PredicateInstanceOf, TypeOrdinalAttribute),
-          new Statement(TrainingTheilsInequalityCoefficient, PredicateInstanceOf, TypeQualityAttribute),
-          new Statement(ValidationTheilsInequalityCoefficient, PredicateInstanceOf, TypeOrdinalAttribute),
-          new Statement(ValidationTheilsInequalityCoefficient, PredicateInstanceOf, TypeQualityAttribute),
-          new Statement(TestTheilsInequalityCoefficient, PredicateInstanceOf, TypeOrdinalAttribute),
-          new Statement(TestTheilsInequalityCoefficient, PredicateInstanceOf, TypeQualityAttribute),
+          new Statement(TargetVariable, InstanceOf, TypeCategoricalAttribute),
+          new Statement(TrainingMeanSquaredError, InstanceOf, TypeOrdinalAttribute),
+          new Statement(TrainingMeanSquaredError, InstanceOf, TypeQualityAttribute),
+          new Statement(ValidationMeanSquaredError, InstanceOf, TypeOrdinalAttribute),
+          new Statement(ValidationMeanSquaredError, InstanceOf, TypeQualityAttribute),
+          new Statement(TestMeanSquaredError, InstanceOf, TypeOrdinalAttribute),
+          new Statement(TestMeanSquaredError, InstanceOf, TypeQualityAttribute),
+          new Statement(TrainingMeanAbsolutePercentageError, InstanceOf, TypeOrdinalAttribute),
+          new Statement(TrainingMeanAbsolutePercentageError, InstanceOf, TypeQualityAttribute),
+          new Statement(ValidationMeanAbsolutePercentageError, InstanceOf, TypeOrdinalAttribute),
+          new Statement(ValidationMeanAbsolutePercentageError, InstanceOf, TypeQualityAttribute),
+          new Statement(TestMeanAbsolutePercentageError, InstanceOf, TypeOrdinalAttribute),
+          new Statement(TestMeanAbsolutePercentageError, InstanceOf, TypeQualityAttribute),
+          new Statement(TrainingMeanAbsolutePercentageOfRangeError, InstanceOf, TypeOrdinalAttribute),
+          new Statement(TrainingMeanAbsolutePercentageOfRangeError, InstanceOf, TypeQualityAttribute),
+          new Statement(ValidationMeanAbsolutePercentageOfRangeError, InstanceOf, TypeOrdinalAttribute),
+          new Statement(ValidationMeanAbsolutePercentageOfRangeError, InstanceOf, TypeQualityAttribute),
+          new Statement(TestMeanAbsolutePercentageOfRangeError, InstanceOf, TypeOrdinalAttribute),
+          new Statement(TestMeanAbsolutePercentageOfRangeError, InstanceOf, TypeQualityAttribute),
+          new Statement(TrainingCoefficientOfDetermination, InstanceOf, TypeOrdinalAttribute),
+          new Statement(TrainingCoefficientOfDetermination, InstanceOf, TypeQualityAttribute),
+          new Statement(ValidationCoefficientOfDetermination, InstanceOf, TypeOrdinalAttribute),
+          new Statement(ValidationCoefficientOfDetermination, InstanceOf, TypeQualityAttribute),
+          new Statement(TestCoefficientOfDetermination, InstanceOf, TypeOrdinalAttribute),
+          new Statement(TestCoefficientOfDetermination, InstanceOf, TypeQualityAttribute),
+          new Statement(TrainingAccuracy, InstanceOf, TypeOrdinalAttribute),
+          new Statement(TrainingAccuracy, InstanceOf, TypeQualityAttribute),
+          new Statement(ValidationAccuracy, InstanceOf, TypeOrdinalAttribute),
+          new Statement(ValidationAccuracy, InstanceOf, TypeQualityAttribute),
+          new Statement(TestAccuracy, InstanceOf, TypeOrdinalAttribute),
+          new Statement(TestAccuracy, InstanceOf, TypeQualityAttribute),
+          new Statement(TrainingTheilsInequalityCoefficient, InstanceOf, TypeOrdinalAttribute),
+          new Statement(TrainingTheilsInequalityCoefficient, InstanceOf, TypeQualityAttribute),
+          new Statement(ValidationTheilsInequalityCoefficient, InstanceOf, TypeOrdinalAttribute),
+          new Statement(ValidationTheilsInequalityCoefficient, InstanceOf, TypeQualityAttribute),
+          new Statement(TestTheilsInequalityCoefficient, InstanceOf, TypeOrdinalAttribute),
+          new Statement(TestTheilsInequalityCoefficient, InstanceOf, TypeQualityAttribute),
 
-          new Statement(TreeSize, PredicateInstanceOf, TypeOrdinalAttribute),
-          new Statement(TreeSize, PredicateInstanceOf, TypeComplexityAttribute),
-          new Statement(TreeHeight, PredicateInstanceOf, TypeOrdinalAttribute),
-          new Statement(TreeHeight, PredicateInstanceOf, TypeComplexityAttribute),
-          new Statement(EvaluatedSolutions, PredicateInstanceOf, TypeOrdinalAttribute)
+          new Statement(TreeSize, InstanceOf, TypeOrdinalAttribute),
+          new Statement(TreeSize, InstanceOf, TypeComplexityAttribute),
+          new Statement(TreeHeight, InstanceOf, TypeOrdinalAttribute),
+          new Statement(TreeHeight, InstanceOf, TypeComplexityAttribute),
+          new Statement(EvaluatedSolutions, InstanceOf, TypeOrdinalAttribute),
+
+          new Statement(Name, InstanceOf, TypeCategoricalAttribute),
+          new Statement(QualityImpact, InstanceOf, TypeOrdinalAttribute),
+          new Statement(EvaluationImpact, InstanceOf, TypeOrdinalAttribute)
         };
       }
     }

sources/HeuristicLab.CEDMA.Server/3.3/DispatcherBase.cs
@@ -47,7 +47,7 @@ namespace HeuristicLab.CEDMA.Server {
 
     public IAlgorithm GetNextJob() {
       if (dataset == null) {
-        var datasetEntities = store.Query("?DataSet <" + Ontology.PredicateInstanceOf.Uri + "> <" + Ontology.TypeDataSet.Uri + "> .", 0, 1)
+        var datasetEntities = store.Query("?DataSet <" + Ontology.InstanceOf.Uri + "> <" + Ontology.TypeDataSet.Uri + "> .", 0, 1)
           .Select(x => (Entity)x.Get("DataSet"));
         if (datasetEntities.Count() == 0) return null;
         dataset = new DataSet(store, datasetEntities.ElementAt(0));

commit ba302e3b3eb6ce3729257037058772e2588ef37a

Fixed compile error (introduced with r2047). #644
git-svn-id: https://src.heuristiclab.com/svn/core/trunk@2048 2abd9481-f8db-48e9-bd25-06bc13291c1b

sources/HeuristicLab.CEDMA.Server/3.3/ExecuterBase.cs
@@ -72,7 +72,7 @@ namespace HeuristicLab.CEDMA.Server {
       Entity modelEntity = new Entity(Ontology.CedmaNameSpace + Guid.NewGuid());
       store.Add(new Statement(modelEntity, Ontology.InstanceOf, Ontology.TypeModel));
       StoreEntityAttribute(modelEntity, Ontology.TargetVariable, finishedAlgorithm.Model.TargetVariable);
-      StoreEntityAttribute(modelEntity, Ontology.AlgorithmName, finishedAlgorithm.Description);
+      StoreEntityAttribute(modelEntity, Ontology.Name, finishedAlgorithm.Description);
       
       IModel model = finishedAlgorithm.Model;
       StoreEntityAttribute(modelEntity, Ontology.TrainingMeanSquaredError, model.TrainingMeanSquaredError);