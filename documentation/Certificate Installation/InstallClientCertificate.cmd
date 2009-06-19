5d977f95d2d5d9b6e9bd814b4e76fa622d9ee56c
mbecirov <>
2009-06-19 12:39:17 +0000

    #586: Added server certificate.
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@2072 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- documentation/Certificate Installation/InstallClientCertificate.cmd
+++ documentation/Certificate Installation/InstallClientCertificate.cmd
@@ -3,9 +3,9 @@ REM Note: Installation of Certificates requires Admin-Rights!
 REM       Be sure the certhash is the same as on the generated certificate (fingerprint)!
 
 REM --install public key on windows xp clients:
-httpcfg set ssl -i 0.0.0.0:9000 -h 3ccf80ac7d3fac977f76517e576084f2a58382bd
+httpcfg set ssl -i 0.0.0.0:9000 -h 280aea192b55742cb44b171326a15383afa2819a
 
 REM --install public key on vista clients:
-REM netsh http add sslcert ipport=0.0.0.0:9000 certhash=3ccf80ac7d3fac977f76517e576084f2a58382bd appid={00112233-4455-6677-8899-AABBCCDDEEFF}
+REM netsh http add sslcert ipport=0.0.0.0:9000 certhash=280aea192b55742cb44b171326a15383afa2819a appid={00112233-4455-6677-8899-AABBCCDDEEFF}
 
 pause
