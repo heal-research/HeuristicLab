f33476ec63078e5e0301d0eb8a288bcdb91f6089
Michael Kommenda <michael.kommenda@fh-ooe.at>
2009-08-05 17:31:03 +0000

    forgotten to add one Interface in !HeuristicLab.!MainForm (ticket #716)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@2244 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ sources/HeuristicLab.MainForm/3.2/Interfaces/IUserInterfaceItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HeuristicLab.MainForm {
+  public interface IUserInterfaceItem : IAction {
+    string Name { get; }
+  }
+}
