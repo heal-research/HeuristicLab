6b259128972d0b2a584a29903cd8efd5fd9c6ba7
Michael Kommenda <michael.kommenda@fh-ooe.at>
2009-11-10 16:48:04 +0000

    Added !AssemblyInfo.cs and missing plugin files in plugin class to project !HeuristicLab.SQLServerCompact (ticket #798)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@2477 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.SQLServerCompact/3.2/HeuristicLabSQLServerCompactPlugin.cs
+++ sources/HeuristicLab.SQLServerCompact/3.2/HeuristicLabSQLServerCompactPlugin.cs
@@ -8,7 +8,13 @@ using HeuristicLab.PluginInfrastructure;
 namespace HeuristicLab.SQLServerCompact {
   [ClassInfo(Name = "HeuristicLab.SQLServerCompact-3.2")]
   [PluginFile(Filename = "HeuristicLab.SQLServerCompact-3.2.dll", Filetype = PluginFileType.Assembly)]
-  [PluginFile(Filename = "System.Data.SqlServerCe.dll", Filetype = PluginFileType.Assembly)] 
+  [PluginFile(Filename = "sqlceca35.dll", Filetype = PluginFileType.Data)]
+  [PluginFile(Filename = "sqlcecompact35.dll", Filetype = PluginFileType.Data)]
+  [PluginFile(Filename = "sqlceer35EN.dll", Filetype = PluginFileType.Data)]
+  [PluginFile(Filename = "sqlceme35.dll", Filetype = PluginFileType.Data)]
+  [PluginFile(Filename = "sqlceoledb35.dll", Filetype = PluginFileType.Data)]
+  [PluginFile(Filename = "sqlceqp35.dll", Filetype = PluginFileType.Data)]
+  [PluginFile(Filename = "sqlcese35.dll", Filetype = PluginFileType.Data)] 
   public class HeuristicLabSQLServerCompactPlugin : PluginBase {
   }
 }
