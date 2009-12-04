9e98ff92f9b97e6ec25790c325b282fa5ee67d2e
Michael Kommenda <michael.kommenda@fh-ooe.at>
2009-12-04 16:24:24 +0000

    added !ViewContextMenuStrip  (ticket #771)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@2545 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ sources/HeuristicLab.MainForm.WindowsForms/3.2/ViewContextMenuStrip.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HeuristicLab.MainForm.WindowsForms {
+  public partial class ViewContextMenuStrip : ContextMenuStrip {
+    private object item;
+    public ViewContextMenuStrip() {
+      InitializeComponent();
+    }
+
+    public ViewContextMenuStrip(object item) :this() {
+      if (item != null) {
+        this.item = item;
+        IEnumerable<Type> types = MainFormManager.GetViewTypes(item.GetType());
+        if (types != null) {
+          foreach (Type t in types) {
+            ToolStripMenuItem menuItem = new ToolStripMenuItem() {
+              Text = t.Name,
+              Tag = t
+            };
+            this.Items.Add(menuItem);
+          }
+        }
+      }
+    }
+
+    private void ViewContextMenuStrip_ItemClicked(object sender, ToolStripItemClickedEventArgs e) {
+      if (item != null) {
+        Type viewType = e.ClickedItem.Tag as Type;
+        IView view = MainFormManager.CreateView(viewType, this.item);
+        MainFormManager.MainForm.ShowView(view);
+      }
+    }
+
+  }
+}
