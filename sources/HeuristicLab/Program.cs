7610a867a388dbd0a20a83138bd5232d521a5ae9
Gabriel Kronberger <gabriel.kronberger@fh-hagenberg.at>
2010-01-05 14:31:21 +0000

    Re-added ICSharpZipLib and replaced main start up code with call to plugin infrastructure start up code. #799
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@2600 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab/Program.cs
+++ sources/HeuristicLab/Program.cs
@@ -20,98 +20,12 @@
 #endregion
 
 using System;
-using System.Collections.Generic;
-using System.Windows.Forms;
-using System.Xml;
-using System.Threading;
-using System.Text;
-using System.Linq;
-using HeuristicLab.PluginInfrastructure;
-using HeuristicLab.PluginInfrastructure.Advanced;
-using System.Runtime.InteropServices;
-using HeuristicLab.PluginInfrastructure.Starter;
-using System.IO;
 
 namespace HeuristicLab {
   static class Program {
     [STAThread]
     static void Main(string[] args) {
-      if (args.Length == 0) {  // normal mode
-        try {
-          Application.EnableVisualStyles();
-          Application.SetCompatibleTextRenderingDefault(false);
-          Application.Run(new StarterForm());
-        }
-        catch (Exception ex) {
-          ShowErrorMessageBox(ex);
-        }
-
-      } else {
-        var cmd = args[0].ToUpperInvariant();
-        string pluginDir = Path.GetFullPath(Application.StartupPath);
-        switch (cmd) {
-          case "START": {
-              if (args.Length != 2) {
-                PrintUsage();
-              } else {
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new StarterForm(args[1]));
-              }
-              break;
-            }
-          case "SHOW": {
-              InstallationManagerConsole managerConsole = new InstallationManagerConsole(pluginDir);
-              managerConsole.Show(args.Skip(1));
-              break;
-            }
-          case "INSTALL": {
-              InstallationManagerConsole managerConsole = new InstallationManagerConsole(pluginDir);
-              managerConsole.Install(args.Skip(1));
-              break;
-            }
-          case "UPDATE": {
-              InstallationManagerConsole managerConsole = new InstallationManagerConsole(pluginDir);
-              managerConsole.Update(args.Skip(1));
-              break;
-            }
-          case "REMOVE": {
-              InstallationManagerConsole managerConsole = new InstallationManagerConsole(pluginDir);
-              managerConsole.Remove(args.Skip(1));
-              break;
-            }
-          default: PrintUsage(); break;
-        }
-      }
-    }
-
-    private static void PrintUsage() {
-      Console.WriteLine("Usage: HeuristicLab.exe <command> <args>");
-      Console.WriteLine("Commands:");
-      Console.WriteLine("\tstart <application name>");
-      Console.WriteLine("\tshow <plugin name(s)>");
-      Console.WriteLine("\tupdate <plugin name(s)>");
-      Console.WriteLine("\tremove <plugin name(s)>");
-      Console.WriteLine("\tinstall <plugin name(s)>");
-    }
-
-    private static void ShowErrorMessageBox(Exception ex) {
-      MessageBox.Show(null,
-        BuildErrorMessage(ex),
-        "Error - " + ex.GetType().Name,
-        MessageBoxButtons.OK,
-        MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
-    }
-
-    private static string BuildErrorMessage(Exception ex) {
-      StringBuilder sb = new StringBuilder();
-      sb.Append("Sorry, but something went wrong!\n\n" + ex.Message + "\n\n" + ex.StackTrace);
-
-      while (ex.InnerException != null) {
-        ex = ex.InnerException;
-        sb.Append("\n\n-----\n\n" + ex.Message + "\n\n" + ex.StackTrace);
-      }
-      return sb.ToString();
+      HeuristicLab.PluginInfrastructure.Main.Run(args);
     }
   }
 }
