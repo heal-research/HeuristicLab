9f4afacc8d129538f7c2fc2fd8b00c909b3929f5
kgrading <>
2010-01-06 14:59:55 +0000

    Initial import of the LINQDataAccessPlugin (#830)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@2611 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ sources/HeuristicLab.Hive.Server.LINQDataAccess/3.2/LINQDataAccessPlugin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HeuristicLab.PluginInfrastructure;
+
+namespace HeuristicLab.Hive.Server.LINQDataAccess {
+  [Plugin("HeuristicLab.Hive.Server.LINQDataAccess-3.2")]
+  [PluginFile("HeuristicLab.Hive.Server.LINQDataAcces-3.2.dll", PluginFileType.Assembly)]
+  [PluginDependency("HeuristicLab.Core-3.2")]
+  public class LINQDataAccessPlugin: PluginBase {
+  }
+}
