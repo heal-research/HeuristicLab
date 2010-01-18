1fe9c1c0fd0bab0b9d911794dbed462bafda674e
Michael Kommenda <michael.kommenda@fh-ooe.at>
2010-01-18 15:19:21 +0000

    adapted !CopyAssemblies.cmd and !CustomPostBuildTemplate_!UpdateLocalInstallation.cmd (ticket #799)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@2638 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab/CustomPostBuildTemplate_UpdateLocalInstallation.cmd
+++ sources/HeuristicLab/CustomPostBuildTemplate_UpdateLocalInstallation.cmd
@@ -5,19 +5,19 @@ copy HeuristicLab.Console.exe "%target%"
 copy HeuristicLab.exe.config "%target%"
 copy HeuristicLab.PluginInfrastructure.dll "%target%"
 
-FOR /F "skip=1 tokens=1-2 delims=: usebackq" %%G IN ("%ProjectDir%\Files.txt") DO copy "%SolutionDir%\%%G\%Outdir%\%%H" "%target%"
+FOR /F "skip=1 tokens=1-2 delims=: usebackq" %%G IN ("%ProjectDir%\Files.txt") DO copy "%SolutionDir%\%%G\%Outdir%\%%H" "%target%" >nul
 
 
 echo "Platform: %Platform%, architecture: %PROCESSOR_ARCHITECTURE%"
 if "%Platform%" == "x86" (   
-  FOR /F "skip=1 tokens=* usebackq" %%G IN ("%ProjectDir%\Files.x86.txt") DO copy "%SolutionDir%\%%G" "%target%"
+  FOR /F "skip=1 tokens=* usebackq" %%G IN ("%ProjectDir%\Files.x86.txt") DO copy "%SolutionDir%\%%G" "%target%" >nul
 ) else if "%Platform%" == "x64" ( 
-  FOR /F "skip=1 tokens=* usebackq" %%G IN ("%ProjectDir%\Files.x64.txt") DO copy "%SolutionDir%\%%G" "%target%"
+  FOR /F "skip=1 tokens=* usebackq" %%G IN ("%ProjectDir%\Files.x64.txt") DO copy "%SolutionDir%\%%G" "%target%" >nul
 ) else if "%Platform%" == "AnyCPU" (
   if "%PROCESSOR_ARCHITECTURE%" == "x64" (
-  FOR /F "skip=1 tokens=* usebackq" %%G IN ("%ProjectDir%\Files.x64.txt") DO copy "%SolutionDir%\%%G" "%target%"
+  FOR /F "skip=1 tokens=* usebackq" %%G IN ("%ProjectDir%\Files.x64.txt") DO copy "%SolutionDir%\%%G" "%target%" >nul
   ) else if "%PROCESSOR_ARCHITECTURE%" == "x86" (
-  FOR /F "skip=1 tokens=* usebackq" %%G IN ("%ProjectDir%\Files.x86.txt") DO copy "%SolutionDir%\%%G" "%target%"
+  FOR /F "skip=1 tokens=* usebackq" %%G IN ("%ProjectDir%\Files.x86.txt") DO copy "%SolutionDir%\%%G" "%target%" >nul
   ) else (
     echo "ERROR: unknown architecture: "%PROCESSOR_ARCHITECTURE%"
   ) 
