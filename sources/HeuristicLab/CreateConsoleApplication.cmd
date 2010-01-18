d5f0c4d5d8a13cbdeab07b971172a3c90f626be3
Michael Kommenda <michael.kommenda@fh-ooe.at>
2010-01-18 15:37:33 +0000

    update create console application for x64 systems (ticket #799)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@2639 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab/CreateConsoleApplication.cmd
+++ sources/HeuristicLab/CreateConsoleApplication.cmd
@@ -1,5 +1,24 @@
 copy HeuristicLab.exe HeuristicLab.Console.exe
-@call "c:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
-@editbin.exe /subsystem:console HeuristicLab.Console.exe
+
+echo "Platform: %Platform%, architecture: %PROCESSOR_ARCHITECTURE%"
+if "%Platform%" == "x86" (   
+ @call "c:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
+ @call "c:\Program Files\Microsoft Visual Studio 9.0\VC\bin\editbin.exe" /subsystem:console HeuristicLab.Console.exe
+) else if "%Platform%" == "x64" ( 
+  @call "c:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x64
+  @call "c:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\bin\editbin.exe" /subsystem:console HeuristicLab.Console.exe
+) else if "%Platform%" == "AnyCPU" (
+ if "%PROCESSOR_ARCHITECTURE%" == "x64" (
+   @call "c:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x64
+   @call "c:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\bin\editbin.exe" /subsystem:console HeuristicLab.Console.exe
+  ) else if "%PROCESSOR_ARCHITECTURE%" == "x86" (
+   @call "c:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
+   @call "c:\Program Files\Microsoft Visual Studio 9.0\VC\bin\editbin.exe" /subsystem:console HeuristicLab.Console.exe
+  ) else (
+      echo "ERROR: unknown architecture: "%PROCESSOR_ARCHITECTURE%"
+  ) 
+) else (
+  echo "ERROR: unknown platform: %Platform%"
+)
 
 echo "Done creating console application"
