63b95f24d686c7bf1f3546974b7bce01bfc65d98
Erik Pitzer <erik.pitzer@fh-hagenberg.at>
2010-01-20 15:00:08 +0000

    Add SharpDevelop libraries to support syntax highlighting and code completion (#842)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@2658 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ sources/HeuristicLab.ExtLibs/HeuristicLab.SharpDevelop/3.1.1.5327/HeuristicLabSharpDevelopPlugin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HeuristicLab.PluginInfrastructure;
+
+namespace HeuristicLab.SharpDevelop {
+
+  [Plugin("HeuristicLab.SharpDevelop-3.1.1.5327")]
+  [PluginFile("ICSharpCode.TextEditor.dll", PluginFileType.Assembly)]
+  [PluginFile("ICSharpCode.SharpDevelop.Dom.dll", PluginFileType.Assembly)]
+  [PluginFile("ICSharpCode.NRefactory.dll", PluginFileType.Assembly)]
+  [PluginFile("Mono.Cecil.dll", PluginFileType.Assembly)]
+  [PluginFile("lgpl.txt", PluginFileType.License)]
+  [PluginFile("MIT_X11.txt", PluginFileType.License)]
+  [PluginDependency("HeuristicLab.log4net-1.2.10.0")]
+  public class HeuristicLabSharpDevelopPlugin : PluginBase {
+  }
+}
