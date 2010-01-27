heal-research/HeuristicLab

commit 1ba4194a2f31211f3f81032c1a869b503083a42b

Fixed caching and resolve event handler in plugin infrastructure to fix the bug that the `SqlServerCompact` assembly is not found when opening a db connection. #854 (System.Data.SqlServerCe assembly is not loaded correctly)
git-svn-id: https://src.heuristiclab.com/svn/core/trunk@2690 2abd9481-f8db-48e9-bd25-06bc13291c1b

sources/HeuristicLab.PluginInfrastructure/ApplicationManager.cs
@@ -74,7 +74,6 @@ namespace HeuristicLab.PluginInfrastructure {
       : base() {
       loadedAssemblies = new Dictionary<string, Assembly>();
       loadedPlugins = new List<IPlugin>();
-      // needed for the special case when assemblies are loaded dynamically via LoadAssemblies()
       AppDomain.CurrentDomain.AssemblyResolve += (sender, args) => {
         if (loadedAssemblies.ContainsKey(args.Name)) {
           return loadedAssemblies[args.Name];
@@ -102,9 +101,9 @@ namespace HeuristicLab.PluginInfrastructure {
     private void LoadPlugins(IEnumerable<PluginDescription> plugins) {
       // load all loadable plugins (all dependencies available) into the execution context
       foreach (var desc in PluginDescriptionIterator.IterateDependenciesBottomUp(plugins.Where(x => x.PluginState != PluginState.Disabled))) {
-        foreach (AssemblyName assemblyName in desc.AssemblyNames) {
-          var asm = Assembly.Load(assemblyName);
-
+        foreach (string fileName in desc.AssemblyLocations) {
+          var asm = Assembly.LoadFrom(fileName);
+          RegisterLoadedAssembly(asm);
           // instantiate and load all plugins in this assembly
           foreach (var plugin in GetInstances<IPlugin>(asm)) {
             plugin.OnLoad();
@@ -212,7 +211,7 @@ namespace HeuristicLab.PluginInfrastructure {
     internal static IEnumerable<Type> GetTypes(Type type, IPluginDescription pluginDescription, bool onlyInstantiable) {
       PluginDescription pluginDesc = (PluginDescription)pluginDescription;
       return from asm in AppDomain.CurrentDomain.GetAssemblies()
-             where pluginDesc.AssemblyNames.Any(asmName => asmName.FullName.Equals(asm.GetName().FullName))
+             where pluginDesc.AssemblyLocations.Any(location => location.Equals(Path.GetFullPath(asm.Location), StringComparison.CurrentCultureIgnoreCase))
              from t in GetTypes(type, asm, onlyInstantiable)
              select t;
     }
@@ -282,7 +281,7 @@ namespace HeuristicLab.PluginInfrastructure {
     /// <returns>The description of the plugin that declares the given type or null if the type has not been declared by a known plugin.</returns>
     public IPluginDescription GetDeclaringPlugin(Type type) {
       foreach (PluginDescription info in Plugins) {
-        if (info.AssemblyNames.Contains(type.Assembly.GetName())) return info;
+        if (info.AssemblyLocations.Contains(Path.GetFullPath(type.Assembly.Location))) return info;
       }
       return null;
     }

sources/HeuristicLab.PluginInfrastructure/Advanced/InstallationManager.cs
@@ -126,8 +126,8 @@ namespace HeuristicLab.PluginInfrastructure.Advanced {
           } else {
             // or there was a problem loading the assemblies
             builder.AppendLine("There was a problem while loading assemblies: ");
-            foreach (AssemblyName assembly in desc.AssemblyNames) {
-              builder.AppendLine(assembly.FullName);
+            foreach (string assemblyLocation in desc.AssemblyLocations) {
+              builder.AppendLine(assemblyLocation + ": " + AssemblyName.GetAssemblyName(assemblyLocation).FullName);
             }
             return builder.ToString();
           }

sources/HeuristicLab.PluginInfrastructure/Manager/PluginDescription.cs
@@ -105,16 +105,12 @@ namespace HeuristicLab.PluginInfrastructure.Manager {
       dependencies.Add(dependency);
     }
 
-    private List<AssemblyName> assemblyNames = new List<AssemblyName>();
+    
     /// <summary>
-    /// Gets the names of the assemblies that belong to this plugin.
+    /// Gets the locations (file names) of the assemblies that belong to this plugin.
     /// </summary>
-    public IEnumerable<AssemblyName> AssemblyNames {
-      get { return assemblyNames; }
-    }
-
-    internal void AddAssemblyNames(IEnumerable<AssemblyName> assemblyNames) {
-      this.assemblyNames.AddRange(assemblyNames);
+    public IEnumerable<string> AssemblyLocations {
+      get { return Files.Where(f => f.Type == PluginFileType.Assembly).Select(f => f.Name); }
     }
 
     internal PluginDescription() {

sources/HeuristicLab.PluginInfrastructure/Manager/PluginValidator.cs
@@ -67,8 +67,12 @@ namespace HeuristicLab.PluginInfrastructure.Manager {
       AppDomain.CurrentDomain.ReflectionOnlyAssemblyResolve += ReflectionOnlyAssemblyResolveEventHandler;
     }
 
+    private Dictionary<string, Assembly> reflectionOnlyAssemblies = new Dictionary<string, Assembly>();
     private Assembly ReflectionOnlyAssemblyResolveEventHandler(object sender, ResolveEventArgs args) {
-      return Assembly.ReflectionOnlyLoad(args.Name);
+      if (reflectionOnlyAssemblies.ContainsKey(args.Name))
+        return reflectionOnlyAssemblies[args.Name];
+      else
+        return Assembly.ReflectionOnlyLoad(args.Name);
     }
 
 
@@ -146,7 +150,7 @@ namespace HeuristicLab.PluginInfrastructure.Manager {
              select (IApplication)Activator.CreateInstance(t);
     }
 
-    private static IEnumerable<Assembly> ReflectionOnlyLoadDlls(string baseDir) {
+    private IEnumerable<Assembly> ReflectionOnlyLoadDlls(string baseDir) {
       List<Assembly> assemblies = new List<Assembly>();
       // recursively load .dll files in subdirectories
       foreach (string dirName in Directory.GetDirectories(baseDir)) {
@@ -155,7 +159,9 @@ namespace HeuristicLab.PluginInfrastructure.Manager {
       // try to load each .dll file in the plugin directory into the reflection only context
       foreach (string filename in Directory.GetFiles(baseDir, "*.dll")) {
         try {
-          assemblies.Add(Assembly.ReflectionOnlyLoadFrom(filename));
+          Assembly asm = Assembly.ReflectionOnlyLoadFrom(filename);
+          RegisterLoadedAssembly(asm);
+          assemblies.Add(asm);
         }
         catch (BadImageFormatException) { } // just ignore the case that the .dll file is not a CLR assembly (e.g. a native dll)
         catch (FileLoadException) { }
@@ -171,8 +177,15 @@ namespace HeuristicLab.PluginInfrastructure.Manager {
     private void CheckPluginAssemblies(IEnumerable<PluginDescription> pluginDescriptions) {
       foreach (var desc in pluginDescriptions.Where(x => x.PluginState != PluginState.Disabled)) {
         try {
-          foreach (var asmName in desc.AssemblyNames) {
-            Assembly.ReflectionOnlyLoad(asmName.FullName);
+          foreach (var asmLocation in desc.AssemblyLocations) {
+            // the assembly must have been loaded in ReflectionOnlyDlls
+            // so we simply determine the name of the assembly and try to find it in the cache of loaded assemblies
+            var asmName = AssemblyName.GetAssemblyName(asmLocation);
+
+            if (!reflectionOnlyAssemblies.ContainsKey(asmName.FullName)) {
+              desc.Disable();
+              break; // as soon as one assembly is not available disable the plugin and check the next plugin description
+            }
           }
         }
         catch (BadImageFormatException) {
@@ -236,7 +249,6 @@ namespace HeuristicLab.PluginInfrastructure.Manager {
     private PluginDescription GetPluginDescription(Type pluginType) {
       // get all attributes of that type
       IList<CustomAttributeData> attributes = CustomAttributeData.GetCustomAttributes(pluginType);
-      List<AssemblyName> pluginAssemblyNames = new List<AssemblyName>();
       List<string> pluginDependencies = new List<string>();
       List<PluginFile> pluginFiles = new List<PluginFile>();
       string pluginName = null;
@@ -254,9 +266,6 @@ namespace HeuristicLab.PluginInfrastructure.Manager {
           string pluginFileName = (string)attributeData.ConstructorArguments[0].Value;
           PluginFileType fileType = (PluginFileType)attributeData.ConstructorArguments[1].Value;
           pluginFiles.Add(new PluginFile(Path.GetFullPath(Path.Combine(PluginDir, pluginFileName)), fileType));
-          if (fileType == PluginFileType.Assembly) {
-            pluginAssemblyNames.Add(AssemblyName.GetAssemblyName(Path.GetFullPath(Path.Combine(PluginDir, pluginFileName))));
-          }
         }
       }
 
@@ -266,16 +275,15 @@ namespace HeuristicLab.PluginInfrastructure.Manager {
 
       // minimal sanity check of the attribute values
       if (!string.IsNullOrEmpty(pluginName) &&
-          pluginFiles.Count > 0 &&
-          pluginAssemblyNames.Count > 0 &&
-          buildDates.Count() == 1) {
+          pluginFiles.Count > 0 &&                                   // at least on file
+          pluginFiles.Any(f => f.Type == PluginFileType.Assembly) && // at least on assembly
+          buildDates.Count() == 1) {                                 // build date must be declared
         // create a temporary PluginDescription that contains the attribute values
         PluginDescription info = new PluginDescription();
         info.Name = pluginName;
         info.Description = pluginDescription;
         info.Version = pluginType.Assembly.GetName().Version;
         info.BuildDate = DateTime.Parse(buildDates.Single(), System.Globalization.CultureInfo.InvariantCulture);
-        info.AddAssemblyNames(pluginAssemblyNames);
         info.AddFiles(pluginFiles);
 
         this.pluginDependencies[info] = pluginDependencies;
@@ -348,8 +356,9 @@ namespace HeuristicLab.PluginInfrastructure.Manager {
       foreach (var desc in PluginDescriptionIterator.IterateDependenciesBottomUp(pluginDescriptions
                                                                                 .Where(x => x.PluginState != PluginState.Disabled))) {
         List<Type> types = new List<Type>();
-        foreach (AssemblyName assemblyName in desc.AssemblyNames) {
-          var asm = Assembly.Load(assemblyName);
+        foreach (string assemblyLocation in desc.AssemblyLocations) {
+          // now load the assemblies into the execution context
+          var asm = Assembly.LoadFrom(assemblyLocation);
           foreach (Type t in asm.GetTypes()) {
             if (typeof(IPlugin).IsAssignableFrom(t)) {
               types.Add(t);
@@ -390,6 +399,12 @@ namespace HeuristicLab.PluginInfrastructure.Manager {
     private static bool FileLiesInDirectory(string dir, string fileName) {
       var basePath = Path.GetFullPath(dir);
       return Path.GetFullPath(fileName).StartsWith(basePath);
+    }
+
+    // register assembly in the assembly cache for the ReflectionOnlyAssemblyResolveEvent
+    private void RegisterLoadedAssembly(Assembly asm) {
+      reflectionOnlyAssemblies.Add(asm.FullName, asm);
+      reflectionOnlyAssemblies.Add(asm.GetName().Name, asm); // add short name
     }
 
     internal void OnPluginLoaded(PluginInfrastructureEventArgs e) {

commit 3ed9944266c029e9543d06cac257f9fe547c556f

Fixed a bug introduced with r2690 #854
git-svn-id: https://src.heuristiclab.com/svn/core/trunk@2693 2abd9481-f8db-48e9-bd25-06bc13291c1b

sources/HeuristicLab.PluginInfrastructure/ApplicationManager.cs
@@ -211,6 +211,7 @@ namespace HeuristicLab.PluginInfrastructure {
     internal static IEnumerable<Type> GetTypes(Type type, IPluginDescription pluginDescription, bool onlyInstantiable) {
       PluginDescription pluginDesc = (PluginDescription)pluginDescription;
       return from asm in AppDomain.CurrentDomain.GetAssemblies()
+             where !string.IsNullOrEmpty(asm.Location) // ignore dynamically generated assemblies
              where pluginDesc.AssemblyLocations.Any(location => location.Equals(Path.GetFullPath(asm.Location), StringComparison.CurrentCultureIgnoreCase))
              from t in GetTypes(type, asm, onlyInstantiable)
              select t;