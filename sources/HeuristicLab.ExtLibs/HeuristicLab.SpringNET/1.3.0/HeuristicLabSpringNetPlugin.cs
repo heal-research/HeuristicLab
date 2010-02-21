dfc9befb9183180ab65ba630df035ab271c96b9a
kgrading <>
2010-02-21 17:53:09 +0000

    Added Spring.NET in v 1.3.0 (#884)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@2847 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ sources/HeuristicLab.ExtLibs/HeuristicLab.SpringNET/1.3.0/HeuristicLabSpringNetPlugin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HeuristicLab.PluginInfrastructure;
+
+namespace HeuristicLab.SpringNET {
+  [Plugin("HeuristicLab.SpringNET", "1.3.0")]
+  [PluginFile("HeuristicLab.SpringNET-1.3.0.dll", PluginFileType.Assembly)]
+  [PluginFile("Common.Logging.dll", PluginFileType.Assembly)]
+  [PluginFile("Spring.Services.dll", PluginFileType.Assembly)]
+  [PluginFile("Spring.Core.dll", PluginFileType.Assembly)]
+  [PluginFile("Spring.Aop.dll", PluginFileType.Assembly)]  
+  public class HeuristicLabSpringNetPlugin: PluginBase {
+
+  }
+}
