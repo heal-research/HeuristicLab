6ca5e503a558790bb346a91cdfbb452c022c09fc
kgrading <>
2010-02-21 20:21:20 +0000

    fixed plugin setting. plugin now valid (#830)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@2850 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Hive.Server.LINQDataAccess/3.2/LINQDataAccessPlugin.cs
+++ sources/HeuristicLab.Hive.Server.LINQDataAccess/3.2/LINQDataAccessPlugin.cs
@@ -6,7 +6,7 @@ using HeuristicLab.PluginInfrastructure;
 
 namespace HeuristicLab.Hive.Server.LINQDataAccess {
   [Plugin("HeuristicLab.Hive.Server.LINQDataAccess-3.2")]
-  [PluginFile("HeuristicLab.Hive.Server.LINQDataAcces-3.2.dll", PluginFileType.Assembly)]
+  [PluginFile("HeuristicLab.Hive.Server.LINQDataAccess-3.2.dll", PluginFileType.Assembly)]
   [PluginDependency("HeuristicLab.Core-3.2")]
   public class LINQDataAccessPlugin: PluginBase {
   }
