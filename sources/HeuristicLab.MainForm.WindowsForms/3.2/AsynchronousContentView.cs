e9e585e064a4a9ae7574754d8da5d45f5b1c8c53
Michael Kommenda <michael.kommenda@fh-ooe.at>
2010-03-28 15:53:41 +0000

    added !AsynchronousContentView (ticket #953)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@3227 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ sources/HeuristicLab.MainForm.WindowsForms/3.2/AsynchronousContentView.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace HeuristicLab.MainForm.WindowsForms {
+  public partial class AsynchronousContentView : ContentView {
+    public AsynchronousContentView() {
+      InitializeComponent();
+    }
+
+    public AsynchronousContentView(object content)
+      : this() {
+      this.Content = content;
+    }
+
+    /// <summary>
+    /// Asynchron call of GUI updating.
+    /// </summary>
+    /// <param name="method">The delegate to invoke.</param>
+    protected new void Invoke(Delegate method) {
+      IAsyncResult res = base.BeginInvoke(method);
+      ThreadPool.RegisterWaitForSingleObject(res.AsyncWaitHandle,
+        new WaitOrTimerCallback((x, b) => { EndInvoke(res); }),
+        null, -1, true);
+    }
+    /// <summary>
+    /// Asynchron call of GUI updating.
+    /// </summary>
+    /// <param name="method">The delegate to invoke.</param>
+    /// <param name="args">The invoke arguments.</param>
+    protected new void Invoke(Delegate method, params object[] args) {
+      IAsyncResult res = base.BeginInvoke(method, args);
+      ThreadPool.RegisterWaitForSingleObject(res.AsyncWaitHandle,
+        new WaitOrTimerCallback((x, b) => { EndInvoke(res); }),
+        null, -1, true);
+    }
+  }
+}
