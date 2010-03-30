63015957e0a2ea60468d55ad7d1ee229d4bc8f2a
Stefan Vonolfen <stefan.vonolfen@gmail.com>
2010-03-30 13:13:38 +0000

    Added check to ProgrammableOperator if the ApplicationManager exists so it can be used outside of HL (#842)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@3234 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Operators.Programmable/3.3/ProgrammableOperator.cs
+++ sources/HeuristicLab.Operators.Programmable/3.3/ProgrammableOperator.cs
@@ -196,20 +196,24 @@ namespace HeuristicLab.Operators.Programmable {
     private static Dictionary<string, List<Assembly>> GroupAssemblies(IEnumerable<Assembly> assemblies) {
       var plugins = new Dictionary<string, List<Assembly>>();
       var locationTable = assemblies.ToDictionary(a => a.Location, a => a);
-      foreach (var plugin in ApplicationManager.Manager.Plugins) {
-        var aList = new List<Assembly>();
-        foreach (var aName in from file in plugin.Files
-                              where file.Type == PluginFileType.Assembly
-                              select file.Name) {
-          Assembly a;
-          locationTable.TryGetValue(aName, out a);
-          if (a != null) {
-            aList.Add(a);
-            locationTable.Remove(aName);
+
+      if (ApplicationManager.Manager != null) {
+        foreach (var plugin in ApplicationManager.Manager.Plugins) {
+          var aList = new List<Assembly>();
+          foreach (var aName in from file in plugin.Files
+                                where file.Type == PluginFileType.Assembly
+                                select file.Name) {
+            Assembly a;
+            locationTable.TryGetValue(aName, out a);
+            if (a != null) {
+              aList.Add(a);
+              locationTable.Remove(aName);
+            }
           }
+          plugins[plugin.Name] = aList;
         }
-        plugins[plugin.Name] = aList;
       }
+
       plugins["other"] = locationTable.Values.ToList();
       return plugins;
     }
