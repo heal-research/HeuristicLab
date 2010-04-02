5b565e70ee320a0e9cdc6aea6cce083129de014e
Gabriel Kronberger <gabriel.kronberger@fh-hagenberg.at>
2010-04-02 14:44:34 +0000

    Moved general set of symbols and evaluator into Encodings.SymbolicExpressionTreeEncoding. #937 (Data types and operators for symbolic expression tree encoding)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@3256 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ sources/HeuristicLab.Encodings.SymbolicExpressionTreeEncoding/3.3/GeneralSymbolicExpressionTreeEvaluator.cs
@@ -0,0 +1,52 @@
+﻿#region License Information
+/* HeuristicLab
+ * Copyright (C) 2002-2010 Heuristic and Evolutionary Algorithms Laboratory (HEAL)
+ *
+ * This file is part of HeuristicLab.
+ *
+ * HeuristicLab is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HeuristicLab is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HeuristicLab. If not, see <http://www.gnu.org/licenses/>.
+ */
+#endregion
+
+using System;
+using HeuristicLab.Persistence.Default.CompositeSerializers.Storable;
+using HeuristicLab.Core;
+using System.Collections.Generic;
+using HeuristicLab.Encodings.SymbolicExpressionTreeEncoding;
+using HeuristicLab.Encodings.SymbolicExpressionTreeEncoding.GeneralSymbols;
+
+namespace HeuristicLab.Encodings.SymbolicExpressionTreeEncoding {
+  /// <summary>
+  /// Evaluates FunctionTrees recursively by interpretation of the function symbols in each node.
+  /// Simple unoptimized code, general symbolic expressions only.
+  /// Not thread-safe!
+  /// </summary>
+  [StorableClass]
+  [Item("GeneralSymbolicExpressionTreeEvaluator", "Default evaluator for symbolic expression trees.")]
+  public class GeneralSymbolicExpressionTreeEvaluator : Item {
+    public virtual double Evaluate(SymbolicExpressionTreeNode node) {
+      if (node.Symbol is Addition) {
+        return Evaluate(node.SubTrees[0]) + Evaluate(node.SubTrees[1]);
+      } else if (node.Symbol is Subtraction) {
+        return Evaluate(node.SubTrees[0]) - Evaluate(node.SubTrees[1]);
+      } else if (node.Symbol is Multiplication) {
+        return Evaluate(node.SubTrees[0]) * Evaluate(node.SubTrees[1]);
+      } else if (node.Symbol is Division) {
+        return Evaluate(node.SubTrees[0]) / Evaluate(node.SubTrees[1]);
+      } else {
+        throw new NotSupportedException("Tree contains unknown symbol: " + node.Symbol.Name);
+      }
+    }
+  }
+}
---
+++ sources/HeuristicLab.Encodings.SymbolicExpressionTreeEncoding/3.3/SymbolicExpressionTreeTerminalNode.cs
@@ -0,0 +1,54 @@
+#region License Information
+/* HeuristicLab
+ * Copyright (C) 2002-2008 Heuristic and Evolutionary Algorithms Laboratory (HEAL)
+ *
+ * This file is part of HeuristicLab.
+ *
+ * HeuristicLab is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HeuristicLab is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HeuristicLab. If not, see <http://www.gnu.org/licenses/>.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using HeuristicLab.Core;
+using System.Xml;
+using HeuristicLab.Persistence.Default.CompositeSerializers.Storable;
+using HeuristicLab.Data;
+
+namespace HeuristicLab.Encodings.SymbolicExpressionTreeEncoding {
+  public abstract class SymbolicExpressionTreeTerminalNode : SymbolicExpressionTreeNode {
+    private static List<SymbolicExpressionTreeNode> emptyList = new List<SymbolicExpressionTreeNode>();
+
+    protected SymbolicExpressionTreeTerminalNode(Symbol symbol) : base(symbol) { }
+    protected SymbolicExpressionTreeTerminalNode(SymbolicExpressionTreeTerminalNode original)
+      : base(original) {
+    }
+
+    protected internal override void AddSubTree(SymbolicExpressionTreeNode tree) {
+      throw new NotSupportedException();
+    }
+    protected internal override void InsertSubTree(int index, SymbolicExpressionTreeNode tree) {
+      throw new NotSupportedException();
+    }
+    protected internal override void RemoveSubTree(int index) {
+      throw new NotSupportedException();
+    }
+    public override IList<SymbolicExpressionTreeNode> SubTrees {
+      get {
+        return SymbolicExpressionTreeTerminalNode.emptyList;
+      }
+    }
+  }
+}
