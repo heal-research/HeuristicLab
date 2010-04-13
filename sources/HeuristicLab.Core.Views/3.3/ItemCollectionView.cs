heal-research/HeuristicLab

commit 2a55fa862f50387862d7ad4ef850cb26b4beb077

Implemented reviewers' comments (#893).
git-svn-id: https://src.heuristiclab.com/svn/core/trunk@3306 2abd9481-f8db-48e9-bd25-06bc13291c1b

sources/HeuristicLab.Core.Views/3.3/ItemCollectionView.cs
@@ -21,6 +21,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 using HeuristicLab.Collections;
 using HeuristicLab.MainForm;
@@ -96,32 +97,32 @@ namespace HeuristicLab.Core.Views {
       }
     }
     protected virtual ListViewItem CreateListViewItem(T item) {
-      if (!itemsListView.SmallImageList.Images.ContainsKey(item.GetType().FullName))
-        itemsListView.SmallImageList.Images.Add(item.GetType().FullName, item.ItemImage);
-
       ListViewItem listViewItem = new ListViewItem();
       listViewItem.Text = item.ToString();
       listViewItem.ToolTipText = item.ItemName + ": " + item.ItemDescription;
-      listViewItem.ImageIndex = itemsListView.SmallImageList.Images.IndexOfKey(item.GetType().FullName);
       listViewItem.Tag = item;
+      SetListViewItemImage(listViewItem);
       return listViewItem;
     }
     protected virtual void AddListViewItem(ListViewItem listViewItem) {
       itemsListView.Items.Add(listViewItem);
+      ((T)listViewItem.Tag).ItemImageChanged += new EventHandler(Item_ItemImageChanged);
       ((T)listViewItem.Tag).ToStringChanged += new EventHandler(Item_ToStringChanged);
       sortAscendingButton.Enabled = itemsListView.Items.Count > 0;
       sortDescendingButton.Enabled = itemsListView.Items.Count > 0;
     }
     protected virtual void RemoveListViewItem(ListViewItem listViewItem) {
+      ((T)listViewItem.Tag).ItemImageChanged -= new EventHandler(Item_ItemImageChanged);
       ((T)listViewItem.Tag).ToStringChanged -= new EventHandler(Item_ToStringChanged);
       listViewItem.Remove();
       sortAscendingButton.Enabled = itemsListView.Items.Count > 0;
       sortDescendingButton.Enabled = itemsListView.Items.Count > 0;
     }
     protected virtual void UpdateListViewItem(ListViewItem listViewItem) {
-      if (!listViewItem.Text.Equals(listViewItem.Tag.ToString())) {
+      if (!listViewItem.Text.Equals(listViewItem.Tag.ToString()))
         listViewItem.Text = listViewItem.Tag.ToString();
-      }
+      if (itemsListView.SmallImageList.Images[listViewItem.ImageIndex] != ((T)listViewItem.Tag).ItemImage)
+        SetListViewItemImage(listViewItem);
     }
     protected virtual IEnumerable<ListViewItem> GetListViewItemsForItem(T item) {
       foreach (ListViewItem listViewItem in itemsListView.Items) {
@@ -254,6 +255,15 @@ namespace HeuristicLab.Core.Views {
     #endregion
 
     #region Item Events
+    protected virtual void Item_ItemImageChanged(object sender, EventArgs e) {
+      if (InvokeRequired)
+        Invoke(new EventHandler(Item_ItemImageChanged), sender, e);
+      else {
+        T item = (T)sender;
+        foreach (ListViewItem listViewItem in GetListViewItemsForItem(item))
+          UpdateListViewItem(listViewItem);
+      }
+    }
     protected virtual void Item_ToStringChanged(object sender, EventArgs e) {
       if (InvokeRequired)
         Invoke(new EventHandler(Item_ToStringChanged), sender, e);
@@ -266,6 +276,14 @@ namespace HeuristicLab.Core.Views {
     #endregion
 
     #region Helpers
+    protected virtual void SetListViewItemImage(ListViewItem listViewItem) {
+      T item = (T)listViewItem.Tag;
+      int i = 0;
+      while ((i < itemsListView.SmallImageList.Images.Count) && (item.ItemImage != itemsListView.SmallImageList.Images[i]))
+        i++;
+      if (i == itemsListView.SmallImageList.Images.Count) itemsListView.SmallImageList.Images.Add(item.ItemImage);
+      listViewItem.ImageIndex = i;
+    }
     protected virtual void SortItemsListView(SortOrder sortOrder) {
       itemsListView.Sorting = SortOrder.None;
       itemsListView.Sorting = sortOrder;

sources/HeuristicLab.Core.Views/3.3/ItemListView.cs
@@ -118,36 +118,34 @@ namespace HeuristicLab.Core.Views {
       }
     }
     protected virtual ListViewItem CreateListViewItem(T item) {
-      if (!itemsListView.SmallImageList.Images.ContainsKey(item.GetType().FullName))
-        itemsListView.SmallImageList.Images.Add(item.GetType().FullName, item.ItemImage);
-
       ListViewItem listViewItem = new ListViewItem();
       listViewItem.Text = item.ToString();
       listViewItem.ToolTipText = item.ItemName + ": " + item.ItemDescription;
-      listViewItem.ImageIndex = itemsListView.SmallImageList.Images.IndexOfKey(item.GetType().FullName);
       listViewItem.Tag = item;
+      SetListViewItemImage(listViewItem);
       return listViewItem;
     }
     protected virtual void AddListViewItem(ListViewItem listViewItem) {
       itemsListView.Items.Add(listViewItem);
+      ((T)listViewItem.Tag).ItemImageChanged += new EventHandler(Item_ItemImageChanged);
       ((T)listViewItem.Tag).ToStringChanged += new EventHandler(Item_ToStringChanged);
     }
     protected virtual void InsertListViewItem(int index, ListViewItem listViewItem) {
       itemsListView.Items.Insert(index, listViewItem);
+      ((T)listViewItem.Tag).ItemImageChanged += new EventHandler(Item_ItemImageChanged);
       ((T)listViewItem.Tag).ToStringChanged += new EventHandler(Item_ToStringChanged);
     }
     protected virtual void RemoveListViewItem(ListViewItem listViewItem) {
+      ((T)listViewItem.Tag).ItemImageChanged -= new EventHandler(Item_ItemImageChanged);
       ((T)listViewItem.Tag).ToStringChanged -= new EventHandler(Item_ToStringChanged);
       listViewItem.Remove();
     }
     protected virtual void UpdateListViewItem(ListViewItem listViewItem) {
       T item = (T)listViewItem.Tag;
-      if (!itemsListView.SmallImageList.Images.ContainsKey(item.GetType().FullName))
-        itemsListView.SmallImageList.Images.Add(item.GetType().FullName, item.ItemImage);
-
       listViewItem.Text = item.ToString();
       listViewItem.ToolTipText = item.ItemName + ": " + item.ItemDescription;
-      listViewItem.ImageIndex = itemsListView.SmallImageList.Images.IndexOfKey(item.GetType().FullName);
+      if (itemsListView.SmallImageList.Images[listViewItem.ImageIndex] != item.ItemImage)
+        SetListViewItemImage(listViewItem);
     }
 
     protected virtual void itemsListView_SelectedIndexChanged(object sender, EventArgs e) {
@@ -333,6 +331,17 @@ namespace HeuristicLab.Core.Views {
     #endregion
 
     #region Item Events
+    protected virtual void Item_ItemImageChanged(object sender, EventArgs e) {
+      if (InvokeRequired)
+        Invoke(new EventHandler(Item_ItemImageChanged), sender, e);
+      else {
+        T item = (T)sender;
+        foreach (ListViewItem listViewItem in itemsListView.Items) {
+          if (((T)listViewItem.Tag) == item)
+            UpdateListViewItem(listViewItem);
+        }
+      }
+    }
     protected virtual void Item_ToStringChanged(object sender, EventArgs e) {
       if (InvokeRequired)
         Invoke(new EventHandler(Item_ToStringChanged), sender, e);
@@ -343,6 +352,17 @@ namespace HeuristicLab.Core.Views {
             UpdateListViewItem(listViewItem);
         }
       }
+    }
+    #endregion
+
+    #region Helpers
+    protected virtual void SetListViewItemImage(ListViewItem listViewItem) {
+      T item = (T)listViewItem.Tag;
+      int i = 0;
+      while ((i < itemsListView.SmallImageList.Images.Count) && (item.ItemImage != itemsListView.SmallImageList.Images[i]))
+        i++;
+      if (i == itemsListView.SmallImageList.Images.Count) itemsListView.SmallImageList.Images.Add(item.ItemImage);
+      listViewItem.ImageIndex = i;
     }
     #endregion
   }

sources/HeuristicLab.Algorithms.GeneticAlgorithm.Views/3.3/GeneticAlgorithmView.Designer.cs
@@ -49,6 +49,8 @@ namespace HeuristicLab.Algorithms.GeneticAlgorithm.Views {
       this.parametersTabPage.SuspendLayout();
       this.problemTabPage.SuspendLayout();
       this.resultsTabPage.SuspendLayout();
+      this.runsTabPage.SuspendLayout();
+      this.problemPanel.SuspendLayout();
       ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
       this.SuspendLayout();
       // 
@@ -137,6 +139,18 @@ namespace HeuristicLab.Algorithms.GeneticAlgorithm.Views {
       this.stopButton.Location = new System.Drawing.Point(60, 552);
       this.toolTip.SetToolTip(this.stopButton, "Stop Algorithm");
       // 
+      // runsTabPage
+      // 
+      this.runsTabPage.Size = new System.Drawing.Size(736, 468);
+      // 
+      // runsView
+      // 
+      this.runsView.Size = new System.Drawing.Size(724, 456);
+      // 
+      // problemPanel
+      // 
+      this.problemPanel.Size = new System.Drawing.Size(724, 426);
+      // 
       // nameTextBox
       // 
       this.errorProvider.SetIconAlignment(this.nameTextBox, System.Windows.Forms.ErrorIconAlignment.MiddleLeft);
@@ -159,6 +173,8 @@ namespace HeuristicLab.Algorithms.GeneticAlgorithm.Views {
       this.parametersTabPage.ResumeLayout(false);
       this.problemTabPage.ResumeLayout(false);
       this.resultsTabPage.ResumeLayout(false);
+      this.runsTabPage.ResumeLayout(false);
+      this.problemPanel.ResumeLayout(false);
       ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
       this.ResumeLayout(false);
       this.PerformLayout();

sources/HeuristicLab.Analysis/3.3/DataTable.cs
@@ -19,6 +19,7 @@
  */
 #endregion
 
+using System.Drawing;
 using HeuristicLab.Core;
 using HeuristicLab.Persistence.Default.CompositeSerializers.Storable;
 
@@ -29,6 +30,10 @@ namespace HeuristicLab.Analysis {
   [Item("DataTable", "A table of data values.")]
   [StorableClass]
   public sealed class DataTable : NamedItem {
+    public override Image ItemImage {
+      get { return HeuristicLab.Common.Resources.VS2008ImageLibrary.Table; }
+    }
+
     [Storable]
     private NamedItemCollection<DataRow> rows;
     public NamedItemCollection<DataRow> Rows {

sources/HeuristicLab.Analysis/3.3/HeuristicLab.Analysis-3.3.csproj
@@ -72,6 +72,7 @@
     <Reference Include="System.Core">
       <RequiredTargetFramework>3.5</RequiredTargetFramework>
     </Reference>
+    <Reference Include="System.Drawing" />
     <Reference Include="System.Xml.Linq">
       <RequiredTargetFramework>3.5</RequiredTargetFramework>
     </Reference>
@@ -102,6 +103,10 @@
     <ProjectReference Include="..\..\HeuristicLab.Collections\3.3\HeuristicLab.Collections-3.3.csproj">
       <Project>{958B43BC-CC5C-4FA2-8628-2B3B01D890B6}</Project>
       <Name>HeuristicLab.Collections-3.3</Name>
+    </ProjectReference>
+    <ProjectReference Include="..\..\HeuristicLab.Common.Resources\3.2\HeuristicLab.Common.Resources-3.2.csproj">
+      <Project>{0E27A536-1C4A-4624-A65E-DC4F4F23E3E1}</Project>
+      <Name>HeuristicLab.Common.Resources-3.2</Name>
     </ProjectReference>
     <ProjectReference Include="..\..\HeuristicLab.Core\3.3\HeuristicLab.Core-3.3.csproj">
       <Project>{C36BD924-A541-4A00-AFA8-41701378DDC5}</Project>

sources/HeuristicLab.Analysis/3.3/HeuristicLabAnalysisPlugin.cs.frame
@@ -28,6 +28,7 @@ namespace HeuristicLab.Analysis {
   [Plugin("HeuristicLab.Analysis", "3.3.0.$WCREV$")]
   [PluginFile("HeuristicLab.Analysis-3.3.dll", PluginFileType.Assembly)]
   [PluginDependency("HeuristicLab.Collections", "3.3")]
+  [PluginDependency("HeuristicLab.Common.Resources", "3.2")]
   [PluginDependency("HeuristicLab.Core", "3.3")]
   [PluginDependency("HeuristicLab.Data", "3.3")]
   [PluginDependency("HeuristicLab.Operators", "3.3")]

sources/HeuristicLab.Core.Views/3.3/CreateParameterDialog.Designer.cs
@@ -147,7 +147,7 @@ namespace HeuristicLab.Core.Views {
       // 
       this.setDataTypeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
       this.setDataTypeButton.Enabled = false;
-      this.setDataTypeButton.Image = HeuristicLab.Common.Resources.VS2008ImageLibrary.Add;
+      this.setDataTypeButton.Image = HeuristicLab.Common.Resources.VS2008ImageLibrary.Edit;
       this.setDataTypeButton.Location = new System.Drawing.Point(506, 420);
       this.setDataTypeButton.Name = "setDataTypeButton";
       this.setDataTypeButton.Size = new System.Drawing.Size(24, 24);

sources/HeuristicLab.Core.Views/3.3/VariableView.Designer.cs
@@ -139,7 +139,7 @@ namespace HeuristicLab.Core.Views {
       // 
       // setValueButton
       // 
-      this.setValueButton.Image = HeuristicLab.Common.Resources.VS2008ImageLibrary.Add;
+      this.setValueButton.Image = HeuristicLab.Common.Resources.VS2008ImageLibrary.Edit;
       this.setValueButton.Location = new System.Drawing.Point(6, 19);
       this.setValueButton.Name = "setValueButton";
       this.setValueButton.Size = new System.Drawing.Size(24, 24);

commit 60629bdb65772ae0f15c754c25499cdc42ec41e7

Fixed bug which was introduced in r3306 (#893).
git-svn-id: https://src.heuristiclab.com/svn/core/trunk@3327 2abd9481-f8db-48e9-bd25-06bc13291c1b

sources/HeuristicLab.Core.Views/3.3/ItemCollectionView.cs
@@ -101,7 +101,8 @@ namespace HeuristicLab.Core.Views {
       listViewItem.Text = item.ToString();
       listViewItem.ToolTipText = item.ItemName + ": " + item.ItemDescription;
       listViewItem.Tag = item;
-      SetListViewItemImage(listViewItem);
+      itemsListView.SmallImageList.Images.Add(item.ItemImage);
+      listViewItem.ImageIndex = itemsListView.SmallImageList.Images.Count - 1;
       return listViewItem;
     }
     protected virtual void AddListViewItem(ListViewItem listViewItem) {
@@ -118,11 +119,15 @@ namespace HeuristicLab.Core.Views {
       sortAscendingButton.Enabled = itemsListView.Items.Count > 0;
       sortDescendingButton.Enabled = itemsListView.Items.Count > 0;
     }
-    protected virtual void UpdateListViewItem(ListViewItem listViewItem) {
+    protected virtual void UpdateListViewItemImage(ListViewItem listViewItem) {
+      int i = listViewItem.ImageIndex;
+      listViewItem.ImageList.Images[i] = ((T)listViewItem.Tag).ItemImage;
+      listViewItem.ImageIndex = -1;
+      listViewItem.ImageIndex = i;
+    }
+    protected virtual void UpdateListViewItemText(ListViewItem listViewItem) {
       if (!listViewItem.Text.Equals(listViewItem.Tag.ToString()))
         listViewItem.Text = listViewItem.Tag.ToString();
-      if (itemsListView.SmallImageList.Images[listViewItem.ImageIndex] != ((T)listViewItem.Tag).ItemImage)
-        SetListViewItemImage(listViewItem);
     }
     protected virtual IEnumerable<ListViewItem> GetListViewItemsForItem(T item) {
       foreach (ListViewItem listViewItem in itemsListView.Items) {
@@ -261,7 +266,7 @@ namespace HeuristicLab.Core.Views {
       else {
         T item = (T)sender;
         foreach (ListViewItem listViewItem in GetListViewItemsForItem(item))
-          UpdateListViewItem(listViewItem);
+          UpdateListViewItemImage(listViewItem);
       }
     }
     protected virtual void Item_ToStringChanged(object sender, EventArgs e) {
@@ -270,20 +275,12 @@ namespace HeuristicLab.Core.Views {
       else {
         T item = (T)sender;
         foreach (ListViewItem listViewItem in GetListViewItemsForItem(item))
-          UpdateListViewItem(listViewItem);
+          UpdateListViewItemText(listViewItem);
       }
     }
     #endregion
 
     #region Helpers
-    protected virtual void SetListViewItemImage(ListViewItem listViewItem) {
-      T item = (T)listViewItem.Tag;
-      int i = 0;
-      while ((i < itemsListView.SmallImageList.Images.Count) && !item.ItemImage.Equals(itemsListView.SmallImageList.Images[i]))
-        i++;
-      if (i == itemsListView.SmallImageList.Images.Count) itemsListView.SmallImageList.Images.Add(item.ItemImage);
-      listViewItem.ImageIndex = i;
-    }
     protected virtual void SortItemsListView(SortOrder sortOrder) {
       itemsListView.Sorting = SortOrder.None;
       itemsListView.Sorting = sortOrder;

sources/HeuristicLab.Core.Views/3.3/ItemListView.cs
@@ -122,7 +122,8 @@ namespace HeuristicLab.Core.Views {
       listViewItem.Text = item.ToString();
       listViewItem.ToolTipText = item.ItemName + ": " + item.ItemDescription;
       listViewItem.Tag = item;
-      SetListViewItemImage(listViewItem);
+      itemsListView.SmallImageList.Images.Add(item.ItemImage);
+      listViewItem.ImageIndex = itemsListView.SmallImageList.Images.Count - 1;
       return listViewItem;
     }
     protected virtual void AddListViewItem(ListViewItem listViewItem) {
@@ -140,12 +141,15 @@ namespace HeuristicLab.Core.Views {
       ((T)listViewItem.Tag).ToStringChanged -= new EventHandler(Item_ToStringChanged);
       listViewItem.Remove();
     }
-    protected virtual void UpdateListViewItem(ListViewItem listViewItem) {
-      T item = (T)listViewItem.Tag;
-      listViewItem.Text = item.ToString();
-      listViewItem.ToolTipText = item.ItemName + ": " + item.ItemDescription;
-      if (itemsListView.SmallImageList.Images[listViewItem.ImageIndex] != item.ItemImage)
-        SetListViewItemImage(listViewItem);
+    protected virtual void UpdateListViewItemImage(ListViewItem listViewItem) {
+      int i = listViewItem.ImageIndex;
+      listViewItem.ImageList.Images[i] = ((T)listViewItem.Tag).ItemImage;
+      listViewItem.ImageIndex = -1;
+      listViewItem.ImageIndex = i;
+    }
+    protected virtual void UpdateListViewItemText(ListViewItem listViewItem) {
+      if (!listViewItem.Text.Equals(listViewItem.Tag.ToString()))
+        listViewItem.Text = listViewItem.Tag.ToString();
     }
 
     protected virtual void itemsListView_SelectedIndexChanged(object sender, EventArgs e) {
@@ -310,7 +314,8 @@ namespace HeuristicLab.Core.Views {
         foreach (IndexedItem<T> item in e.Items) {
           ListViewItem listViewItem = itemsListView.Items[item.Index];
           listViewItem.Tag = item.Value;
-          UpdateListViewItem(listViewItem);
+          UpdateListViewItemImage(listViewItem);
+          UpdateListViewItemText(listViewItem);
         }
       }
     }
@@ -338,7 +343,7 @@ namespace HeuristicLab.Core.Views {
         T item = (T)sender;
         foreach (ListViewItem listViewItem in itemsListView.Items) {
           if (((T)listViewItem.Tag) == item)
-            UpdateListViewItem(listViewItem);
+            UpdateListViewItemImage(listViewItem);
         }
       }
     }
@@ -349,20 +354,9 @@ namespace HeuristicLab.Core.Views {
         T item = (T)sender;
         foreach (ListViewItem listViewItem in itemsListView.Items) {
           if (((T)listViewItem.Tag) == item)
-            UpdateListViewItem(listViewItem);
+            UpdateListViewItemText(listViewItem);
         }
       }
-    }
-    #endregion
-
-    #region Helpers
-    protected virtual void SetListViewItemImage(ListViewItem listViewItem) {
-      T item = (T)listViewItem.Tag;
-      int i = 0;
-      while ((i < itemsListView.SmallImageList.Images.Count) && (item.ItemImage != itemsListView.SmallImageList.Images[i]))
-        i++;
-      if (i == itemsListView.SmallImageList.Images.Count) itemsListView.SmallImageList.Images.Add(item.ItemImage);
-      listViewItem.ImageIndex = i;
     }
     #endregion
   }

sources/HeuristicLab.Core.Views/3.3/NamedItemCollectionView.cs
@@ -99,7 +99,7 @@ namespace HeuristicLab.Core.Views {
       else {
         foreach (T item in e.Items) {
           foreach (ListViewItem listViewItem in GetListViewItemsForItem(item))
-            UpdateListViewItem(listViewItem);
+            UpdateListViewItemText(listViewItem);
         }
       }
     }