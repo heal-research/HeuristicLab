e311827449ce33510028b5ba01dddb05d8c9c03d
Michael Kommenda <michael.kommenda@fh-ooe.at>
2010-04-14 21:00:15 +0000

    added prototype of !RunCollectionBubbleChartView (ticket #970)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@3349 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ sources/HeuristicLab.Optimization.Views/3.3/RunCollectionBubbleChartView.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace HeuristicLab.Optimization.Views {
+  partial class RunCollectionBubbleChartView {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+      #region Component Designer generated code
+
+    /// <summary> 
+    /// Required method for Designer support - do not modify 
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+      System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+      this.yJitterLabel = new System.Windows.Forms.Label();
+      this.xJitterlabel = new System.Windows.Forms.Label();
+      this.xTrackBar = new System.Windows.Forms.TrackBar();
+      this.xAxisLabel = new System.Windows.Forms.Label();
+      this.xAxisComboBox = new System.Windows.Forms.ComboBox();
+      this.yAxisLabel = new System.Windows.Forms.Label();
+      this.yAxisComboBox = new System.Windows.Forms.ComboBox();
+      this.yTrackBar = new System.Windows.Forms.TrackBar();
+      this.sizeComboBox = new System.Windows.Forms.ComboBox();
+      this.sizeLabel = new System.Windows.Forms.Label();
+      this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+      this.zoomButton = new System.Windows.Forms.RadioButton();
+      this.selectButton = new System.Windows.Forms.RadioButton();
+      this.radioButtonGroup = new System.Windows.Forms.GroupBox();
+      this.colorButton = new System.Windows.Forms.Button();
+      this.colorDialog = new System.Windows.Forms.ColorDialog();
+      ((System.ComponentModel.ISupportInitialize)(this.xTrackBar)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.yTrackBar)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+      this.radioButtonGroup.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // yJitterLabel
+      // 
+      this.yJitterLabel.AutoSize = true;
+      this.yJitterLabel.BackColor = System.Drawing.SystemColors.Control;
+      this.yJitterLabel.Location = new System.Drawing.Point(257, 6);
+      this.yJitterLabel.Name = "yJitterLabel";
+      this.yJitterLabel.Size = new System.Drawing.Size(29, 13);
+      this.yJitterLabel.TabIndex = 13;
+      this.yJitterLabel.Text = "jitter:";
+      // 
+      // xJitterlabel
+      // 
+      this.xJitterlabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.xJitterlabel.AutoSize = true;
+      this.xJitterlabel.BackColor = System.Drawing.SystemColors.Control;
+      this.xJitterlabel.Location = new System.Drawing.Point(461, 309);
+      this.xJitterlabel.Name = "xJitterlabel";
+      this.xJitterlabel.Size = new System.Drawing.Size(29, 13);
+      this.xJitterlabel.TabIndex = 12;
+      this.xJitterlabel.Text = "jitter:";
+      // 
+      // xTrackBar
+      // 
+      this.xTrackBar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.xTrackBar.BackColor = System.Drawing.SystemColors.Control;
+      this.xTrackBar.Location = new System.Drawing.Point(496, 306);
+      this.xTrackBar.Maximum = 100;
+      this.xTrackBar.Name = "xTrackBar";
+      this.xTrackBar.Size = new System.Drawing.Size(64, 45);
+      this.xTrackBar.TabIndex = 11;
+      this.xTrackBar.TickStyle = System.Windows.Forms.TickStyle.None;
+      // 
+      // xAxisLabel
+      // 
+      this.xAxisLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.xAxisLabel.AutoSize = true;
+      this.xAxisLabel.BackColor = System.Drawing.SystemColors.Control;
+      this.xAxisLabel.Location = new System.Drawing.Point(213, 309);
+      this.xAxisLabel.Name = "xAxisLabel";
+      this.xAxisLabel.Size = new System.Drawing.Size(15, 13);
+      this.xAxisLabel.TabIndex = 8;
+      this.xAxisLabel.Text = "x:";
+      // 
+      // xAxisComboBox
+      // 
+      this.xAxisComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.xAxisComboBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+      this.xAxisComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.xAxisComboBox.FormattingEnabled = true;
+      this.xAxisComboBox.Location = new System.Drawing.Point(234, 307);
+      this.xAxisComboBox.Name = "xAxisComboBox";
+      this.xAxisComboBox.Size = new System.Drawing.Size(221, 21);
+      this.xAxisComboBox.TabIndex = 7;
+      this.xAxisComboBox.SelectedIndexChanged += new System.EventHandler(this.AxisComboBox_SelectedIndexChanged);
+      // 
+      // yAxisLabel
+      // 
+      this.yAxisLabel.AutoSize = true;
+      this.yAxisLabel.BackColor = System.Drawing.SystemColors.Control;
+      this.yAxisLabel.Location = new System.Drawing.Point(3, 6);
+      this.yAxisLabel.Name = "yAxisLabel";
+      this.yAxisLabel.Size = new System.Drawing.Size(15, 13);
+      this.yAxisLabel.TabIndex = 6;
+      this.yAxisLabel.Text = "y:";
+      // 
+      // yAxisComboBox
+      // 
+      this.yAxisComboBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+      this.yAxisComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.yAxisComboBox.FormattingEnabled = true;
+      this.yAxisComboBox.Location = new System.Drawing.Point(24, 3);
+      this.yAxisComboBox.Name = "yAxisComboBox";
+      this.yAxisComboBox.Size = new System.Drawing.Size(221, 21);
+      this.yAxisComboBox.TabIndex = 5;
+      this.yAxisComboBox.SelectedIndexChanged += new System.EventHandler(this.AxisComboBox_SelectedIndexChanged);
+      // 
+      // yTrackBar
+      // 
+      this.yTrackBar.BackColor = System.Drawing.SystemColors.Control;
+      this.yTrackBar.Location = new System.Drawing.Point(292, 3);
+      this.yTrackBar.Maximum = 100;
+      this.yTrackBar.Name = "yTrackBar";
+      this.yTrackBar.Size = new System.Drawing.Size(59, 45);
+      this.yTrackBar.TabIndex = 10;
+      this.yTrackBar.TickStyle = System.Windows.Forms.TickStyle.None;
+      // 
+      // sizeComboBox
+      // 
+      this.sizeComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.sizeComboBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+      this.sizeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.sizeComboBox.FormattingEnabled = true;
+      this.sizeComboBox.Location = new System.Drawing.Point(441, 3);
+      this.sizeComboBox.Name = "sizeComboBox";
+      this.sizeComboBox.Size = new System.Drawing.Size(121, 21);
+      this.sizeComboBox.TabIndex = 14;
+      // 
+      // sizeLabel
+      // 
+      this.sizeLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.sizeLabel.AutoSize = true;
+      this.sizeLabel.BackColor = System.Drawing.SystemColors.Control;
+      this.sizeLabel.Location = new System.Drawing.Point(371, 6);
+      this.sizeLabel.Name = "sizeLabel";
+      this.sizeLabel.Size = new System.Drawing.Size(64, 13);
+      this.sizeLabel.TabIndex = 15;
+      this.sizeLabel.Text = "Bubble size:";
+      // 
+      // chart
+      // 
+      this.chart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                  | System.Windows.Forms.AnchorStyles.Left)
+                  | System.Windows.Forms.AnchorStyles.Right)));
+      chartArea1.Name = "ChartArea1";
+      this.chart.ChartAreas.Add(chartArea1);
+      this.chart.Location = new System.Drawing.Point(6, 30);
+      this.chart.Name = "chart";
+      series1.ChartArea = "ChartArea1";
+      series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bubble;
+      series1.CustomProperties = "BubbleMaxSize=0";
+      series1.IsVisibleInLegend = false;
+      series1.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+      series1.Name = "Bubbles";
+      series1.YValuesPerPoint = 2;
+      this.chart.Series.Add(series1);
+      this.chart.Size = new System.Drawing.Size(556, 263);
+      this.chart.TabIndex = 16;
+      this.chart.Text = "chart1";
+      // 
+      // zoomButton
+      // 
+      this.zoomButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.zoomButton.AutoSize = true;
+      this.zoomButton.Checked = true;
+      this.zoomButton.Location = new System.Drawing.Point(6, 10);
+      this.zoomButton.Name = "zoomButton";
+      this.zoomButton.Size = new System.Drawing.Size(52, 17);
+      this.zoomButton.TabIndex = 17;
+      this.zoomButton.TabStop = true;
+      this.zoomButton.Text = "Zoom";
+      this.zoomButton.UseVisualStyleBackColor = true;
+      // 
+      // selectButton
+      // 
+      this.selectButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.selectButton.AutoSize = true;
+      this.selectButton.Location = new System.Drawing.Point(64, 9);
+      this.selectButton.Name = "selectButton";
+      this.selectButton.Size = new System.Drawing.Size(55, 17);
+      this.selectButton.TabIndex = 18;
+      this.selectButton.Text = "Select";
+      this.selectButton.UseVisualStyleBackColor = true;
+      // 
+      // radioButtonGroup
+      // 
+      this.radioButtonGroup.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.radioButtonGroup.Controls.Add(this.zoomButton);
+      this.radioButtonGroup.Controls.Add(this.selectButton);
+      this.radioButtonGroup.Location = new System.Drawing.Point(6, 299);
+      this.radioButtonGroup.Name = "radioButtonGroup";
+      this.radioButtonGroup.Size = new System.Drawing.Size(135, 32);
+      this.radioButtonGroup.TabIndex = 19;
+      this.radioButtonGroup.TabStop = false;
+      // 
+      // colorButton
+      // 
+      this.colorButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.colorButton.Enabled = false;
+      this.colorButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+      this.colorButton.Location = new System.Drawing.Point(144, 306);
+      this.colorButton.Name = "colorButton";
+      this.colorButton.Size = new System.Drawing.Size(64, 23);
+      this.colorButton.TabIndex = 20;
+      this.colorButton.Text = "Color";
+      this.colorButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+      this.colorButton.UseVisualStyleBackColor = true;
+      // 
+      // colorDialog
+      // 
+      this.colorDialog.FullOpen = true;
+      // 
+      // RunCollectionBubbleChartView
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.Controls.Add(this.colorButton);
+      this.Controls.Add(this.radioButtonGroup);
+      this.Controls.Add(this.chart);
+      this.Controls.Add(this.sizeLabel);
+      this.Controls.Add(this.sizeComboBox);
+      this.Controls.Add(this.yJitterLabel);
+      this.Controls.Add(this.xJitterlabel);
+      this.Controls.Add(this.xTrackBar);
+      this.Controls.Add(this.xAxisLabel);
+      this.Controls.Add(this.xAxisComboBox);
+      this.Controls.Add(this.yAxisLabel);
+      this.Controls.Add(this.yAxisComboBox);
+      this.Controls.Add(this.yTrackBar);
+      this.Name = "RunCollectionBubbleChartView";
+      this.Size = new System.Drawing.Size(567, 334);
+      ((System.ComponentModel.ISupportInitialize)(this.xTrackBar)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.yTrackBar)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+      this.radioButtonGroup.ResumeLayout(false);
+      this.radioButtonGroup.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label xAxisLabel;
+    private System.Windows.Forms.ComboBox xAxisComboBox;
+    private System.Windows.Forms.Label yAxisLabel;
+    private System.Windows.Forms.ComboBox yAxisComboBox;
+    private System.Windows.Forms.TrackBar yTrackBar;
+    private System.Windows.Forms.TrackBar xTrackBar;
+    private System.Windows.Forms.Label xJitterlabel;
+    private System.Windows.Forms.Label yJitterLabel;
+    private System.Windows.Forms.ComboBox sizeComboBox;
+    private System.Windows.Forms.Label sizeLabel;
+    private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+    private System.Windows.Forms.RadioButton zoomButton;
+    private System.Windows.Forms.RadioButton selectButton;
+    private System.Windows.Forms.GroupBox radioButtonGroup;
+    private System.Windows.Forms.Button colorButton;
+    private System.Windows.Forms.ColorDialog colorDialog;
+  }
+}
---
+++ sources/HeuristicLab.Optimization.Views/3.3/RunCollectionBubbleChartView.cs
@@ -0,0 +1,162 @@
+﻿#region License Information
+/* HeuristicLab
+ * Copyright (C) 2002-2010 Heuristic and Evolutionary Algorithms Laboratory (HEAL)
+ *
+ * This file is part of HeuristicLab.
+ *
+ * HeuristicLab is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HeuristicLab is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HeuristicLab. If not, see <http://www.gnu.org/licenses/>.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using HeuristicLab.MainForm.WindowsForms;
+using HeuristicLab.MainForm;
+using System.Windows.Forms.DataVisualization.Charting;
+using HeuristicLab.Core;
+
+namespace HeuristicLab.Optimization.Views {
+  [View("RunCollection BubbleChart")]
+  [Content(typeof(RunCollection), false)]
+  public partial class RunCollectionBubbleChartView : AsynchronousContentView {
+    private Dictionary<int, Dictionary<object, double>> categoricalMapping;
+
+    public RunCollectionBubbleChartView() {
+      InitializeComponent();
+      Caption = "Run Collection Bubble Chart";
+      this.categoricalMapping = new Dictionary<int, Dictionary<object, double>>();
+    }
+
+    public RunCollectionBubbleChartView(RunCollection content)
+      : this() {
+      Content = content;
+    }
+
+    public new RunCollection Content {
+      get { return (RunCollection)base.Content; }
+      set { base.Content = value; }
+    }
+
+    protected override void RegisterContentEvents() {
+      base.RegisterContentEvents();
+      Content.Reset += new EventHandler(Content_Reset);
+      Content.ColumnNamesChanged += new EventHandler(Content_ColumnNamesChanged);
+    }
+
+    protected override void DeregisterContentEvents() {
+      base.DeregisterContentEvents();
+      Content.Reset -= new EventHandler(Content_Reset);
+      Content.ColumnNamesChanged -= new EventHandler(Content_ColumnNamesChanged);
+    }
+
+    protected override void OnContentChanged() {
+      base.OnContentChanged();
+      this.UpdateComboBoxes();
+    }
+
+    private void Content_ColumnNamesChanged(object sender, EventArgs e) {
+      if (InvokeRequired)
+        Invoke(new EventHandler(Content_ColumnNamesChanged), sender, e);
+      else
+        UpdateComboBoxes();
+    }
+
+    private void UpdateComboBoxes() {
+      this.xAxisComboBox.Items.Clear();
+      this.yAxisComboBox.Items.Clear();
+      this.xAxisComboBox.Items.AddRange(Content.ColumnNames.ToArray());
+      this.yAxisComboBox.Items.AddRange(Content.ColumnNames.ToArray());
+    }
+
+    private void Content_Reset(object sender, EventArgs e) {
+      if (InvokeRequired)
+        Invoke(new EventHandler(Content_Reset), sender, e);
+      else {
+        this.categoricalMapping.Clear();
+        UpdateDataPoints();
+      }
+    }
+
+    private void UpdateDataPoints() {
+      Series series = this.chart.Series[0];
+      series.Points.Clear();
+
+      double? xValue;
+      double? yValue;
+      for (int row = 0; row < Content.Count; row++) {
+        xValue = GetValue(row, xAxisComboBox.SelectedIndex);
+        yValue = GetValue(row, yAxisComboBox.SelectedIndex);
+        if (xValue.HasValue && yValue.HasValue)
+          series.Points.Add(new DataPoint(xValue.Value, yValue.Value));
+      }
+    }
+
+    private void AxisComboBox_SelectedIndexChanged(object sender, EventArgs e) {
+      UpdateDataPoints();
+      UpdateAxisLabels();
+    }
+
+    private double? GetValue(int row, int column) {
+      if (column < 0 || row < 0)
+        return null;
+
+      string value = Content.GetValue(row, column);
+      double d;
+      double? ret = null;
+      if (double.TryParse(value, out d))
+        ret = d;
+      else if (value != null)
+        ret = GetCategoricalValue(column, value);
+      return ret;
+    }
+    private double GetCategoricalValue(int dimension, object c) {
+      if (!this.categoricalMapping.ContainsKey(dimension))
+        this.categoricalMapping[dimension] = new Dictionary<object, double>();
+      if (!this.categoricalMapping[dimension].ContainsKey(c)) {
+        if (this.categoricalMapping[dimension].Values.Count == 0)
+          this.categoricalMapping[dimension][c] = 1.0;
+        else
+          this.categoricalMapping[dimension][c] = this.categoricalMapping[dimension].Values.Max() + 1.0;
+      }
+      return this.categoricalMapping[dimension][c];
+    }
+
+    private void UpdateAxisLabels() {
+      Axis xAxis = this.chart.ChartAreas[0].AxisX;
+      Axis yAxis = this.chart.ChartAreas[0].AxisY;
+      SetAxisLabels(xAxis, xAxisComboBox.SelectedIndex);
+      SetAxisLabels(yAxis, yAxisComboBox.SelectedIndex);
+    }
+    private void SetAxisLabels(Axis axis, int dimension) {
+      if (categoricalMapping.ContainsKey(dimension)) {
+        axis.CustomLabels.Clear();
+        CustomLabel label = null;
+        foreach (var pair in categoricalMapping[dimension]) {
+          label = axis.CustomLabels.Add(pair.Value - 0.5, pair.Value + 0.5, pair.Key.ToString());
+          label.GridTicks = GridTickTypes.TickMark;
+        }
+        axis.IsLabelAutoFit = false;
+        axis.LabelStyle.Enabled = true;
+        axis.LabelStyle.Angle = 0;
+        axis.LabelStyle.TruncatedLabels = true;
+      }
+    }
+  }
+}
