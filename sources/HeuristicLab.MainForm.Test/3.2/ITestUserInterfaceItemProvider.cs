17eda895e2b625784e8948ac09ce16464876f385
Stefan Wagner <swagner@heuristiclab.com>
2010-05-02 03:39:41 +0000

    Removed HL 3.2 related parts from trunk (#995)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@3587 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.MainForm.Test/3.2/ITestUserInterfaceItemProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace HeuristicLab.MainForm.Test {
-  public interface ITestUserInterfaceItemProvider {
-  }
-}
