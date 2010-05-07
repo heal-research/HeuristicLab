b6727ed9aa26f4aee4f12199fc3eef8ce4ac4dd3
Stefan Wagner <swagner@heuristiclab.com>
2010-05-07 21:33:00 +0000

    Adapted content and layout of start page (#936)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@3711 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Optimizer/3.3/StartPage.Designer.cs
+++ sources/HeuristicLab.Optimizer/3.3/StartPage.Designer.cs
@@ -56,8 +56,12 @@ namespace HeuristicLab.Optimizer {
       this.nameColumnHeader = new System.Windows.Forms.ColumnHeader();
       this.descriptionColumnHeader = new System.Windows.Forms.ColumnHeader();
       this.imageList = new System.Windows.Forms.ImageList(this.components);
+      this.splitContainer = new System.Windows.Forms.SplitContainer();
       this.samplesGroupBox.SuspendLayout();
       this.loadingPanel.SuspendLayout();
+      this.splitContainer.Panel1.SuspendLayout();
+      this.splitContainer.Panel2.SuspendLayout();
+      this.splitContainer.SuspendLayout();
       this.SuspendLayout();
       // 
       // showStartPageCheckBox
@@ -65,10 +69,10 @@ namespace HeuristicLab.Optimizer {
       this.showStartPageCheckBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
       this.showStartPageCheckBox.AutoSize = true;
       this.showStartPageCheckBox.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
-      this.showStartPageCheckBox.Location = new System.Drawing.Point(3, 542);
+      this.showStartPageCheckBox.Location = new System.Drawing.Point(3, 620);
       this.showStartPageCheckBox.Name = "showStartPageCheckBox";
       this.showStartPageCheckBox.Size = new System.Drawing.Size(158, 17);
-      this.showStartPageCheckBox.TabIndex = 3;
+      this.showStartPageCheckBox.TabIndex = 1;
       this.showStartPageCheckBox.Text = "Show Start Page on Startup";
       this.showStartPageCheckBox.UseVisualStyleBackColor = true;
       this.showStartPageCheckBox.CheckedChanged += new System.EventHandler(this.showStartPageCheckBox_CheckedChanged);
@@ -84,7 +88,7 @@ namespace HeuristicLab.Optimizer {
       this.firstStepsRichTextBox.Location = new System.Drawing.Point(3, 33);
       this.firstStepsRichTextBox.Name = "firstStepsRichTextBox";
       this.firstStepsRichTextBox.ReadOnly = true;
-      this.firstStepsRichTextBox.Size = new System.Drawing.Size(760, 319);
+      this.firstStepsRichTextBox.Size = new System.Drawing.Size(725, 370);
       this.firstStepsRichTextBox.TabIndex = 1;
       this.firstStepsRichTextBox.Text = "First Steps";
       this.firstStepsRichTextBox.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.firstStepsRichTextBox_LinkClicked);
@@ -94,34 +98,35 @@ namespace HeuristicLab.Optimizer {
       this.titleLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                   | System.Windows.Forms.AnchorStyles.Right)));
       this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.titleLabel.Location = new System.Drawing.Point(3, 0);
+      this.titleLabel.Location = new System.Drawing.Point(-1, 0);
       this.titleLabel.Name = "titleLabel";
-      this.titleLabel.Size = new System.Drawing.Size(760, 30);
+      this.titleLabel.Size = new System.Drawing.Size(729, 30);
       this.titleLabel.TabIndex = 0;
       this.titleLabel.Text = "Title";
       this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
       // 
       // samplesGroupBox
       // 
-      this.samplesGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+      this.samplesGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                  | System.Windows.Forms.AnchorStyles.Left)
                   | System.Windows.Forms.AnchorStyles.Right)));
       this.samplesGroupBox.Controls.Add(this.loadingPanel);
       this.samplesGroupBox.Controls.Add(this.samplesListView);
-      this.samplesGroupBox.Location = new System.Drawing.Point(3, 358);
+      this.samplesGroupBox.Location = new System.Drawing.Point(0, 3);
       this.samplesGroupBox.Name = "samplesGroupBox";
-      this.samplesGroupBox.Size = new System.Drawing.Size(760, 178);
-      this.samplesGroupBox.TabIndex = 2;
+      this.samplesGroupBox.Size = new System.Drawing.Size(728, 201);
+      this.samplesGroupBox.TabIndex = 0;
       this.samplesGroupBox.TabStop = false;
       this.samplesGroupBox.Text = "Samples";
       // 
       // loadingPanel
       // 
-      this.loadingPanel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+      this.loadingPanel.Anchor = System.Windows.Forms.AnchorStyles.None;
       this.loadingPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
       this.loadingPanel.Controls.Add(this.loadingProgressBar);
       this.loadingPanel.Controls.Add(this.loadingLabel);
       this.loadingPanel.Enabled = false;
-      this.loadingPanel.Location = new System.Drawing.Point(205, 74);
+      this.loadingPanel.Location = new System.Drawing.Point(189, 89);
       this.loadingPanel.Name = "loadingPanel";
       this.loadingPanel.Size = new System.Drawing.Size(350, 62);
       this.loadingPanel.TabIndex = 1;
@@ -156,7 +161,7 @@ namespace HeuristicLab.Optimizer {
       this.samplesListView.MultiSelect = false;
       this.samplesListView.Name = "samplesListView";
       this.samplesListView.ShowItemToolTips = true;
-      this.samplesListView.Size = new System.Drawing.Size(754, 159);
+      this.samplesListView.Size = new System.Drawing.Size(722, 182);
       this.samplesListView.SmallImageList = this.imageList;
       this.samplesListView.Sorting = System.Windows.Forms.SortOrder.Ascending;
       this.samplesListView.TabIndex = 0;
@@ -181,18 +186,40 @@ namespace HeuristicLab.Optimizer {
       this.imageList.ImageSize = new System.Drawing.Size(16, 16);
       this.imageList.TransparentColor = System.Drawing.Color.Transparent;
       // 
+      // splitContainer
+      // 
+      this.splitContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                  | System.Windows.Forms.AnchorStyles.Left)
+                  | System.Windows.Forms.AnchorStyles.Right)));
+      this.splitContainer.Location = new System.Drawing.Point(0, 0);
+      this.splitContainer.Name = "splitContainer";
+      this.splitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+      // 
+      // splitContainer.Panel1
+      // 
+      this.splitContainer.Panel1.Controls.Add(this.titleLabel);
+      this.splitContainer.Panel1.Controls.Add(this.firstStepsRichTextBox);
+      // 
+      // splitContainer.Panel2
+      // 
+      this.splitContainer.Panel2.Controls.Add(this.samplesGroupBox);
+      this.splitContainer.Size = new System.Drawing.Size(728, 614);
+      this.splitContainer.SplitterDistance = 406;
+      this.splitContainer.TabIndex = 0;
+      // 
       // StartPage
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.Controls.Add(this.firstStepsRichTextBox);
-      this.Controls.Add(this.samplesGroupBox);
-      this.Controls.Add(this.titleLabel);
+      this.Controls.Add(this.splitContainer);
       this.Controls.Add(this.showStartPageCheckBox);
       this.Name = "StartPage";
-      this.Size = new System.Drawing.Size(766, 562);
+      this.Size = new System.Drawing.Size(728, 640);
       this.samplesGroupBox.ResumeLayout(false);
       this.loadingPanel.ResumeLayout(false);
+      this.splitContainer.Panel1.ResumeLayout(false);
+      this.splitContainer.Panel2.ResumeLayout(false);
+      this.splitContainer.ResumeLayout(false);
       this.ResumeLayout(false);
       this.PerformLayout();
 
@@ -211,5 +238,6 @@ namespace HeuristicLab.Optimizer {
     private System.Windows.Forms.Panel loadingPanel;
     private System.Windows.Forms.ProgressBar loadingProgressBar;
     private System.Windows.Forms.Label loadingLabel;
+    private System.Windows.Forms.SplitContainer splitContainer;
   }
 }
