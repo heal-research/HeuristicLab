84f82803afb935c2655639296b30e3b9c9afae19
Stefan Wagner <swagner@heuristiclab.com>
2010-05-18 02:21:08 +0000

    Restructured repository in preparation of the HeuristicLab 3.3.0 release (#1000)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk/sources@3837 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- HeuristicLab/3.3/MergeConfigs.cmd
+++ HeuristicLab/3.3/MergeConfigs.cmd
@@ -1,4 +1,4 @@
-ConfigMerger "%SolutionDir%\HeuristicLab.PluginInfrastructure\%Outdir%\HeuristicLab.PluginInfrastructure.dll.config" "HeuristicLab.exe.config"
-ConfigMerger "%SolutionDir%\HeuristicLab.Optimizer\3.3\%Outdir%\HeuristicLab.Optimizer-3.3.dll.config" "HeuristicLab.exe.config"
-ConfigMerger "%SolutionDir%\HeuristicLab.Persistence\3.3\%Outdir%\HeuristicLab.Persistence-3.3.dll.config" "HeuristicLab.exe.config"
-ConfigMerger "%SolutionDir%\HeuristicLab.Tracing\3.2\%Outdir%\HeuristicLab.Tracing-3.2.dll.config" "HeuristicLab.exe.config"
+ConfigMerger "%SolutionDir%\HeuristicLab.PluginInfrastructure\3.3\%Outdir%\HeuristicLab.PluginInfrastructure-3.3.dll.config" "HeuristicLab 3.3.exe.config"
+ConfigMerger "%SolutionDir%\HeuristicLab.Optimizer\3.3\%Outdir%\HeuristicLab.Optimizer-3.3.dll.config" "HeuristicLab 3.3.exe.config"
+ConfigMerger "%SolutionDir%\HeuristicLab.Persistence\3.3\%Outdir%\HeuristicLab.Persistence-3.3.dll.config" "HeuristicLab 3.3.exe.config"
+ConfigMerger "%SolutionDir%\HeuristicLab.Tracing\3.3\%Outdir%\HeuristicLab.Tracing-3.3.dll.config" "HeuristicLab 3.3.exe.config"
