c8525dbdce203f9f81697bb561f5f9440e282a21
Michael Kommenda <michael.kommenda@fh-ooe.at>
2010-05-18 21:25:23 +0000

    updated !LinearRegressionSolutionCreator (ticket #1012)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@3848 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Problems.DataAnalysis.Regression/3.3/LinearRegression/LinearRegressionSolutionCreator.cs
+++ sources/HeuristicLab.Problems.DataAnalysis.Regression/3.3/LinearRegression/LinearRegressionSolutionCreator.cs
@@ -32,6 +32,7 @@ using HeuristicLab.Problems.DataAnalysis.Symbolic;
 using HeuristicLab.Problems.DataAnalysis.Symbolic.Symbols;
 using HeuristicLab.Encodings.SymbolicExpressionTreeEncoding.Symbols;
 using HeuristicLab.Parameters;
+using HeuristicLab.Data;
 
 namespace HeuristicLab.Problems.DataAnalysis.Regression.LinearRegression {
   /// <summary>
@@ -42,10 +43,14 @@ namespace HeuristicLab.Problems.DataAnalysis.Regression.LinearRegression {
   public class LinearRegressionSolutionCreator : SingleSuccessorOperator, ISolutionCreator {
     private const string SymbolicExpressionTreeParameterName = "SymbolicExpressionTree";
     private const string DataAnalysisProblemDataParameterName = "DataAnalysisProblemData";
+    private const string SamplesStartParameterName = "SamplesStart";
+    private const string SamplesEndParameterName = "SamplesEnd";
 
     public LinearRegressionSolutionCreator() {
       Parameters.Add(new LookupParameter<SymbolicExpressionTree>(SymbolicExpressionTreeParameterName, "The resulting solution encoded as a symbolic expression tree."));
       Parameters.Add(new LookupParameter<DataAnalysisProblemData>(DataAnalysisProblemDataParameterName, "The problem data on which the linear regression should be calculated."));
+      Parameters.Add(new ValueLookupParameter<IntValue>(SamplesStartParameterName,"The start of the samples on which the linear regression should be applied."));
+      Parameters.Add(new ValueLookupParameter<IntValue>(SamplesEndParameterName,"The end of the samples on which the linear regression should be applied."));
     }
     [StorableConstructor]
     public LinearRegressionSolutionCreator(bool deserializing)
@@ -68,39 +73,54 @@ namespace HeuristicLab.Problems.DataAnalysis.Regression.LinearRegression {
       get { return DataAnalysisProblemDataParameter.ActualValue; }
       set { DataAnalysisProblemDataParameter.ActualValue = value; }
     }
+
+    public IValueLookupParameter<IntValue> SamplesStartParameter {
+      get { return (IValueLookupParameter<IntValue>)Parameters[SamplesStartParameterName]; }
+    }
+    public IntValue SamplesStart {
+      get { return SamplesStartParameter.ActualValue; }
+      set { SamplesStartParameter.ActualValue = value; }
+    }
+
+    public IValueLookupParameter<IntValue> SamplesEndParameter {
+      get { return (IValueLookupParameter<IntValue>)Parameters[SamplesEndParameterName]; }
+    }
+    public IntValue SamplesEnd {
+      get { return SamplesEndParameter.ActualValue; }
+      set { SamplesEndParameter.ActualValue = value; }
+    }
     #endregion
 
 
     public override IOperation Apply() {
-      SymbolicExpressionTree = CreateSymbolicExpressionTree(DataAnalysisProblemData);
+      SymbolicExpressionTree = CreateSymbolicExpressionTree(DataAnalysisProblemData.Dataset,DataAnalysisProblemData.TargetVariable.Value, DataAnalysisProblemData.InputVariables.CheckedItems.Select(x => x.Value.Value), SamplesStart.Value, SamplesEnd.Value);
       return base.Apply();
     }
 
-    public static SymbolicExpressionTree CreateSymbolicExpressionTree(DataAnalysisProblemData problem) {
-      List<int> allowedRows = CalculateAllowedRows(problem);
-      double[,] inputMatrix = PrepareInputMatrix(problem, allowedRows);
+    public static SymbolicExpressionTree CreateSymbolicExpressionTree(Dataset dataset, string targetVariable, IEnumerable<string> allowedInputVariables, int start, int end) {
+      double[,] inputMatrix = LinearRegressionUtil.PrepareInputMatrix(dataset, targetVariable, allowedInputVariables, start, end);
 
       alglib.linreg.linearmodel lm = new alglib.linreg.linearmodel();
       alglib.linreg.lrreport ar = new alglib.linreg.lrreport();
       int nRows = inputMatrix.GetLength(0);
-      int nFeatures = inputMatrix.GetLength(1) -1;
-      double[] coefficients = new double[nFeatures +1]; //last coefficient is for the constant
+      int nFeatures = inputMatrix.GetLength(1) - 1;
+      double[] coefficients = new double[nFeatures + 1]; //last coefficient is for the constant
 
       int retVal = 1;
       alglib.linreg.lrbuild(ref inputMatrix, nRows, nFeatures, ref retVal, ref lm, ref ar);
       if (retVal != 1) throw new ArgumentException("Error in calculation of linear regression model");
 
-      for (int i = 0; i < nFeatures+1; i++)
+      for (int i = 0; i < nFeatures + 1; i++)
         coefficients[i] = lm.w[i + 4];
 
       SymbolicExpressionTree tree = new SymbolicExpressionTree(new ProgramRootSymbol().CreateTreeNode());
-      SymbolicExpressionTreeNode start = new StartSymbol().CreateTreeNode();
-      tree.Root.AddSubTree(start);
+      SymbolicExpressionTreeNode startNode = new StartSymbol().CreateTreeNode();
+      tree.Root.AddSubTree(startNode);
       SymbolicExpressionTreeNode addition = new Addition().CreateTreeNode();
-      start.AddSubTree(addition);
+      startNode.AddSubTree(addition);
 
       int col = 0;
-      foreach (string column in problem.InputVariables.CheckedItems.Select(c => c.Value.Value)) {
+      foreach (string column in allowedInputVariables) {
         VariableTreeNode vNode = (VariableTreeNode)new HeuristicLab.Problems.DataAnalysis.Symbolic.Symbols.Variable().CreateTreeNode();
         vNode.VariableName = column;
         vNode.Weight = coefficients[col];
@@ -110,41 +130,9 @@ namespace HeuristicLab.Problems.DataAnalysis.Regression.LinearRegression {
 
       ConstantTreeNode cNode = (ConstantTreeNode)new Constant().CreateTreeNode();
       cNode.Value = coefficients[coefficients.Length - 1];
+      addition.AddSubTree(cNode);
 
       return tree;
-    }
-
-    private static List<int> CalculateAllowedRows(DataAnalysisProblemData problem) {
-      List<int> allowedRows = new List<int>();
-      bool add = false;
-
-      for (int row = problem.TrainingSamplesStart.Value; row < problem.TrainingSamplesEnd.Value; row++) {
-        add = true;
-        foreach (string column in problem.InputVariables.CheckedItems.Select(c => c.Value.Value)) {
-          double value = problem.Dataset[column, row];
-          if (double.IsInfinity(value) ||
-            double.IsNaN(value))
-            add = false;
-        }
-        if (double.IsNaN(problem.Dataset[problem.TargetVariable.Value, row]))
-          add = false;
-        if (add)
-          allowedRows.Add(row);
-        add = true;
-      }
-      return allowedRows;
-    }
-    private static double[,] PrepareInputMatrix(DataAnalysisProblemData problem, IList<int> allowedRows) {
-      double[,] matrix = new double[allowedRows.Count, problem.InputVariables.CheckedItems.Count()+1];
-      for (int row = 0; row < allowedRows.Count; row++) {
-        int col = 0;
-        foreach (string column in problem.InputVariables.CheckedItems.Select(c => c.Value.Value)) {
-          matrix[row, col] = problem.Dataset[column, row];
-          col++;
-        }
-        matrix[row, problem.InputVariables.CheckedItems.Count()] = problem.Dataset[problem.TargetVariable.Value, row];
-      }
-      return matrix;
     }
   }
 }
