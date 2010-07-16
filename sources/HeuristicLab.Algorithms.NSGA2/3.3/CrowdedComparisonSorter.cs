52012dab3089e2a4e12a5f09afba1114596cdee4
Andreas Beham <andreas.beham@heuristiclab.com>
2010-07-16 23:20:46 +0000

    #1040  * Changed license text and organized usings
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@4040 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Algorithms.NSGA2/3.3/CrowdedComparisonSorter.cs
+++ sources/HeuristicLab.Algorithms.NSGA2/3.3/CrowdedComparisonSorter.cs
@@ -1,6 +1,6 @@
 ﻿#region License Information
 /* HeuristicLab
- * Copyright (C) 2002-2008 Heuristic and Evolutionary Algorithms Laboratory (HEAL)
+ * Copyright (C) 2002-2010 Heuristic and Evolutionary Algorithms Laboratory (HEAL)
  *
  * This file is part of HeuristicLab.
  *
@@ -18,16 +18,15 @@
  * along with HeuristicLab. If not, see <http://www.gnu.org/licenses/>.
  */
 #endregion
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using HeuristicLab.Core;
-using HeuristicLab.Common;
 using HeuristicLab.Data;
-using HeuristicLab.Persistence.Default.CompositeSerializers.Storable;
 using HeuristicLab.Operators;
 using HeuristicLab.Parameters;
+using HeuristicLab.Persistence.Default.CompositeSerializers.Storable;
 
 namespace HeuristicLab.Algorithms.NSGA2 {
   /// <summary>
