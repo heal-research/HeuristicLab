heal-research/HeuristicLab

commit 2d6c67685dc24a78e56b5e61135986f5fa1a35bb

#1090  * Fixed initialization of operators and made operator list storable
git-svn-id: https://src.heuristiclab.com/svn/core/trunk@4098 2abd9481-f8db-48e9-bd25-06bc13291c1b

sources/HeuristicLab.Problems.ArtificialAnt/3.3/ArtificialAntProblem.cs
@@ -41,6 +41,7 @@ namespace HeuristicLab.Problems.ArtificialAnt {
   [Creatable("Problems")]
   [StorableClass]
   public sealed class ArtificialAntProblem : ParameterizedNamedItem, ISingleObjectiveProblem {
+
     public override Image ItemImage {
       get { return HeuristicLab.Common.Resources.VS2008ImageLibrary.Type; }
     }
@@ -179,7 +180,6 @@ namespace HeuristicLab.Problems.ArtificialAnt {
     public DoubleValue BestKnownQuality {
       get { return BestKnownQualityParameter.Value; }
     }
-    private List<IOperator> operators;
     public IEnumerable<IOperator> Operators {
       get { return operators; }
     }
@@ -189,6 +189,11 @@ namespace HeuristicLab.Problems.ArtificialAnt {
     }
     #endregion
 
+    [Storable]
+    private List<IOperator> operators;
+
+    [StorableConstructor]
+    private ArtificialAntProblem(bool deserializing) : base() { }
     public ArtificialAntProblem()
       : base() {
       SymbolicExpressionTreeCreator creator = new ProbabilisticTreeCreator();
@@ -211,15 +216,14 @@ namespace HeuristicLab.Problems.ArtificialAnt {
       evaluator.QualityParameter.ActualName = "FoodEaten";
       ParameterizeSolutionCreator();
       ParameterizeEvaluator();
-      Initialize();
+      InitializeOperators();
+      AttachEventHandlers();
     }
-
-    [StorableConstructor]
-    private ArtificialAntProblem(bool deserializing) : base() { }
 
     public override IDeepCloneable Clone(Cloner cloner) {
       ArtificialAntProblem clone = (ArtificialAntProblem)base.Clone(cloner);
-      clone.Initialize();
+      clone.operators = operators.Select(x => (IOperator)cloner.Clone(x)).ToList();
+      clone.AttachEventHandlers();
       return clone;
     }
 
@@ -273,8 +277,10 @@ namespace HeuristicLab.Problems.ArtificialAnt {
 
     #region Helpers
     [StorableHook(HookType.AfterDeserialization)]
-    private void Initialize() {
-      InitializeOperators();
+    private void AttachEventHandlers() {
+      // Start BackwardsCompatibility3.3 (remove with 3.4)
+      if (operators == null) InitializeOperators();
+      // End BackwardsCompatibility3.3
       SolutionCreatorParameter.ValueChanged += new EventHandler(SolutionCreatorParameter_ValueChanged);
       SolutionCreator.SymbolicExpressionTreeParameter.ActualNameChanged += new EventHandler(SolutionCreator_SymbolicExpressionTreeParameter_ActualNameChanged);
       EvaluatorParameter.ValueChanged += new EventHandler(EvaluatorParameter_ValueChanged);

sources/HeuristicLab.Problems.DataAnalysis.FeatureSelection/3.3/FeatureSelectionProblem.cs
@@ -80,12 +80,16 @@ namespace HeuristicLab.Problems.DataAnalysis.FeatureSelection {
     IEvaluator IProblem.Evaluator {
       get { return EvaluatorParameter.Value; }
     }
-    private List<IOperator> operators;
     public override IEnumerable<IOperator> Operators {
       get { return operators; }
     }
     #endregion
 
+    [Storable]
+    private List<IOperator> operators;
+
+    [StorableConstructor]
+    private FeatureSelectionProblem(bool deserializing) : base() { }
     public FeatureSelectionProblem()
       : base() {
       BinaryVectorCreator creator = new RandomBinaryVectorCreator();
@@ -100,11 +104,9 @@ namespace HeuristicLab.Problems.DataAnalysis.FeatureSelection {
       ParameterizeSolutionCreator();
       ParameterizeEvaluator();
 
-      Initialize();
+      InitializeOperators();
+      AttachEventHandlers();
     }
-
-    [StorableConstructor]
-    private FeatureSelectionProblem(bool deserializing) : base() { }
 
     [StorableHook(HookType.AfterDeserialization)]
     private void AfterDeserializationHook() {
@@ -114,7 +116,8 @@ namespace HeuristicLab.Problems.DataAnalysis.FeatureSelection {
 
     public override IDeepCloneable Clone(Cloner cloner) {
       FeatureSelectionProblem clone = (FeatureSelectionProblem)base.Clone(cloner);
-      clone.Initialize();
+      clone.operators = operators.Select(x => (IOperator)cloner.Clone(x)).ToList();
+      clone.AttachEventHandlers();
       return clone;
     }
 
@@ -164,8 +167,10 @@ namespace HeuristicLab.Problems.DataAnalysis.FeatureSelection {
     #endregion
 
     #region Helpers
-    private void Initialize() {
-      InitializeOperators();
+    private void AttachEventHandlers() {
+      // Start BackwardsCompatibility3.3 (remove with 3.4)
+      if (operators == null) InitializeOperators();
+      // End BackwardsCompatibility3.3
       RegisterParameterEvents();
       RegisterParameterValueEvents();
     }

commit 64c4f58b8e1edd03fa506e25456f3400097075f9

#1090  * Fixed problem plugins reloading their operators on deserialization in following problems (forgot on them in the first commit)   * `SupportVectorRegressionProblem`   * `SymbolicTimeSeriesPrognosisProblem`  * Fixed a bug in the `FeatureSelectionProblem` introduced in r4098  * Fixed the issues mentioned in the code review of mkommend
git-svn-id: https://src.heuristiclab.com/svn/core/trunk@4118 2abd9481-f8db-48e9-bd25-06bc13291c1b

sources/HeuristicLab.Problems.ArtificialAnt/3.3/ArtificialAntProblem.cs
@@ -277,10 +277,15 @@ namespace HeuristicLab.Problems.ArtificialAnt {
 
     #region Helpers
     [StorableHook(HookType.AfterDeserialization)]
-    private void AttachEventHandlers() {
-      // Start BackwardsCompatibility3.3 (remove with 3.4)
+    private void AfterDeserializationHook() {
+      // BackwardsCompatibility3.3
+      #region Backwards compatible code (remove with 3.4)
       if (operators == null) InitializeOperators();
-      // End BackwardsCompatibility3.3
+      #endregion
+      AttachEventHandlers();
+    }
+
+    private void AttachEventHandlers() {
       SolutionCreatorParameter.ValueChanged += new EventHandler(SolutionCreatorParameter_ValueChanged);
       SolutionCreator.SymbolicExpressionTreeParameter.ActualNameChanged += new EventHandler(SolutionCreator_SymbolicExpressionTreeParameter_ActualNameChanged);
       EvaluatorParameter.ValueChanged += new EventHandler(EvaluatorParameter_ValueChanged);

sources/HeuristicLab.Problems.DataAnalysis.FeatureSelection/3.3/FeatureSelectionProblem.cs
@@ -89,7 +89,7 @@ namespace HeuristicLab.Problems.DataAnalysis.FeatureSelection {
     private List<IOperator> operators;
 
     [StorableConstructor]
-    private FeatureSelectionProblem(bool deserializing) : base() { }
+    protected FeatureSelectionProblem(bool deserializing) : base(deserializing) { }
     public FeatureSelectionProblem()
       : base() {
       BinaryVectorCreator creator = new RandomBinaryVectorCreator();
@@ -105,11 +105,16 @@ namespace HeuristicLab.Problems.DataAnalysis.FeatureSelection {
       ParameterizeEvaluator();
 
       InitializeOperators();
-      AttachEventHandlers();
+      RegisterParameterEvents();
+      RegisterParameterValueEvents();
     }
 
     [StorableHook(HookType.AfterDeserialization)]
     private void AfterDeserializationHook() {
+      // BackwardsCompatibility3.3
+      #region Backwards compatible code (remove with 3.4)
+      if (operators == null) InitializeOperators();
+      #endregion
       RegisterParameterEvents();
       RegisterParameterValueEvents();
     }
@@ -117,7 +122,8 @@ namespace HeuristicLab.Problems.DataAnalysis.FeatureSelection {
     public override IDeepCloneable Clone(Cloner cloner) {
       FeatureSelectionProblem clone = (FeatureSelectionProblem)base.Clone(cloner);
       clone.operators = operators.Select(x => (IOperator)cloner.Clone(x)).ToList();
-      clone.AttachEventHandlers();
+      clone.RegisterParameterEvents();
+      clone.RegisterParameterValueEvents();
       return clone;
     }
 
@@ -167,14 +173,6 @@ namespace HeuristicLab.Problems.DataAnalysis.FeatureSelection {
     #endregion
 
     #region Helpers
-    private void AttachEventHandlers() {
-      // Start BackwardsCompatibility3.3 (remove with 3.4)
-      if (operators == null) InitializeOperators();
-      // End BackwardsCompatibility3.3
-      RegisterParameterEvents();
-      RegisterParameterValueEvents();
-    }
-
     private void InitializeOperators() {
       operators = new List<IOperator>();
       operators.AddRange(ApplicationManager.Manager.GetInstances<IBinaryVectorOperator>().OfType<IOperator>());

sources/HeuristicLab.Problems.DataAnalysis.MultiVariate.Regression/3.3/Symbolic/MultiObjectiveSymbolicVectorRegressionProblem.cs
@@ -65,6 +65,8 @@ namespace HeuristicLab.Problems.DataAnalysis.MultiVariate.Regression.Symbolic {
     }
     #endregion
 
+    [StorableConstructor]
+    protected MultiObjectiveSymbolicVectorRegressionProblem(bool deserializing) : base(deserializing) { }
     public MultiObjectiveSymbolicVectorRegressionProblem()
       : base() {
       var evaluator = new SymbolicVectorRegressionScaledMseEvaluator();
@@ -74,10 +76,7 @@ namespace HeuristicLab.Problems.DataAnalysis.MultiVariate.Regression.Symbolic {
       ParameterizeEvaluator();
       Initialize();
     }
-
-    [StorableConstructor]
-    private MultiObjectiveSymbolicVectorRegressionProblem(bool deserializing) : base() { }
-
+    
     [StorableHook(HookType.AfterDeserialization)]
     private void AfterDeserializationHook() {
       Initialize();

sources/HeuristicLab.Problems.DataAnalysis.MultiVariate.Regression/3.3/Symbolic/SymbolicVectorRegressionProblem.cs
@@ -118,7 +118,6 @@ namespace HeuristicLab.Problems.DataAnalysis.MultiVariate.Regression.Symbolic {
       set { FunctionTreeGrammarParameter.Value = value; }
     }
 
-    private List<IOperator> operators;
     public override IEnumerable<IOperator> Operators {
       get { return operators; }
     }
@@ -153,7 +152,11 @@ namespace HeuristicLab.Problems.DataAnalysis.MultiVariate.Regression.Symbolic {
 
     [Storable]
     private SymbolicVectorRegressionGrammar grammar;
+    [Storable]
+    private List<IOperator> operators;
 
+    [StorableConstructor]
+    protected SymbolicVectorRegressionProblem(bool deserializing) : base(deserializing) { }
     public SymbolicVectorRegressionProblem()
       : base() {
       SymbolicExpressionTreeCreator creator = new ProbabilisticTreeCreator();
@@ -174,20 +177,26 @@ namespace HeuristicLab.Problems.DataAnalysis.MultiVariate.Regression.Symbolic {
       ParameterizeSolutionCreator();
       UpdateGrammar();
       UpdateEstimationLimits();
-      Initialize();
+      InitializeOperators();
+      RegisterParameterEvents();
+      RegisterParameterValueEvents();
     }
-
-    [StorableConstructor]
-    private SymbolicVectorRegressionProblem(bool deserializing) : base() { }
 
     [StorableHook(HookType.AfterDeserialization)]
     private void AfterDeserializationHook() {
-      Initialize();
+      // BackwardsCompatibility3.3
+      #region Backwards compatible code (remove with 3.4)
+      if (operators == null) InitializeOperators();
+      #endregion
+      RegisterParameterEvents();
+      RegisterParameterValueEvents();
     }
 
     public override IDeepCloneable Clone(Cloner cloner) {
       SymbolicVectorRegressionProblem clone = (SymbolicVectorRegressionProblem)base.Clone(cloner);
-      clone.Initialize();
+      clone.operators = operators.Select(x => (IOperator)cloner.Clone(x)).ToList();
+      clone.RegisterParameterEvents();
+      clone.RegisterParameterValueEvents();
       return clone;
     }
 
@@ -253,12 +262,6 @@ namespace HeuristicLab.Problems.DataAnalysis.MultiVariate.Regression.Symbolic {
     #region Helpers
     protected void AddOperator(IOperator op) {
       operators.Add(op);
-    }
-
-    private void Initialize() {
-      InitializeOperators();
-      RegisterParameterEvents();
-      RegisterParameterValueEvents();
     }
 
     private void UpdateGrammar() {

sources/HeuristicLab.Problems.DataAnalysis.MultiVariate.TimeSeriesPrognosis/3.3/Symbolic/SingleObjectiveSymbolicTimeSeriesPrognosisProblem.cs
@@ -84,7 +84,8 @@ namespace HeuristicLab.Problems.DataAnalysis.MultiVariate.TimeSeriesPrognosis.Sy
     }
     #endregion
 
-
+    [StorableConstructor]
+    protected SingleObjectiveSymbolicTimeSeriesPrognosisProblem(bool deserializing) : base(deserializing) { }
     public SingleObjectiveSymbolicTimeSeriesPrognosisProblem()
       : base() {
       var evaluator = new SymbolicTimeSeriesPrognosisScaledNormalizedMseEvaluator();
@@ -95,20 +96,18 @@ namespace HeuristicLab.Problems.DataAnalysis.MultiVariate.TimeSeriesPrognosis.Sy
 
       ParameterizeEvaluator();
 
-      Initialize();
+      InitializeOperators();
+      AttachEventHandlers();
     }
-
-    [StorableConstructor]
-    protected SingleObjectiveSymbolicTimeSeriesPrognosisProblem(bool deserializing) : base(deserializing) { }
 
     [StorableHook(HookType.AfterDeserialization)]
     private void AfterDeserializationHook() {
-      Initialize();
+      AttachEventHandlers();
     }
 
     public override IDeepCloneable Clone(Cloner cloner) {
       SingleObjectiveSymbolicTimeSeriesPrognosisProblem clone = (SingleObjectiveSymbolicTimeSeriesPrognosisProblem)base.Clone(cloner);
-      clone.Initialize();
+      clone.AttachEventHandlers();
       return clone;
     }
 
@@ -134,8 +133,7 @@ namespace HeuristicLab.Problems.DataAnalysis.MultiVariate.TimeSeriesPrognosis.Sy
     #endregion
 
     #region Helpers
-    private void Initialize() {
-      InitializeOperators();
+    private void AttachEventHandlers() {
     }
 
     private void InitializeOperators() {

sources/HeuristicLab.Problems.DataAnalysis.MultiVariate.TimeSeriesPrognosis/3.3/Symbolic/SymbolicTimeSeriesPrognosisProblem.cs
@@ -121,7 +121,6 @@ namespace HeuristicLab.Problems.DataAnalysis.MultiVariate.TimeSeriesPrognosis.Sy
       get { return (ISymbolicExpressionGrammar)FunctionTreeGrammarParameter.Value; }
       set { FunctionTreeGrammarParameter.Value = value; }
     }
-    private List<IOperator> operators;
     public override IEnumerable<IOperator> Operators {
       get { return operators; }
     }
@@ -156,7 +155,11 @@ namespace HeuristicLab.Problems.DataAnalysis.MultiVariate.TimeSeriesPrognosis.Sy
 
     [Storable]
     private SymbolicTimeSeriesPrognosisGrammar grammar;
+    [Storable]
+    private List<IOperator> operators;
 
+    [StorableConstructor]
+    protected SymbolicTimeSeriesPrognosisProblem(bool deserializing) : base(deserializing) { }
     public SymbolicTimeSeriesPrognosisProblem()
       : base() {
       SymbolicExpressionTreeCreator creator = new ProbabilisticTreeCreator();
@@ -178,21 +181,27 @@ namespace HeuristicLab.Problems.DataAnalysis.MultiVariate.TimeSeriesPrognosis.Sy
       ParameterizeSolutionCreator();
 
       UpdateGrammar();
-      Initialize();
+      InitializeOperators();
+      RegisterParameterEvents();
+      RegisterParameterValueEvents();
     }
-
-    [StorableConstructor]
-    protected SymbolicTimeSeriesPrognosisProblem(bool deserializing) : base(deserializing) { }
 
     [StorableHook(HookType.AfterDeserialization)]
     private void AfterDeserializationHook() {
-      Initialize();
+      // BackwardsCompatibility3.3
+      #region Backwards compatible code (remove with 3.4)
+      if (operators == null) InitializeOperators();
+      #endregion
+      RegisterParameterEvents();
+      RegisterParameterValueEvents();
     }
 
     public override IDeepCloneable Clone(Cloner cloner) {
       SymbolicTimeSeriesPrognosisProblem clone = (SymbolicTimeSeriesPrognosisProblem)base.Clone(cloner);
+      clone.operators = operators.Select(x => (IOperator)cloner.Clone(x)).ToList();
       clone.grammar = (SymbolicTimeSeriesPrognosisGrammar)cloner.Clone(grammar);
-      clone.Initialize();
+      clone.RegisterParameterEvents();
+      clone.RegisterParameterValueEvents();
       return clone;
     }
 
@@ -252,12 +261,6 @@ namespace HeuristicLab.Problems.DataAnalysis.MultiVariate.TimeSeriesPrognosis.Sy
     #region Helpers
     protected void AddOperator(IOperator op) {
       operators.Add(op);
-    }
-
-    private void Initialize() {
-      InitializeOperators();
-      RegisterParameterEvents();
-      RegisterParameterValueEvents();
     }
 
     private void UpdateGrammar() {

sources/HeuristicLab.Problems.DataAnalysis.Regression/3.3/Symbolic/SymbolicRegressionProblem.cs
@@ -182,7 +182,7 @@ namespace HeuristicLab.Problems.DataAnalysis.Regression.Symbolic {
     private List<IOperator> operators;
 
     [StorableConstructor]
-    private SymbolicRegressionProblem(bool deserializing) : base() { }
+    protected SymbolicRegressionProblem(bool deserializing) : base(deserializing) { }
     public SymbolicRegressionProblem()
       : base() {
       SymbolicExpressionTreeCreator creator = new ProbabilisticTreeCreator();
@@ -212,13 +212,15 @@ namespace HeuristicLab.Problems.DataAnalysis.Regression.Symbolic {
       UpdateGrammar();
       UpdateEstimationLimits();
       InitializeOperators();
-      AttachEventHandlers();
+      RegisterParameterEvents();
+      RegisterParameterValueEvents();
     }
 
     public override IDeepCloneable Clone(Cloner cloner) {
       SymbolicRegressionProblem clone = (SymbolicRegressionProblem)base.Clone(cloner);
       clone.operators = operators.Select(x => (IOperator)cloner.Clone(x)).ToList();
-      clone.AttachEventHandlers();
+      clone.RegisterParameterEvents();
+      clone.RegisterParameterValueEvents();
       return clone;
     }
 
@@ -302,10 +304,11 @@ namespace HeuristicLab.Problems.DataAnalysis.Regression.Symbolic {
 
     #region Helpers
     [StorableHook(HookType.AfterDeserialization)]
-    private void AttachEventHandlers() {
-      // Start BackwardsCompatibility3.3 (remove with 3.4)
+    private void AfterDeserializationHook() {
+      // BackwardsCompatibility3.3
+      #region Backwards compatible code (remove with 3.4)
       if (operators == null) InitializeOperators();
-      // End BackwardsCompatibility3.3
+      #endregion
       RegisterParameterEvents();
       RegisterParameterValueEvents();
     }

sources/HeuristicLab.Problems.DataAnalysis/3.3/DataAnalysisProblem.cs
@@ -49,15 +49,14 @@ namespace HeuristicLab.Problems.DataAnalysis {
     }
     #endregion
 
+    [StorableConstructor]
+    protected DataAnalysisProblem(bool deserializing) : base(deserializing) { }
     public DataAnalysisProblem()
       : base() {
       Parameters.Add(new ValueParameter<DataAnalysisProblemData>(DataAnalysisProblemDataParameterName, "The data set, target variable and input variables of the data analysis problem.", new DataAnalysisProblemData()));
       RegisterParameterEvents();
       RegisterParameterValueEvents();
     }
-
-    [StorableConstructor]
-    private DataAnalysisProblem(bool deserializing) : base() { }
 
     [StorableHook(HookType.AfterDeserialization)]
     private void AfterDeserializationHook() {

sources/HeuristicLab.Problems.DataAnalysis/3.3/SupportVectorMachine/ParameterAdjustmentProblem/SupportVectorMachineParameterAdjustmentProblem.cs
@@ -128,7 +128,7 @@ namespace HeuristicLab.Problems.DataAnalysis.SupportVectorMachine.ParameterAdjus
     private StdDevStrategyVectorManipulator strategyVectorManipulator;
 
     [StorableConstructor]
-    private SupportVectorMachineParameterAdjustmentProblem(bool deserializing) : base() { }
+    private SupportVectorMachineParameterAdjustmentProblem(bool deserializing) : base(deserializing) { }
     public SupportVectorMachineParameterAdjustmentProblem()
       : base() {
       UniformRandomRealVectorCreator creator = new UniformRandomRealVectorCreator();
@@ -215,10 +215,15 @@ namespace HeuristicLab.Problems.DataAnalysis.SupportVectorMachine.ParameterAdjus
 
     #region Helpers
     [StorableHook(HookType.AfterDeserialization)]
-    private void AttachEventHandlers() {
-      // Start BackwardsCompatibility3.3 (remove with 3.4)
+    private void AfterDeserializationHook() {
+      // BackwardsCompatibility3.3
+      #region Backwards compatible code (remove with 3.4)
       if (operators == null) InitializeOperators();
-      // End BackwardsCompatibility3.3
+      #endregion
+      AttachEventHandlers();
+    }
+
+    private void AttachEventHandlers() {
       SolutionCreatorParameter.ValueChanged += new EventHandler(SolutionCreatorParameter_ValueChanged);
       SolutionCreator.RealVectorParameter.ActualNameChanged += new EventHandler(SolutionCreator_RealVectorParameter_ActualNameChanged);
       EvaluatorParameter.ValueChanged += new EventHandler(EvaluatorParameter_ValueChanged);

sources/HeuristicLab.Problems.ExternalEvaluation/3.3/ExternalEvaluationProblem.cs
@@ -192,6 +192,10 @@ namespace HeuristicLab.Problems.ExternalEvaluation {
 
     #region Helpers
     [StorableHook(HookType.AfterDeserialization)]
+    private void AfterDeserializationHook() {
+      AttachEventHandlers();
+    }
+
     private void AttachEventHandlers() {
       SolutionCreatorParameter.ValueChanged += new EventHandler(SolutionCreatorParameter_ValueChanged);
       EvaluatorParameter.ValueChanged += new EventHandler(EvaluatorParameter_ValueChanged);

sources/HeuristicLab.Problems.Knapsack/3.3/KnapsackProblem.cs
@@ -137,7 +137,7 @@ namespace HeuristicLab.Problems.Knapsack {
     private List<IOperator> operators;
 
     [StorableConstructor]
-    private KnapsackProblem(bool deserializing) : base() { }
+    private KnapsackProblem(bool deserializing) : base(deserializing) { }
     public KnapsackProblem()
       : base() {
       RandomBinaryVectorCreator creator = new RandomBinaryVectorCreator();
@@ -254,10 +254,15 @@ namespace HeuristicLab.Problems.Knapsack {
 
     #region Helpers
     [StorableHook(HookType.AfterDeserialization)]
-    private void AttachEventHandlers() {
-      // Start BackwardsCompatibility3.3 (remove with 3.4)
+    private void AfterDeserializationHook() {
+      // BackwardsCompatibility3.3
+      #region Backwards compatible code (remove with 3.4)
       if (operators == null) InitializeOperators();
-      // End BackwardsCompatibility3.3
+      #endregion
+      AttachEventHandlers();
+    }
+
+    private void AttachEventHandlers() {
       SolutionCreatorParameter.ValueChanged += new EventHandler(SolutionCreatorParameter_ValueChanged);
       SolutionCreator.BinaryVectorParameter.ActualNameChanged += new EventHandler(SolutionCreator_BinaryVectorParameter_ActualNameChanged);
       EvaluatorParameter.ValueChanged += new EventHandler(EvaluatorParameter_ValueChanged);

sources/HeuristicLab.Problems.OneMax/3.3/OnemaxProblem.cs
@@ -108,7 +108,7 @@ namespace HeuristicLab.Problems.OneMax {
     private List<IOperator> operators;
 
     [StorableConstructor]
-    private OneMaxProblem(bool deserializing) : base() { }
+    private OneMaxProblem(bool deserializing) : base(deserializing) { }
     public OneMaxProblem()
       : base() {
       RandomBinaryVectorCreator creator = new RandomBinaryVectorCreator();
@@ -197,18 +197,23 @@ namespace HeuristicLab.Problems.OneMax {
 
     #region Helpers
     [StorableHook(HookType.AfterDeserialization)]
-    private void AttachEventHandlers() {
-      // Start BackwardsCompatibility3.3 (remove with 3.4)
+    private void AfterDeserializationHook() {
+      // BackwardsCompatibility3.3
+      #region Backwards compatible code (remove with 3.4)
       if (operators == null) InitializeOperators();
-      // End BackwardsCompatibility3.3
+      #endregion
+      AttachEventHandlers();
+    }
+
+    private void AttachEventHandlers() {
       SolutionCreatorParameter.ValueChanged += new EventHandler(SolutionCreatorParameter_ValueChanged);
       SolutionCreator.BinaryVectorParameter.ActualNameChanged += new EventHandler(SolutionCreator_BinaryVectorParameter_ActualNameChanged);
       EvaluatorParameter.ValueChanged += new EventHandler(EvaluatorParameter_ValueChanged);
       LengthParameter.ValueChanged += new EventHandler(LengthParameter_ValueChanged);
       LengthParameter.Value.ValueChanged += new EventHandler(Length_ValueChanged);
-      BestKnownQualityParameter.Value.Value = Length.Value;
       BestKnownQualityParameter.ValueChanged += new EventHandler(BestKnownQualityParameter_ValueChanged);
     }
+
     private void ParameterizeSolutionCreator() {
       SolutionCreator.LengthParameter.ActualName = LengthParameter.Name;
     }

sources/HeuristicLab.Problems.TestFunctions/3.3/SingleObjectiveTestFunctionProblem.cs
@@ -130,7 +130,7 @@ namespace HeuristicLab.Problems.TestFunctions {
     private List<IOperator> operators;
 
     [StorableConstructor]
-    private SingleObjectiveTestFunctionProblem(bool deserializing) : base() { }
+    private SingleObjectiveTestFunctionProblem(bool deserializing) : base(deserializing) { }
     public SingleObjectiveTestFunctionProblem()
       : base() {
       UniformRandomRealVectorCreator creator = new UniformRandomRealVectorCreator();
@@ -296,10 +296,15 @@ namespace HeuristicLab.Problems.TestFunctions {
 
     #region Helpers
     [StorableHook(HookType.AfterDeserialization)]
-    private void AttachEventHandlers() {
-      // Start BackwardsCompatibility3.3 (remove with 3.4)
+    private void AfterDeserializationHook() {
+      // BackwardsCompatibility3.3
+      #region Backwards compatible code (remove with 3.4)
       if (operators == null) InitializeOperators();
-      // End BackwardsCompatibility3.3
+      #endregion
+      AttachEventHandlers();
+    }
+
+    private void AttachEventHandlers() {
       ProblemSizeParameter.ValueChanged += new EventHandler(ProblemSizeParameter_ValueChanged);
       ProblemSize.ValueChanged += new EventHandler(ProblemSize_ValueChanged);
       BoundsParameter.ValueChanged += new EventHandler(BoundsParameter_ValueChanged);

sources/HeuristicLab.Problems.TravelingSalesman/3.3/TravelingSalesmanProblem.cs
@@ -131,7 +131,7 @@ namespace HeuristicLab.Problems.TravelingSalesman {
     private List<IOperator> operators;
 
     [StorableConstructor]
-    private TravelingSalesmanProblem(bool deserializing) : base() { }
+    private TravelingSalesmanProblem(bool deserializing) : base(deserializing) { }
     public TravelingSalesmanProblem()
       : base() {
       RandomPermutationCreator creator = new RandomPermutationCreator();
@@ -245,10 +245,15 @@ namespace HeuristicLab.Problems.TravelingSalesman {
 
     #region Helpers
     [StorableHook(HookType.AfterDeserialization)]
-    private void AttachEventHandlers() {
-      // Start BackwardsCompatibility3.3 (remove with 3.4)
+    private void AfterDeserializationHook() {
+      // BackwardsCompatibility3.3
+      #region Backwards compatible code (remove with 3.4)
       if (operators == null) InitializeOperators();
-      // End BackwardsCompatibility3.3
+      #endregion
+      AttachEventHandlers();
+    }
+
+    private void AttachEventHandlers() {
       CoordinatesParameter.ValueChanged += new EventHandler(CoordinatesParameter_ValueChanged);
       Coordinates.ItemChanged += new EventHandler<EventArgs<int, int>>(Coordinates_ItemChanged);
       Coordinates.Reset += new EventHandler(Coordinates_Reset);

sources/HeuristicLab.Problems.VehicleRouting/3.3/VehicleRoutingProblem.cs
@@ -181,7 +181,7 @@ namespace HeuristicLab.Problems.VehicleRouting {
     private List<IOperator> operators;
 
     [StorableConstructor]
-    private VehicleRoutingProblem(bool deserializing) : base() { }
+    private VehicleRoutingProblem(bool deserializing) : base(deserializing) { }
     public VehicleRoutingProblem()
       : base() {
       IVRPCreator creator = new AlbaPermutationCreator();
@@ -294,10 +294,15 @@ namespace HeuristicLab.Problems.VehicleRouting {
 
     #region Helpers
     [StorableHook(HookType.AfterDeserialization)]
-    private void AttachEventHandlers() {
-      // Start BackwardsCompatibility3.3 (remove with 3.4)
+    private void AfterDeserializationHook() {
+      // BackwardsCompatibility3.3
+      #region Backwards compatible code (remove with 3.4)
       if (operators == null) InitializeOperators();
-      // End BackwardsCompatibility3.3
+      #endregion
+      AttachEventHandlers();
+    }
+
+    private void AttachEventHandlers() {
       CoordinatesParameter.ValueChanged += new EventHandler(CoordinatesParameter_ValueChanged);
       Vehicles.ValueChanged += new EventHandler(VehiclesValue_ValueChanged);
       Coordinates.ItemChanged += new EventHandler<EventArgs<int, int>>(Coordinates_ItemChanged);