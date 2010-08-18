72b264197c5538b9cb59187855475e796c7331d2
Stefan Wagner <swagner@heuristiclab.com>
2010-08-18 22:59:26 +0000

    Moved HeuristicLab.Services projects into a separate solution (#1160)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@4256 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Services.Deployment.DataAccess/3.3/Properties/Settings.Designer.cs
+++ sources/HeuristicLab.Services.Deployment.DataAccess/3.3/Properties/Settings.Designer.cs
@@ -26,7 +26,7 @@ namespace HeuristicLab.Services.Deployment.DataAccess.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Server=SERVDEV;Database=HeuristicLab.Deployment;Integrated Security=SSPI")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Server=SERVICES;Database=HeuristicLab.Deployment;Integrated Security=SSPI")]
         public string HeuristicLab_PluginStoreConnectionString {
             get {
                 return ((string)(this["HeuristicLab_PluginStoreConnectionString"]));
