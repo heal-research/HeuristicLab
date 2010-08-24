2d4f6651d4e25f40836a47ea860e9bb7bad829c5
Stefan Wagner <swagner@heuristiclab.com>
2010-08-24 22:53:15 +0000

    Worked on OKB user authentication (#1167)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@4310 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Services.OKB.DataAccess/3.3/Properties/Settings.Designer.cs
+++ sources/HeuristicLab.Services.OKB.DataAccess/3.3/Properties/Settings.Designer.cs
@@ -26,7 +26,8 @@ namespace HeuristicLab.Services.OKB.DataAccess.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=localhost;Initial Catalog=HeuristicLab.OKB;Integrated Security=SSPI")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SQLEXPRESS;Integrated Security=SSPI;Initial Catalog=HeuristicLab.OK" +
+            "B")]
         public string OKBConnectionString {
             get {
                 return ((string)(this["OKBConnectionString"]));
