0d269a2e6eca4ac8596a557e929900bc682422cf
Michael Kommenda <michael.kommenda@fh-ooe.at>
2010-10-18 21:14:36 +0000

    Added first version of `EnhancedChart` (ticket #1237).
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@4614 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ sources/HeuristicLab.Visualization.ChartControlsExtensions/3.3/EnhancedChart.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace HeuristicLab.Visualization.ChartControlsExtensions {
+  partial class EnhancedChart {
+    /// <summary> 
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Component Designer generated code
+
+    /// <summary> 
+    /// Required method for Designer support - do not modify 
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      this.components = new System.ComponentModel.Container();
+      this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+      this.saveImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.copyImageToClipboardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.contextMenuStrip.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // contextMenuStrip
+      // 
+      this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveImageToolStripMenuItem,
+            this.copyImageToClipboardToolStripMenuItem});
+      this.contextMenuStrip.Name = "contextMenuStrip";
+      this.contextMenuStrip.Size = new System.Drawing.Size(208, 48);
+      // 
+      // saveImageToolStripMenuItem
+      // 
+      this.saveImageToolStripMenuItem.Name = "saveImageToolStripMenuItem";
+      this.saveImageToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+      this.saveImageToolStripMenuItem.Text = "Save Image";
+      this.saveImageToolStripMenuItem.Click += new System.EventHandler(this.saveImageToolStripMenuItem_Click);
+      // 
+      // copyImageToClipboardToolStripMenuItem
+      // 
+      this.copyImageToClipboardToolStripMenuItem.Name = "copyImageToClipboardToolStripMenuItem";
+      this.copyImageToClipboardToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+      this.copyImageToClipboardToolStripMenuItem.Text = "Copy Image to Clipboard";
+      this.copyImageToClipboardToolStripMenuItem.Click += new System.EventHandler(this.copyImageToClipboardToolStripMenuItem_Click);
+      this.contextMenuStrip.ResumeLayout(false);
+      ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+    private System.Windows.Forms.ToolStripMenuItem saveImageToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem copyImageToClipboardToolStripMenuItem;
+  }
+}
---
+++ sources/HeuristicLab.Visualization.ChartControlsExtensions/3.3/EnhancedChart.cs
@@ -0,0 +1,71 @@
+﻿#region License Information
+/* HeuristicLab
+ * Copyright (C) 2002-2010 Heuristic and Evolutionary Algorithms Laboratory (HEAL)
+ *
+ * This file is part of HeuristicLab.
+ *
+ * HeuristicLab is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HeuristicLab is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HeuristicLab. If not, see <http://www.gnu.org/licenses/>.
+ */
+#endregion
+
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+namespace HeuristicLab.Visualization.ChartControlsExtensions {
+  public partial class EnhancedChart : Chart {
+    public EnhancedChart()
+      : base() {
+    }
+
+    private void saveImageToolStripMenuItem_Click(object sender, System.EventArgs e) {
+      SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+      // Sets the current file name filter string, which determines 
+      // the choices that appear in the "Save as file type" or 
+      // "Files of type" box in the dialog box.
+      saveFileDialog.Filter = "Bitmap (*.bmp)|*.bmp|JPEG (*.jpg)|*.jpg|EMF (*.emf)|*.emf|PNG (*.png)|*.png|SVG (*.svg)|*.svg|GIF (*.gif)|*.gif|TIFF (*.tif)|*.tif";
+      saveFileDialog.FilterIndex = 2;
+      saveFileDialog.RestoreDirectory = true;
+
+      // Set image file format
+      if (saveFileDialog.ShowDialog() == DialogResult.OK) {
+        ChartImageFormat format = ChartImageFormat.Bmp;
+
+        if (saveFileDialog.FileName.EndsWith("bmp")) {
+          format = ChartImageFormat.Bmp;
+        } else if (saveFileDialog.FileName.EndsWith("jpg")) {
+          format = ChartImageFormat.Jpeg;
+        } else if (saveFileDialog.FileName.EndsWith("emf")) {
+          format = ChartImageFormat.EmfDual;
+        } else if (saveFileDialog.FileName.EndsWith("gif")) {
+          format = ChartImageFormat.Gif;
+        } else if (saveFileDialog.FileName.EndsWith("png")) {
+          format = ChartImageFormat.Png;
+        } else if (saveFileDialog.FileName.EndsWith("tif")) {
+          format = ChartImageFormat.Tiff;
+        }
+
+        // Save image
+        SaveImage(saveFileDialog.FileName, format);
+      }
+    }
+
+    private void copyImageToClipboardToolStripMenuItem_Click(object sender, System.EventArgs e) {
+      System.IO.MemoryStream stream = new System.IO.MemoryStream();
+      SaveImage(stream, System.Drawing.Imaging.ImageFormat.Bmp);
+      Bitmap bmp = new Bitmap(stream);
+      Clipboard.SetDataObject(bmp);
+    }
+  }
+}
