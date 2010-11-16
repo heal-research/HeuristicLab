45a0917a5edc79fc1585f19eec2b92723bc3c98b
Michael Kommenda <michael.kommenda@fh-ooe.at>
2010-11-16 12:51:54 +0000

    Extracted `ColorGradient` in a separate class (ticket #1261).
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk/sources@4808 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- HeuristicLab.Optimization.Views/3.3/RunCollectionBubbleChartView.cs
+++ HeuristicLab.Optimization.Views/3.3/RunCollectionBubbleChartView.cs
@@ -579,38 +579,13 @@ namespace HeuristicLab.Optimization.Views {
     #endregion
 
     #region automatic coloring
-    private static Color[] colors;
-    protected static Color[] Colors {
-      get {
-        if (colors == null) InitializeColors();
-        return colors;
-      }
-    }
-    private static void InitializeColors() {
-      colors = new Color[256];
-      int stepWidth = (256 * 4) / colors.Length;
-      int currentValue;
-      int currentClass;
-      for (int i = 0; i < colors.Length; i++) {
-        currentValue = (i * stepWidth) % 256;
-        currentClass = (i * stepWidth) / 256;
-        switch (currentClass) {
-          case 0: { colors[i] = Color.FromArgb(0, currentValue, 255); break; }        // blue -> cyan
-          case 1: { colors[i] = Color.FromArgb(0, 255, 255 - currentValue); break; }  // cyan -> green
-          case 2: { colors[i] = Color.FromArgb(currentValue, 255, 0); break; }        // green -> yellow
-          case 3: { colors[i] = Color.FromArgb(255, 255 - currentValue, 0); break; }  // yellow -> red
-        }
-      }
-    }
-
     private void colorXAxisButton_Click(object sender, EventArgs e) {
       double minValue = chart.Series[0].Points.Min(p => p.XValue);
       double maxValue = chart.Series[0].Points.Max(p => p.XValue);
       foreach (DataPoint point in chart.Series[0].Points) {
-        int colorIndex = (int)((Colors.Length - 1) * (point.XValue - minValue) / (maxValue - minValue));
+        int colorIndex = (int)((ColorGradient.Colors.Count - 1) * (point.XValue - minValue) / (maxValue - minValue));
         IRun run = point.Tag as IRun;
-        if (run != null)
-          run.Color = Colors[colorIndex];
+        if (run != null) run.Color = ColorGradient.Colors[colorIndex];
       }
     }
 
@@ -618,10 +593,9 @@ namespace HeuristicLab.Optimization.Views {
       double minValue = chart.Series[0].Points.Min(p => p.YValues[0]);
       double maxValue = chart.Series[0].Points.Max(p => p.YValues[0]);
       foreach (DataPoint point in chart.Series[0].Points) {
-        int colorIndex = (int)((Colors.Length - 1) * (point.YValues[0] - minValue) / (maxValue - minValue));
+        int colorIndex = (int)((ColorGradient.Colors.Count - 1) * (point.YValues[0] - minValue) / (maxValue - minValue));
         IRun run = point.Tag as IRun;
-        if (run != null)
-          run.Color = Colors[colorIndex];
+        if (run != null) run.Color = ColorGradient.Colors[colorIndex];
       }
     }
     #endregion
