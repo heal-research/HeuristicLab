16ac285b04e2edbaf65d03d55a714271e8a27248
Erik Pitzer <erik.pitzer@fh-hagenberg.at>
2010-11-17 14:06:41 +0000

    Add ProgrammableSingleSuccessorOperator that properly handles a successor if present (#1275)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk/sources@4828 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ HeuristicLab.Operators.Programmable/3.3/ProgrammableSingleSuccessorOperator.cs
@@ -0,0 +1,39 @@
+﻿
+using HeuristicLab.Common;
+using HeuristicLab.Core;
+using HeuristicLab.Parameters;
+using HeuristicLab.Persistence.Default.CompositeSerializers.Storable;
+namespace HeuristicLab.Operators.Programmable {
+
+  [Item("ProgrammableSingleSuccessorOperator", "An operator that can be programmed for arbitrary needs and handle a single successor.")]
+  [StorableClass]
+  public class ProgrammableSingleSuccessorOperator : ProgrammableOperator {
+
+    [StorableConstructor]
+    protected ProgrammableSingleSuccessorOperator(bool deserializing) : base(deserializing) { }
+    protected ProgrammableSingleSuccessorOperator(ProgrammableSingleSuccessorOperator original, Cloner cloner)
+      : base(original, cloner) {
+    }
+    public ProgrammableSingleSuccessorOperator()
+      : base() {
+      Parameters.Add(new OperatorParameter("Successor", "Operator that is executed next."));
+    }
+
+    public override IDeepCloneable Clone(Cloner cloner) {
+      return new ProgrammableSingleSuccessorOperator(this, cloner);
+    }
+
+    public override IOperation Apply() {
+      IOperation operation = base.Apply();
+      if (operation != null)
+        return operation;
+      IParameter parameter;
+      Parameters.TryGetValue("Successor", out parameter);
+      OperatorParameter successorParameter = parameter as OperatorParameter;
+      if (successorParameter != null && successorParameter.Value != null)
+        return ExecutionContext.CreateOperation(successorParameter.Value);
+      else
+        return null;
+    }
+  }
+}
