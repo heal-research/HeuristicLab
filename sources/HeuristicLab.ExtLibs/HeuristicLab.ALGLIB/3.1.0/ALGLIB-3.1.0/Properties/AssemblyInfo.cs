5240c2181a0b964d7382536ee809d6f1f01f90de
Gabriel Kronberger <gabriel.kronberger@fh-hagenberg.at>
2010-11-28 13:05:52 +0000

    Updated .frame and license files. #875
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@4976 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.ExtLibs/HeuristicLab.ALGLIB/3.1.0/ALGLIB-3.1.0/Properties/AssemblyInfo.cs
+++ sources/HeuristicLab.ExtLibs/HeuristicLab.ALGLIB/3.1.0/ALGLIB-3.1.0/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ALGLIB-2.5.0")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ALGLIB-3.1.0")]
+[assembly: AssemblyDescription("ALGLIB® - numerical analysis library, 1999-2010")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ALGLIB-2.5.0")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyCompany("ALGLIB project")]
+[assembly: AssemblyProduct("ALGLIB-3.1.0")]
+[assembly: AssemblyCopyright("Copyright © Sergey Bochkanov (ALGLIB project) 1999-2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bdca40e7-e549-4082-8352-cc2b7b6bae6b")]
+[assembly: Guid("D68158F2-56A6-41DD-8CBA-FF1153A9CA4A")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.5.0.0")]
-[assembly: AssemblyFileVersion("2.5.0.0")]
+[assembly: AssemblyVersion("3.1.0.0")]
+[assembly: AssemblyFileVersion("3.1.0.0")]
