heal-research/HeuristicLab

commit 59813d0d0d466868ded108f2c6b53da3d687170f

Added a formal parameter for the application-base directory name to !CreateAndInitSandbox method in !SandboxManager. #1297
git-svn-id: https://src.heuristiclab.com/svn/core/trunk@4970 2abd9481-f8db-48e9-bd25-06bc13291c1b

sources/HeuristicLab.PluginInfrastructure/3.3/Sandboxing/SandboxManager.cs
@@ -54,7 +54,7 @@ namespace HeuristicLab.PluginInfrastructure.Sandboxing {
     }
 
     #region ISandboxManager Members
-    public static AppDomain CreateAndInitSandbox(string name) {
+    public static AppDomain CreateAndInitSandbox(string appDomainName, string applicationBase) {
       PermissionSet pset;
 
       #region permission set for sandbox
@@ -78,13 +78,13 @@ namespace HeuristicLab.PluginInfrastructure.Sandboxing {
 
       AppDomainSetup setup = AppDomain.CurrentDomain.SetupInformation;
       //setup.PrivateBinPath = pluginDir;
-      setup.ApplicationBase = name;
+      setup.ApplicationBase = applicationBase;
       setup.ConfigurationFile = AppDomain.CurrentDomain.SetupInformation.ConfigurationFile;
-      AppDomain applicationDomain = AppDomain.CreateDomain(name, AppDomain.CurrentDomain.Evidence, setup, pset, CreateStrongName(Assembly.GetExecutingAssembly()));
+      AppDomain applicationDomain = AppDomain.CreateDomain(appDomainName, AppDomain.CurrentDomain.Evidence, setup, pset, CreateStrongName(Assembly.GetExecutingAssembly()));
       Type applicationManagerType = typeof(DefaultApplicationManager);
       DefaultApplicationManager applicationManager =
         (DefaultApplicationManager)applicationDomain.CreateInstanceAndUnwrap(applicationManagerType.Assembly.FullName, applicationManagerType.FullName, true, BindingFlags.NonPublic | BindingFlags.Instance, null, null, null, null, null);
-      PluginManager pm = new PluginManager(name);
+      PluginManager pm = new PluginManager(appDomainName);
       pm.DiscoverAndCheckPlugins();
       ApplicationDescription[] apps = pm.Applications.Cast<ApplicationDescription>().ToArray();
       PluginDescription[] plugins = pm.Plugins.Cast<PluginDescription>().ToArray();

commit a4a6c73c1c5293a020805a3d249bd938c1cfd7b5

Fixed a bug introduced with r4970. #1297
git-svn-id: https://src.heuristiclab.com/svn/core/trunk@4971 2abd9481-f8db-48e9-bd25-06bc13291c1b

sources/HeuristicLab.PluginInfrastructure/3.3/Sandboxing/SandboxManager.cs
@@ -84,7 +84,7 @@ namespace HeuristicLab.PluginInfrastructure.Sandboxing {
       Type applicationManagerType = typeof(DefaultApplicationManager);
       DefaultApplicationManager applicationManager =
         (DefaultApplicationManager)applicationDomain.CreateInstanceAndUnwrap(applicationManagerType.Assembly.FullName, applicationManagerType.FullName, true, BindingFlags.NonPublic | BindingFlags.Instance, null, null, null, null, null);
-      PluginManager pm = new PluginManager(appDomainName);
+      PluginManager pm = new PluginManager(applicationBase);
       pm.DiscoverAndCheckPlugins();
       ApplicationDescription[] apps = pm.Applications.Cast<ApplicationDescription>().ToArray();
       PluginDescription[] plugins = pm.Plugins.Cast<PluginDescription>().ToArray();