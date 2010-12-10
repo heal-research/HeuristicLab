e45e329efa84670f3baa5b6c55181fa23d69547d
Michael Kommenda <michael.kommenda@fh-ooe.at>
2010-12-10 12:27:28 +0000

    Moved `Dispose` method from Designer.cs to !ContentView.cs (ticket #1326).
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@5079 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.MainForm.WindowsForms/3.3/ContentView.Designer.cs
+++ sources/HeuristicLab.MainForm.WindowsForms/3.3/ContentView.Designer.cs
@@ -25,19 +25,6 @@ namespace HeuristicLab.MainForm.WindowsForms {
     /// Required designer variable.
     /// </summary>
     private System.ComponentModel.IContainer components = null;
-
-    /// <summary> 
-    /// Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-    protected override void Dispose(bool disposing) {
-      if (disposing) {
-        if (Content != null) DeregisterContentEvents();
-        if (components != null) components.Dispose();
-      }
-      base.Dispose(disposing);
-    }
-
     #region Component Designer generated code
 
     /// <summary> 
@@ -55,7 +42,6 @@ namespace HeuristicLab.MainForm.WindowsForms {
       this.ResumeLayout(false);
 
     }
-
     #endregion
   }
 }
--- sources/HeuristicLab.MainForm.WindowsForms/3.3/ContentView.cs
+++ sources/HeuristicLab.MainForm.WindowsForms/3.3/ContentView.cs
@@ -110,5 +110,17 @@ namespace HeuristicLab.MainForm.WindowsForms {
     protected override void SetEnabledStateOfControls() {
       base.SetEnabledStateOfControls();
     }
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing) {
+        if (Content != null) DeregisterContentEvents();
+        if (components != null) components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
   }
 }
