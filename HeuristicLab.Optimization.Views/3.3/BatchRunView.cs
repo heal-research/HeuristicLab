c8e4a70f147934f5e1903e0cabd75a0105bf85e5
Michael Kommenda <michael.kommenda@fh-ooe.at>
2011-02-04 01:16:08 +0000

    #1402 - Refactored `OnClosed` method of `IOptimizer` views.
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk/sources@5420 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- HeuristicLab.Optimization.Views/3.3/BatchRunView.cs
+++ HeuristicLab.Optimization.Views/3.3/BatchRunView.cs
@@ -112,10 +112,10 @@ namespace HeuristicLab.Optimization.Views {
       if ((Content != null) && (Content.ExecutionState == ExecutionState.Started)) {
         //The content must be stopped if no other view showing the content is available
         var optimizers = MainFormManager.MainForm.Views.OfType<IContentView>().Where(v => v != this).Select(v => v.Content).OfType<IOptimizer>();
-        //add nested optimizers
-        optimizers = optimizers.SelectMany(opt => opt.NestedOptimizers).Union(optimizers);
-
-        if (!optimizers.Contains(Content)) Content.Stop();
+        if (!optimizers.Contains(Content)) {
+          var nestedOptimizers = optimizers.SelectMany(opt => opt.NestedOptimizers);
+          if (!nestedOptimizers.Contains(Content)) Content.Stop();
+        }
       }
       base.OnClosed(e);
     }
