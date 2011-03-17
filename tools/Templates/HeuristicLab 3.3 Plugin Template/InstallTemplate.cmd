b5fcf8c2c7fed9a3361f2220e7975ea72bff4973
Andreas Beham <andreas.beham@heuristiclab.com>
2011-03-17 15:42:08 +0000

    #567  * Changed project plugin to create the version folder inside the project folder (a little workaround)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@5740 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- tools/Templates/HeuristicLab 3.3 Plugin Template/InstallTemplate.cmd
+++ tools/Templates/HeuristicLab 3.3 Plugin Template/InstallTemplate.cmd
@@ -1,3 +1,4 @@
 SET ZIPFILENAME=HeuristicLab 3.3 Plugin Template.zip
-"%ProgramFiles%\7-Zip\7z" a "%ZIPFILENAME%" Properties HeuristicLab.Plugin.csproj HeuristicLab.snk HeuristicLabIcon.ico Plugin.cs Plugin.cs.frame PluginProjectTemplate.vstemplate
+REM "%ProgramFiles%\7-Zip\7z" a "%ZIPFILENAME%" AssemblyInfo.cs AssemblyInfo.frame HeuristicLab.Plugin.csproj HeuristicLab.snk HeuristicLabIcon.ico Plugin.cs Plugin.cs.frame PluginProjectTemplate.vstemplate
+"%ProgramFiles%\7-Zip\7z" a "%ZIPFILENAME%" 3.3 HeuristicLabIcon.ico PluginProjectCollectionTemplate.vstemplate
 copy "%ZIPFILENAME%" "%UserProfile%\Documents\Visual Studio 2010\Templates\ProjectTemplates\Visual C#\"
