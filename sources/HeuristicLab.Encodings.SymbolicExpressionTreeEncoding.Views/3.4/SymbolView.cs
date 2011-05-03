2dc1f8d6e65734d6f4b29f20b50bf7a125488da0
Gabriel Kronberger <gabriel.kronberger@fh-hagenberg.at>
2011-05-03 14:55:56 +0000

    #1505 changed input validation in `SymbolView`.
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@6109 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Encodings.SymbolicExpressionTreeEncoding.Views/3.4/SymbolView.cs
+++ sources/HeuristicLab.Encodings.SymbolicExpressionTreeEncoding.Views/3.4/SymbolView.cs
@@ -24,6 +24,7 @@ using System.Windows.Forms;
 using HeuristicLab.Core.Views;
 using HeuristicLab.MainForm;
 using HeuristicLab.MainForm.WindowsForms;
+using System.ComponentModel;
 
 namespace HeuristicLab.Encodings.SymbolicExpressionTreeEncoding.Views {
   [View("Symbol View")]
@@ -66,14 +67,30 @@ namespace HeuristicLab.Encodings.SymbolicExpressionTreeEncoding.Views {
     #endregion
 
     #region control event handlers
-    private void initialFrequencyTextBox_TextChanged(object sender, EventArgs e) {
+    private void initialFrequencyTextBox_KeyDown(object sender, KeyEventArgs e) {
+      if ((e.KeyCode == Keys.Enter) || (e.KeyCode == Keys.Return))
+        initialFrequencyLabel.Select();  // select label to validate data
+
+      if (e.KeyCode == Keys.Escape) {
+        initialFrequencyTextBox.Text = Content.InitialFrequency.ToString();
+        initialFrequencyLabel.Select();  // select label to validate data
+      }
+    }
+    private void initialFrequencyTextBox_Validating(object sender, CancelEventArgs e) {
+      double freq;
+      if (double.TryParse(initialFrequencyTextBox.Text, out freq) && freq >= 0.0) {
+        errorProvider.SetError(initialFrequencyTextBox, string.Empty);
+        e.Cancel = false;
+      } else {
+        errorProvider.SetError(initialFrequencyTextBox, "Invalid value");
+        e.Cancel = true;
+      }
+    }
+    private void initialFrequencyTextBox_Validated(object sender, EventArgs e) {
       double freq;
       if (double.TryParse(initialFrequencyTextBox.Text, out freq) && freq >= 0.0) {
         Content.InitialFrequency = freq;
-        errorProvider.SetError(initialFrequencyTextBox, string.Empty);
-      } else {
-        errorProvider.SetError(initialFrequencyTextBox, "Invalid value");
-      }
+      } 
     }
     #endregion
 
