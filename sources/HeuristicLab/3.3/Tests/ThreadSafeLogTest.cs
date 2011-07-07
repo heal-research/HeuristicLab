64e17e2df5f6b3a513af3524f8ace4903e2dff71
Stefan Wagner <swagner@heuristiclab.com>
2011-07-07 22:16:44 +0000

    Added missing license headers (#1529)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@6545 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab/3.3/Tests/ThreadSafeLogTest.cs
+++ sources/HeuristicLab/3.3/Tests/ThreadSafeLogTest.cs
@@ -1,15 +1,29 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿#region License Information
+/* HeuristicLab
+ * Copyright (C) 2002-2011 Heuristic and Evolutionary Algorithms Laboratory (HEAL)
+ *
+ * This file is part of HeuristicLab.
+ *
+ * HeuristicLab is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HeuristicLab is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HeuristicLab. If not, see <http://www.gnu.org/licenses/>.
+ */
+#endregion
+
+using System;
 using System.Linq;
-using System.Reflection;
-using System.Text;
-using HeuristicLab.Persistence.Default.CompositeSerializers.Storable;
-using HeuristicLab.PluginInfrastructure;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using HeuristicLab.Core;
 using System.Threading.Tasks;
-using System.Threading;
+using HeuristicLab.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HeuristicLab_33.Tests {
   [TestClass]
