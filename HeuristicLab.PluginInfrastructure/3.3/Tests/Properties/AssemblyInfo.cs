94d9b487d00c396d3dc06333ca40a2a2e2c15e47
Stefan Wagner <swagner@heuristiclab.com>
2011-07-08 00:59:18 +0000

    Incremented plugin version and assembly file versions (#1572)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk/sources@6548 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- HeuristicLab.PluginInfrastructure/3.3/Tests/Properties/AssemblyInfo.cs
+++ HeuristicLab.PluginInfrastructure/3.3/Tests/Properties/AssemblyInfo.cs
@@ -20,7 +20,6 @@
 #endregion
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -52,5 +51,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.3.4.0")]
-[assembly: AssemblyFileVersion("3.3.4.0")]
+[assembly: AssemblyVersion("3.3.0.0")]
+[assembly: AssemblyFileVersion("3.3.5.0")]
