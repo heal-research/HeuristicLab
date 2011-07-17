1b5828d21871ac414cd7488be97303f7c609758d
Andreas Beham <andreas.beham@heuristiclab.com>
2011-07-17 22:18:30 +0000

    #1586  * fixed in trunk
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@6571 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Problems.QuadraticAssignment/3.3/QuadraticAssignmentProblem.cs
+++ sources/HeuristicLab.Problems.QuadraticAssignment/3.3/QuadraticAssignmentProblem.cs
@@ -156,7 +156,7 @@ namespace HeuristicLab.Problems.QuadraticAssignment {
       } else if (Parameters["BestKnownSolutions"].GetType().Equals(typeof(OptionalValueParameter<ItemList<Permutation>>))) {
         ItemList<Permutation> list = ((OptionalValueParameter<ItemList<Permutation>>)Parameters["BestKnownSolutions"]).Value;
         Parameters.Remove("BestKnownSolutions");
-        Parameters.Add(new OptionalValueParameter<ItemSet<Permutation>>("BestKnownSolutions", "The list of best known solutions which is updated whenever a new better solution is found or may be the optimal solution if it is known beforehand.", new ItemSet<Permutation>(list)));
+        Parameters.Add(new OptionalValueParameter<ItemSet<Permutation>>("BestKnownSolutions", "The list of best known solutions which is updated whenever a new better solution is found or may be the optimal solution if it is known beforehand.", (list != null ? new ItemSet<Permutation>(list) : null)));
       }
       if (Parameters.ContainsKey("DistanceMatrix")) {
         DoubleMatrix d = ((ValueParameter<DoubleMatrix>)Parameters["DistanceMatrix"]).Value;
