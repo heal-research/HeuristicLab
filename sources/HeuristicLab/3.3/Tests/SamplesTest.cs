84a9b72e939b555f0b88c240e0ca49d5b5357cbe
Stefan Vonolfen <stefan.vonolfen@gmail.com>
2011-07-28 08:00:13 +0000

    Adapted VRP unit test (#1561)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@6605 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab/3.3/Tests/SamplesTest.cs
+++ sources/HeuristicLab/3.3/Tests/SamplesTest.cs
@@ -120,8 +120,8 @@ namespace HeuristicLab_33.Tests {
       ga.SetSeedRandomly.Value = false;
       RunAlgorithm(ga);
       Assert.AreEqual(1828.9368669428336, GetDoubleResult(ga, "BestQuality"));
-      Assert.AreEqual(1829.6829520983711, GetDoubleResult(ga, "CurrentAverageQuality"));
-      Assert.AreEqual(1867.8378222145707, GetDoubleResult(ga, "CurrentWorstQuality"));
+      Assert.AreEqual(1831.6785302558208, GetDoubleResult(ga, "CurrentAverageQuality"));
+      Assert.AreEqual(1948.3864562948675, GetDoubleResult(ga, "CurrentWorstQuality"));
       Assert.AreEqual(99100, GetIntResult(ga, "EvaluatedSolutions"));
     }
 
