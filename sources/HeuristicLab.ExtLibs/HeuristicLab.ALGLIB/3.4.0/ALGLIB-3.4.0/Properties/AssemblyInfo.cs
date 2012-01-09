f432116580132289e5eeaa393ed0731b95dc42c2
Gabriel Kronberger <gabriel.kronberger@fh-hagenberg.at>
2012-01-09 10:42:42 +0000

    #1733 updated alglib sources to version 3.4.0 of alglib and updated project and plugin references
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@7295 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.ExtLibs/HeuristicLab.ALGLIB/3.4.0/ALGLIB-3.4.0/Properties/AssemblyInfo.cs
+++ sources/HeuristicLab.ExtLibs/HeuristicLab.ALGLIB/3.4.0/ALGLIB-3.4.0/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ALGLIB-3.1.0")]
-[assembly: AssemblyDescription("ALGLIB® - numerical analysis library, 1999-2010")]
+[assembly: AssemblyTitle("ALGLIB-3.4.0")]
+[assembly: AssemblyDescription("ALGLIB® - numerical analysis library, 1999-2012")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ALGLIB project")]
-[assembly: AssemblyProduct("ALGLIB-3.1.0")]
-[assembly: AssemblyCopyright("Copyright © Sergey Bochkanov (ALGLIB project) 1999-2010")]
+[assembly: AssemblyProduct("ALGLIB-3.4.0")]
+[assembly: AssemblyCopyright("Copyright © Sergey Bochkanov (ALGLIB project) 1999-2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.0.0")]
-[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyVersion("3.4.0.0")]
+[assembly: AssemblyFileVersion("3.4.0.0")]
