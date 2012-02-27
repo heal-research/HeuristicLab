2516c1fce8031208aa7e2c6fb2a3646375836e42
Andreas Beham <andreas.beham@heuristiclab.com>
2012-02-27 12:14:11 +0000

    #1780: imported branch
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@7526 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ branches/PreBuildEvent.cmd
@@ -0,0 +1,2 @@
+IF EXIST "%ProjectDir%\Properties\AssemblyInfo.cs.frame" SubWCRev "%ProjectDir%\" "%ProjectDir%\Properties\AssemblyInfo.cs.frame" "%ProjectDir%\Properties\AssemblyInfo.cs"
+IF EXIST "%ProjectDir%\Plugin.cs.frame" SubWCRev "%ProjectDir%\" "%ProjectDir%\Plugin.cs.frame" "%ProjectDir%\Plugin.cs"
