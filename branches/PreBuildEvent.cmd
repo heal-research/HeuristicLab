be1460ec296b501900227bb87f2b2ea7bc7123bd
Andreas Beham <andreas.beham@heuristiclab.com>
2012-02-27 12:18:15 +0000

    #1780: removed wrong folder
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@7528 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- branches/PreBuildEvent.cmd
+++
@@ -1,2 +0,0 @@
-IF EXIST "%ProjectDir%\Properties\AssemblyInfo.cs.frame" SubWCRev "%ProjectDir%\" "%ProjectDir%\Properties\AssemblyInfo.cs.frame" "%ProjectDir%\Properties\AssemblyInfo.cs"
-IF EXIST "%ProjectDir%\Plugin.cs.frame" SubWCRev "%ProjectDir%\" "%ProjectDir%\Plugin.cs.frame" "%ProjectDir%\Plugin.cs"
