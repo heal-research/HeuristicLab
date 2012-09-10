ed9be6497faf30ab3adadcdab732e3ba9d242105
Michael Kommenda <michael.kommenda@fh-ooe.at>
2012-09-10 13:23:25 +0000

    #1948: Removed outdated external libraries ALGLIB 3.4, ALGLIB 3.5, WinFormsUI-2.3.1, LibSVM-1.6.3.
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@8621 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.ExtLibs/HeuristicLab.WinFormsUI/2.3.1/WinFormsUI-2.3.1/Docking/DummyControl.cs
+++
@@ -1,9 +0,0 @@
-using System.Windows.Forms;
-
-namespace WeifenLuo.WinFormsUI.Docking {
-  internal class DummyControl : Control {
-    public DummyControl() {
-      SetStyle(ControlStyles.Selectable, false);
-    }
-  }
-}
