a86536ceec3542e100ac0395a515c4d73321beba
Andreas Scheibenpflug <scheibenpflug.a@gmail.com>
2012-09-14 08:21:36 +0000

    #1937 switch mainform type to Multiple Document as Docking doesn't work on Linux at the moment
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@8654 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/prepareProjectsForMono.sh
+++ sources/prepareProjectsForMono.sh
@@ -8,10 +8,18 @@ do
     mv tmp $filename
 done;
 
+
 # remove projects that do not build
 sed -e '/ProtocolBuffers-2.4.1.473/,+1d' -e '/ProtoGen-2.4.1.473/,+1d' -e '/HeuristicLab.ProtobufCS-2.4.1.473/,+1d' HeuristicLab.ExtLibs.sln > tmp
 mv tmp HeuristicLab.ExtLibs.sln
 
-
 sed -e '/HeuristicLab.Problems.ExternalEvaluation-3.3/,+1d' -e '/HeuristicLab.Problems.ExternalEvaluation.GP-3.4/,+1d' -e '/HeuristicLab.Problems.ExternalEvaluation.Views-3.3/,+1d' "HeuristicLab 3.3.sln" > tmp
 mv tmp "HeuristicLab 3.3.sln"
+
+
+# switch to MultiDocument MainForm type as Docking doesn't properly work on Linux
+sed "s/DockingMainForm/MultipleDocumentMainForm/g" HeuristicLab.Optimizer/3.3/Properties/Settings.settings > tmp
+mv tmp HeuristicLab.Optimizer/3.3/Properties/Settings.settings
+
+sed "s/DockingMainForm/MultipleDocumentMainForm/g" HeuristicLab.Optimizer/3.3/Properties/Settings.Designer.cs > tmp
+mv tmp HeuristicLab.Optimizer/3.3/Properties/Settings.Designer.cs
