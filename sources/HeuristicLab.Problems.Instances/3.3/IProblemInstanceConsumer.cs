7df7b6cc09b2c2b0b3be029c4efc123e4075a302
Michael Kommenda <michael.kommenda@fh-ooe.at>
2012-11-13 08:56:31 +0000

    #1981: Removed the contravariance of the generic parameter in the IProblemInstanceConsumer.
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@8904 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Problems.Instances/3.3/IProblemInstanceConsumer.cs
+++ sources/HeuristicLab.Problems.Instances/3.3/IProblemInstanceConsumer.cs
@@ -24,7 +24,7 @@ using HeuristicLab.Common;
 namespace HeuristicLab.Problems.Instances {
   public interface IProblemInstanceConsumer : IContent { }
 
-  public interface IProblemInstanceConsumer<in TData> : IProblemInstanceConsumer {
+  public interface IProblemInstanceConsumer<TData> : IProblemInstanceConsumer {
     /// <summary>
     /// Loads the given instance data.
     /// </summary>
