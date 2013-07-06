3cbe457e6d60a177c314b37a77cd98663baf9aeb
Andreas Scheibenpflug <scheibenpflug.a@gmail.com>
2013-07-06 12:57:56 +0000

    #2084 updated prepare script to work on MacOSX
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@9702 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/prepareProjectsForMono.sh
+++ sources/prepareProjectsForMono.sh
@@ -2,7 +2,7 @@
 ORIG="<Reference Include=\"System.Windows.Forms.DataVisualization\" \/>"
 REP="<Reference Include=\"System.Windows.Forms.DataVisualization\" > \n <HintPath>..\/..\/bin\/System.Windows.Forms.DataVisualization.dll<\/HintPath> \n <\/Reference>"
 
-for filename in $(find -name '*.csproj')
+for filename in $(find . -name '*.csproj')
 do    
     sed "s/$ORIG/$REP/g" $filename > tmp
     mv tmp $filename
@@ -10,11 +10,24 @@ done;
 
 
 # remove projects that do not build
-sed -e '/ProtocolBuffers-2.4.1.473/,+1d' -e '/ProtoGen-2.4.1.473/,+1d' -e '/HeuristicLab.ProtobufCS-2.4.1.473/,+1d' HeuristicLab.ExtLibs.sln > tmp
-mv tmp HeuristicLab.ExtLibs.sln
+unamestr=`uname`
+if [[ "$unamestr" == 'Darwin' ]]; then
+   
+   awk '/ProtocolBuffers-2.4.1.473|ProtoGen-2.4.1.473|HeuristicLab.ProtobufCS-2.4.1.473/ {while (/ProtocolBuffers-2.4.1.473|ProtoGen-2.4.1.473|HeuristicLab.ProtobufCS-2.4.1.473/ && getline>0) ; next} 1' HeuristicLab.ExtLibs.sln > tmp
+   mv tmp HeuristicLab.ExtLibs.sln
 
-sed -e '/HeuristicLab.Problems.ExternalEvaluation-3.3/,+1d' -e '/HeuristicLab.Problems.ExternalEvaluation.GP-3.4/,+1d' -e '/HeuristicLab.Problems.ExternalEvaluation.Views-3.3/,+1d' "HeuristicLab 3.3.sln" > tmp
-mv tmp "HeuristicLab 3.3.sln"
+   awk '/HeuristicLab.Problems.ExternalEvaluation-3.3|HeuristicLab.Problems.ExternalEvaluation.GP-3.4|HeuristicLab.Problems.ExternalEvaluation.Views-3.3/ {while (/HeuristicLab.Problems.ExternalEvaluation-3.3|HeuristicLab.Problems.ExternalEvaluation.GP-3.4|HeuristicLab.Problems.ExternalEvaluation.Views-3.3/ && getline>0) ; next} 1' "HeuristicLab 3.3.sln" > tmp
+   mv tmp "HeuristicLab 3.3.sln"
+
+elif [[ "$unamestr" == 'Linux' ]]; then
+   sed -e '/ProtocolBuffers-2.4.1.473/,+1d' -e '/ProtoGen-2.4.1.473/,+1d' -e '/HeuristicLab.ProtobufCS-2.4.1.473/,+1d' HeuristicLab.ExtLibs.sln > tmp
+   mv tmp HeuristicLab.ExtLibs.sln
+
+   sed -e '/HeuristicLab.Problems.ExternalEvaluation-3.3/,+1d' -e '/HeuristicLab.Problems.ExternalEvaluation.GP-3.4/,+1d' -e '/HeuristicLab.Problems.ExternalEvaluation.Views-3.3/,+1d' "HeuristicLab 3.3.sln" > tmp
+   mv tmp "HeuristicLab 3.3.sln"
+else 
+   echo "Unsupported operating system, compiling HeuristicLab may not work!"
+fi
 
 
 # switch to MultiDocument MainForm type as Docking doesn't properly work on Linux
