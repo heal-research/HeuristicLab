c9e45e245bfea154a5dfb73d37637b2179d6598f
Michael Kommenda <michael.kommenda@fh-ooe.at>
2013-07-17 11:35:44 +0000

    #2081: Integrated new HL path data types from the branch.
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@9714 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ sources/HeuristicLab.Data.Views/3.3/Path Views/DirectoryValueView.cs
@@ -0,0 +1,70 @@
+﻿#region License Information
+/* HeuristicLab
+ * Copyright (C) 2002-2013 Heuristic and Evolutionary Algorithms Laboratory (HEAL)
+ *
+ * This file is part of HeuristicLab.
+ *
+ * HeuristicLab is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HeuristicLab is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HeuristicLab. If not, see <http://www.gnu.org/licenses/>.
+ */
+#endregion
+
+using System;
+using System.Windows.Forms;
+using HeuristicLab.MainForm;
+
+namespace HeuristicLab.Data.Views {
+  [View("DirectoryValueView")]
+  [Content(typeof(DirectoryValue), true)]
+  public partial class DirectoryValueView : StringConvertibleValueView {
+    public new DirectoryValue Content {
+      get { return (DirectoryValue)base.Content; }
+      set { base.Content = value; }
+    }
+
+    public override bool ReadOnly {
+      get {
+        if ((Content != null) && Content.ReadOnly) return true;
+        return base.ReadOnly;
+      }
+      set { base.ReadOnly = value; }
+    }
+
+    public DirectoryValueView() {
+      InitializeComponent();
+    }
+
+    protected override void SetEnabledStateOfControls() {
+      base.SetEnabledStateOfControls();
+      valueTextBox.Enabled = !Locked && !ReadOnly && Content != null;
+      valueTextBox.ReadOnly = Locked || ReadOnly || Content == null;
+      openButton.Enabled = !Locked && !ReadOnly && Content != null;
+    }
+
+    protected override void OnContentChanged() {
+      base.OnContentChanged();
+      if (Content == null) {
+        valueTextBox.Text = string.Empty;
+        return;
+      }
+
+      valueTextBox.Text = Content.Value;
+      openButton.Enabled = !Content.ReadOnly;
+    }
+
+    protected virtual void openButton_Click(object sender, EventArgs e) {
+      if (folderBrowserDialog.ShowDialog(this) != DialogResult.OK) return;
+      Content.Value = folderBrowserDialog.SelectedPath;
+    }
+  }
+}
---
+++ sources/HeuristicLab.Data.Views/3.3/Path Views/FileValueView.Designer.cs
@@ -0,0 +1,112 @@
+﻿#region License Information
+/* HeuristicLab
+ * Copyright (C) 2002-2013 Heuristic and Evolutionary Algorithms Laboratory (HEAL)
+ *
+ * This file is part of HeuristicLab.
+ *
+ * HeuristicLab is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HeuristicLab is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HeuristicLab. If not, see <http://www.gnu.org/licenses/>.
+ */
+#endregion
+
+namespace HeuristicLab.Data.Views {
+  partial class FileValueView {
+    /// <summary> 
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Component Designer generated code
+
+    /// <summary> 
+    /// Required method for Designer support - do not modify 
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FileValueView));
+      this.openButton = new System.Windows.Forms.Button();
+      this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+      ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+      this.splitContainer.Panel1.SuspendLayout();
+      this.splitContainer.Panel2.SuspendLayout();
+      this.splitContainer.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // valueTextBox
+      // 
+      this.errorProvider.SetIconAlignment(this.valueTextBox, System.Windows.Forms.ErrorIconAlignment.MiddleLeft);
+      this.errorProvider.SetIconPadding(this.valueTextBox, 2);
+      this.valueTextBox.Location = new System.Drawing.Point(17, 2);
+      this.valueTextBox.Size = new System.Drawing.Size(116, 20);
+      // 
+      // valueLabel
+      // 
+      this.valueLabel.Location = new System.Drawing.Point(3, 5);
+      this.valueLabel.Size = new System.Drawing.Size(26, 13);
+      this.valueLabel.Text = "File:";
+      // 
+      // splitContainer
+      // 
+      // 
+      // splitContainer.Panel2
+      // 
+      this.splitContainer.Panel2.Controls.Add(this.openButton);
+      this.splitContainer.Size = new System.Drawing.Size(194, 24);
+      this.splitContainer.SplitterDistance = 27;
+      // 
+      // openButton
+      // 
+      this.openButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.openButton.Image = HeuristicLab.Common.Resources.VSImageLibrary.Open;
+      this.openButton.Location = new System.Drawing.Point(139, 0);
+      this.openButton.Name = "openButton";
+      this.openButton.Size = new System.Drawing.Size(24, 24);
+      this.openButton.TabIndex = 3;
+      this.openButton.UseVisualStyleBackColor = true;
+      this.openButton.Click += new System.EventHandler(this.openButton_Click);
+      // 
+      // FileValueView
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.Name = "FileValueView";
+      this.Size = new System.Drawing.Size(194, 24);
+      ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+      this.splitContainer.Panel1.ResumeLayout(false);
+      this.splitContainer.Panel1.PerformLayout();
+      this.splitContainer.Panel2.ResumeLayout(false);
+      this.splitContainer.Panel2.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+      this.splitContainer.ResumeLayout(false);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    protected System.Windows.Forms.Button openButton;
+    protected System.Windows.Forms.OpenFileDialog openFileDialog;
+  }
+}
