f503e02d203cc248a7afe3d4b7a22b8af358bfa4
Stefan Vonolfen <stefan.vonolfen@gmail.com>
2014-02-20 09:10:43 +0000

    Added comment (#2119)
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@10472 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Problems.VehicleRouting/3.4/Encodings/General/Manipulators/BiasedMultiVRPSolutionManipulator.cs
+++ sources/HeuristicLab.Problems.VehicleRouting/3.4/Encodings/General/Manipulators/BiasedMultiVRPSolutionManipulator.cs
@@ -132,7 +132,7 @@ namespace HeuristicLab.Problems.VehicleRouting.Encodings.General {
         }
       }
 
-      ////////////////
+      //////////////// code has to be duplicated since ActualProbabilitiesParameter.ActualValue are updated and used for operator selection
       IRandom random = RandomParameter.ActualValue;
       DoubleArray probabilities = ActualProbabilitiesParameter.ActualValue;
       if (probabilities.Length != Operators.Count) {
