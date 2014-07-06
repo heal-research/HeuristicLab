e8648dbff656e78a19a7cc7999aafa4650d274d7
Gabriel Kronberger <gabriel.kronberger@fh-hagenberg.at>
2014-07-06 13:46:32 +0000

    #2189 reviewed r11088 and tried to add comments to explain the code.
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@11096 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Algorithms.TabuSearch/3.3/TabuSearch.cs
+++ sources/HeuristicLab.Algorithms.TabuSearch/3.3/TabuSearch.cs
@@ -350,11 +350,21 @@ namespace HeuristicLab.Algorithms.TabuSearch {
       IMoveGenerator defaultMoveGenerator = Problem.Operators.OfType<IMoveGenerator>().FirstOrDefault();
       MoveGeneratorParameter.ValidValues.Clear();
       if (Problem != null) {
+        // only add move generators that also have a corresponding tabu-checker and tabu-maker
+        //
+        // 1. assumption: all tabu checkers and tabu makers also implement the IMoveOperator interface
+        // 2. assumption: all move generators also implement the IMoveOperator interface
+        // 3. assumption: if compatible all three operators implement the same (most specific) IMoveOperator interface 
         foreach (IMoveGenerator generator in Problem.Operators.OfType<IMoveGenerator>().OrderBy(x => x.Name)) {
+          // get all interfaces equal to or derived from IMoveOperator that this move generator implements 
           var moveTypes = generator.GetType().GetInterfaces().Where(x => typeof(IMoveOperator).IsAssignableFrom(x)).ToList();
+          // keep only the most specific interfaces (e.g. IPermutationTranslocationMoveOperator);
+          // by removing all interfaces for which a derived interface is also contained in the moveTypes set
           foreach (var type in moveTypes.ToList()) {
             if (moveTypes.Any(t => t != type && type.IsAssignableFrom(t))) moveTypes.Remove(type);
           }
+          // keep move generator only if there is a tabu checker and a tabu maker that is derived from 
+          // one of the most specific interfaces in moveTypes (e.g. IPermutationTranslocationMoveOperator)
           if (Problem.Operators.OfType<ITabuChecker>().Any(op => moveTypes.Any(m => m.IsInstanceOfType(op)))
             && Problem.Operators.OfType<ITabuMaker>().Any(op => moveTypes.Any(m => m.IsInstanceOfType(op))))
             MoveGeneratorParameter.ValidValues.Add(generator);
