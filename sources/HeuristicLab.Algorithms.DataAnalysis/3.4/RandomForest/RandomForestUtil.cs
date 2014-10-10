9d1923effb5addc17ec0d07c13d375f6f34a0753
Bogdan Burlacu <bogdan.burlacu@fh-hagenberg.at>
2014-10-10 11:58:19 +0000

    #2237: Made random forest parameters serializable (by deriving from `ParameterCollection`).
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@11443 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Algorithms.DataAnalysis/3.4/RandomForest/RandomForestUtil.cs
+++ sources/HeuristicLab.Algorithms.DataAnalysis/3.4/RandomForest/RandomForestUtil.cs
@@ -29,16 +29,63 @@ using System.Threading.Tasks;
 using HeuristicLab.Common;
 using HeuristicLab.Core;
 using HeuristicLab.Data;
+using HeuristicLab.Parameters;
+using HeuristicLab.Persistence.Default.CompositeSerializers.Storable;
 using HeuristicLab.Problems.DataAnalysis;
 using HeuristicLab.Random;
 
 namespace HeuristicLab.Algorithms.DataAnalysis {
-  public class RFParameter : ICloneable {
-    public double n; // number of trees
-    public double m;
-    public double r;
+  [Item("RFParameter", "A random forest parameter collection")]
+  [StorableClass]
+  public class RFParameter : ParameterCollection {
+    public RFParameter() {
+      base.Add(new FixedValueParameter<IntValue>("N", "The number of random forest trees", new IntValue(50)));
+      base.Add(new FixedValueParameter<DoubleValue>("M", "The ratio of features that will be used in the construction of individual trees (0<m<=1)", new DoubleValue(0.1)));
+      base.Add(new FixedValueParameter<DoubleValue>("R", "The ratio of the training set that will be used in the construction of individual trees (0<r<=1)", new DoubleValue(0.1)));
+    }
 
-    public object Clone() { return new RFParameter { n = this.n, m = this.m, r = this.r }; }
+    [StorableConstructor]
+    private RFParameter(bool deserializing)
+      : base(deserializing) {
+    }
+
+    private RFParameter(RFParameter original, Cloner cloner)
+      : base(original, cloner) {
+      this.N = original.N;
+      this.R = original.R;
+      this.M = original.M;
+    }
+
+    public override IDeepCloneable Clone(Cloner cloner) {
+      return new RFParameter(this, cloner);
+    }
+
+    private IFixedValueParameter<IntValue> NParameter {
+      get { return (IFixedValueParameter<IntValue>)base["N"]; }
+    }
+
+    private IFixedValueParameter<DoubleValue> RParameter {
+      get { return (IFixedValueParameter<DoubleValue>)base["R"]; }
+    }
+
+    private IFixedValueParameter<DoubleValue> MParameter {
+      get { return (IFixedValueParameter<DoubleValue>)base["M"]; }
+    }
+
+    public int N {
+      get { return NParameter.Value.Value; }
+      set { NParameter.Value.Value = value; }
+    }
+
+    public double R {
+      get { return RParameter.Value.Value; }
+      set { RParameter.Value.Value = value; }
+    }
+
+    public double M {
+      get { return MParameter.Value.Value; }
+      set { MParameter.Value.Value = value; }
+    }
   }
 
   public static class RandomForestUtil {
@@ -63,6 +110,7 @@ namespace HeuristicLab.Algorithms.DataAnalysis {
       }
       avgTestMse /= partitions.Length;
     }
+
     private static void CrossValidate(IClassificationProblemData problemData, Tuple<IEnumerable<int>, IEnumerable<int>>[] partitions, int nTrees, double r, double m, int seed, out double avgTestAccuracy) {
       avgTestAccuracy = 0;
       var ds = problemData.Dataset;
@@ -95,7 +143,7 @@ namespace HeuristicLab.Algorithms.DataAnalysis {
         var parameters = new RFParameter();
         for (int i = 0; i < setters.Count; ++i) { setters[i](parameters, parameterValues[i]); }
         double rmsError, outOfBagRmsError, avgRelError, outOfBagAvgRelError;
-        RandomForestModel.CreateRegressionModel(problemData, problemData.TrainingIndices, (int)parameters.n, parameters.r, parameters.m, seed, out rmsError, out outOfBagRmsError, out avgRelError, out outOfBagAvgRelError);
+        RandomForestModel.CreateRegressionModel(problemData, problemData.TrainingIndices, parameters.N, parameters.R, parameters.M, seed, out rmsError, out outOfBagRmsError, out avgRelError, out outOfBagAvgRelError);
 
         lock (locker) {
           if (bestOutOfBagRmsError > outOfBagRmsError) {
@@ -119,7 +167,7 @@ namespace HeuristicLab.Algorithms.DataAnalysis {
         var parameters = new RFParameter();
         for (int i = 0; i < setters.Count; ++i) { setters[i](parameters, parameterValues[i]); }
         double rmsError, outOfBagRmsError, avgRelError, outOfBagAvgRelError;
-        RandomForestModel.CreateClassificationModel(problemData, problemData.TrainingIndices, (int)parameters.n, parameters.r, parameters.m, seed,
+        RandomForestModel.CreateClassificationModel(problemData, problemData.TrainingIndices, parameters.N, parameters.R, parameters.M, seed,
                                                                 out rmsError, out outOfBagRmsError, out avgRelError, out outOfBagAvgRelError);
 
         lock (locker) {
@@ -134,7 +182,7 @@ namespace HeuristicLab.Algorithms.DataAnalysis {
 
     public static RFParameter GridSearch(IRegressionProblemData problemData, int numberOfFolds, bool shuffleFolds, Dictionary<string, IEnumerable<double>> parameterRanges, int seed = 12345, int maxDegreeOfParallelism = 1) {
       DoubleValue mse = new DoubleValue(Double.MaxValue);
-      RFParameter bestParameter = new RFParameter { n = 1, m = 0.1, r = 0.1 };
+      RFParameter bestParameter = new RFParameter();
 
       var setters = parameterRanges.Keys.Select(GenerateSetter).ToList();
       var partitions = GenerateRandomForestPartitions(problemData, numberOfFolds);
@@ -147,7 +195,7 @@ namespace HeuristicLab.Algorithms.DataAnalysis {
         for (int i = 0; i < setters.Count; ++i) {
           setters[i](parameters, parameterValues[i]);
         }
-        CrossValidate(problemData, partitions, (int)parameters.n, parameters.r, parameters.m, seed, out testMSE);
+        CrossValidate(problemData, partitions, parameters.N, parameters.R, parameters.M, seed, out testMSE);
 
         lock (locker) {
           if (testMSE < mse.Value) {
@@ -161,7 +209,7 @@ namespace HeuristicLab.Algorithms.DataAnalysis {
 
     public static RFParameter GridSearch(IClassificationProblemData problemData, int numberOfFolds, bool shuffleFolds, Dictionary<string, IEnumerable<double>> parameterRanges, int seed = 12345, int maxDegreeOfParallelism = 1) {
       DoubleValue accuracy = new DoubleValue(0);
-      RFParameter bestParameter = new RFParameter { n = 1, m = 0.1, r = 0.1 };
+      RFParameter bestParameter = new RFParameter();
 
       var setters = parameterRanges.Keys.Select(GenerateSetter).ToList();
       var crossProduct = parameterRanges.Values.CartesianProduct();
@@ -174,7 +222,7 @@ namespace HeuristicLab.Algorithms.DataAnalysis {
         for (int i = 0; i < setters.Count; ++i) {
           setters[i](parameters, parameterValues[i]);
         }
-        CrossValidate(problemData, partitions, (int)parameters.n, parameters.r, parameters.m, seed, out testAccuracy);
+        CrossValidate(problemData, partitions, parameters.N, parameters.R, parameters.M, seed, out testAccuracy);
 
         lock (locker) {
           if (testAccuracy > accuracy.Value) {
@@ -255,7 +303,7 @@ namespace HeuristicLab.Algorithms.DataAnalysis {
     private static Action<RFParameter, double> GenerateSetter(string field) {
       var targetExp = Expression.Parameter(typeof(RFParameter));
       var valueExp = Expression.Parameter(typeof(double));
-      var fieldExp = Expression.Field(targetExp, field);
+      var fieldExp = Expression.Property(targetExp, field);
       var assignExp = Expression.Assign(fieldExp, Expression.Convert(valueExp, fieldExp.Type));
       var setter = Expression.Lambda<Action<RFParameter, double>>(assignExp, targetExp, valueExp).Compile();
       return setter;
