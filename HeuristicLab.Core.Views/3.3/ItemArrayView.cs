c9fd9d460905201ad593d7dc5e527ce768ebb5e1
Andreas Beham <andreas.beham@heuristiclab.com>
2015-03-04 14:52:36 +0000

    #2340: merged r12078, r12125 to stable
    git-svn-id: https://src.heuristiclab.com/svn/core/stable@12127 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- HeuristicLab.Core.Views/3.3/ItemArrayView.cs
+++ HeuristicLab.Core.Views/3.3/ItemArrayView.cs
@@ -23,6 +23,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Text;
 using System.Windows.Forms;
 using HeuristicLab.Collections;
 using HeuristicLab.MainForm;
@@ -140,8 +141,7 @@ namespace HeuristicLab.Core.Views {
       if (typeSelectorDialog.ShowDialog(this) == DialogResult.OK) {
         try {
           return (T)typeSelectorDialog.TypeSelector.CreateInstanceOfSelectedType();
-        }
-        catch (Exception ex) {
+        } catch (Exception ex) {
           ErrorHandling.ShowErrorDialog(this, ex);
         }
       }
@@ -255,6 +255,14 @@ namespace HeuristicLab.Core.Views {
         if ((itemsListView.SelectedItems.Count > 0) && !Content.IsReadOnly && !ReadOnly) {
           foreach (ListViewItem item in itemsListView.SelectedItems)
             Content[item.Index] = null;
+        }
+      } else if (e.KeyData == (Keys.Control | Keys.C)) {
+        if (itemsListView.SelectedItems.Count > 0) {
+          var builder = new StringBuilder();
+          foreach (ListViewItem selected in itemsListView.SelectedItems) {
+            builder.AppendLine(selected.Text);
+          }
+          Clipboard.SetText(builder.ToString());
         }
       }
     }
