0ef32181870b939cf4de89b1820a10ee96af605b
Gabriel Kronberger <gabriel.kronberger@fh-hagenberg.at>
2015-08-25 12:25:24 +0000

    #2469: implemented override for Analyze(...) method in SymbolicExpressionTreeProblem
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@12904 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Encodings.SymbolicExpressionTreeEncoding/3.4/SymbolicExpressionTreeProblem.cs
+++ sources/HeuristicLab.Encodings.SymbolicExpressionTreeEncoding/3.4/SymbolicExpressionTreeProblem.cs
@@ -21,6 +21,7 @@
 
 #endregion
 
+using System;
 using System.Linq;
 using HeuristicLab.Common;
 using HeuristicLab.Core;
@@ -49,20 +50,22 @@ namespace HeuristicLab.Encodings.SymbolicExpressionTreeEncoding {
       return (Maximization && quality > bestQuality || !Maximization && quality < bestQuality);
     }
 
-    public abstract double Evaluate(ISymbolicExpressionTree vector, IRandom random);
+    public abstract double Evaluate(ISymbolicExpressionTree tree, IRandom random);
     public sealed override double Evaluate(Individual individual, IRandom random) {
       return Evaluate(individual.SymbolicExpressionTree(), random);
     }
 
-    public override void Analyze(Individual[] individuals, double[] qualities, ResultCollection results, IRandom random) {
-      base.Analyze(individuals, qualities, results, random);
-      var orderedIndividuals = individuals.Zip(qualities, (i, q) => new { Individual = i, Quality = q }).OrderBy(z => z.Quality);
-      var best = Maximization ? orderedIndividuals.Last().Individual : orderedIndividuals.First().Individual;
-
+    public virtual void Analyze(ISymbolicExpressionTree[] trees, double[] qualities, ResultCollection results, IRandom random) {
+      var bestQuality = Maximization ? qualities.Max() : qualities.Min();
+      var bestIdx = Array.IndexOf(qualities, bestQuality);
+      var best = trees[bestIdx];
       if (!results.ContainsKey("Best Solution")) {
         results.Add(new Result("Best Solution", typeof(ISymbolicExpressionTree)));
       }
-      results["Best Solution"].Value = (IItem)best.SymbolicExpressionTree().Clone();
+      results["Best Solution"].Value = (IItem)best.Clone();
+    }
+    public sealed override void Analyze(Individual[] individuals, double[] qualities, ResultCollection results, IRandom random) {
+      Analyze(individuals.Select(ind => ind.SymbolicExpressionTree()).ToArray(), qualities, results, random);
     }
   }
 }
