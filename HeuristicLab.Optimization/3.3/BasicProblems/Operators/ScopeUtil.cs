31d2f901700678026f33cfc96fc3d4508ec577cb
Michael Kommenda <michael.kommenda@fh-ooe.at>
2015-11-23 14:17:12 +0000

    #2521: Added new class for problem refactoring.
    git-svn-id: https://src.heuristiclab.com/svn/core/branches/ProblemRefactoring@13337 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ HeuristicLab.Optimization/3.3/BasicProblems/Operators/ScopeUtil.cs
@@ -0,0 +1,49 @@
+﻿#region License Information
+
+/* HeuristicLab
+ * Copyright (C) 2002-2015 Heuristic and Evolutionary Algorithms Laboratory (HEAL)
+ *
+ * This file is part of HeuristicLab.
+ *
+ * HeuristicLab is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HeuristicLab is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HeuristicLab. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#endregion
+
+using System;
+using HeuristicLab.Core;
+
+namespace HeuristicLab.Optimization {
+  public static class ScopeUtil {
+
+    public static TSolution CopySolutionToScope<TSolution>(IScope scope, IEncoding<TSolution> encoding, TSolution solution)
+      where TSolution : class,ISolution {
+      var name = encoding.Name;
+      var copy = (TSolution)solution.Clone();
+      if (!scope.Variables.ContainsKey(name)) scope.Variables.Add(new Variable(name, copy));
+      else scope.Variables[name].Value = copy;
+      return copy;
+    }
+
+    public static TSolution GetSolution<TSolution>(IScope scope, IEncoding<TSolution> encoding)
+      where TSolution : class,ISolution {
+      var name = encoding.Name;
+      if (!scope.Variables.ContainsKey(name)) throw new ArgumentException(string.Format(" {0} cannot be found in the provided scope.", name));
+      var value = scope.Variables[name].Value as TSolution;
+      if (value == null) throw new InvalidOperationException(string.Format("Value of {0} is null.", name));
+      return value;
+    }
+
+  }
+}
