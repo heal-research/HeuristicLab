ee09870c7288aff2d56f137ab63548f1f547a07b
Michael Kommenda <michael.kommenda@fh-ooe.at>
2016-06-08 13:14:04 +0000

    #2557: Merged r13494, r13510 and r13676 into stable.
    git-svn-id: https://src.heuristiclab.com/svn/core/stable@13879 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- HeuristicLab.Tests/HeuristicLab-3.3/CollectObjectGraphTest.cs
+++ HeuristicLab.Tests/HeuristicLab-3.3/CollectObjectGraphTest.cs
@@ -43,6 +43,19 @@ namespace HeuristicLab.Tests {
     }
 
     [TestMethod]
+    [Description("Verify that the object graph traversal is working by checking the number of objects after traversal.")]
+    [TestCategory("General")]
+    [TestCategory("Essential")]
+    [TestProperty("Time", "medium")]
+    public void TestObjectGraphTraversal() {
+      GeneticAlgorithm ga = (GeneticAlgorithm)XmlParser.Deserialize(@"Test Resources\GA_SymbReg.hl");
+      var objects = ga.GetObjectGraphObjects().ToList();
+
+      // Should be 3982, but count may change slightly as members are added or removed
+      Assert.IsTrue(objects.Count > 1, "Number of objects in the object graph seems to small.");
+    }
+
+    [TestMethod]
     [TestCategory("General")]
     [TestCategory("Essential")]
     [TestProperty("Time", "medium")]
