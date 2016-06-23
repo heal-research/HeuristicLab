0ce36231561acce90706349360ee799a891cf35f
Michael Kommenda <michael.kommenda@fh-ooe.at>
2016-06-23 10:13:49 +0000

    #2616: Added checks in percentile calculation when no valid values are present.
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@13934 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.DataPreprocessing/3.4/Logic/StatisticsLogic.cs
+++ sources/HeuristicLab.DataPreprocessing/3.4/Logic/StatisticsLogic.cs
@@ -164,9 +164,9 @@ namespace HeuristicLab.DataPreprocessing {
     public double GetOneQuarterPercentile(int columnIndex) {
       double percentile = double.NaN;
       if (preprocessingData.VariableHasType<double>(columnIndex)) {
-        percentile = GetValuesWithoutNaN<double>(columnIndex).Quantile(0.25);
+        percentile = GetValuesWithoutNaN<double>(columnIndex).DefaultIfEmpty(double.NaN).Quantile(0.25);
       } else if (preprocessingData.VariableHasType<DateTime>(columnIndex)) {
-        percentile = GetDateTimeAsSeconds(columnIndex).Quantile(0.25);
+        percentile = GetDateTimeAsSeconds(columnIndex).DefaultIfEmpty(double.NaN).Quantile(0.25);
       }
       return percentile;
     }
@@ -174,9 +174,9 @@ namespace HeuristicLab.DataPreprocessing {
     public double GetThreeQuarterPercentile(int columnIndex) {
       double percentile = double.NaN;
       if (preprocessingData.VariableHasType<double>(columnIndex)) {
-        percentile = GetValuesWithoutNaN<double>(columnIndex).Quantile(0.75);
+        percentile = GetValuesWithoutNaN<double>(columnIndex).DefaultIfEmpty(double.NaN).Quantile(0.75);
       } else if (preprocessingData.VariableHasType<DateTime>(columnIndex)) {
-        percentile = GetDateTimeAsSeconds(columnIndex).Quantile(0.75);
+        percentile = GetDateTimeAsSeconds(columnIndex).DefaultIfEmpty(double.NaN).Quantile(0.75);
       }
       return percentile;
     }
