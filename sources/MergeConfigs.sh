eacf4ab47c3414a4ee7dddc6d2f85274549ba532
Stefan Wagner <swagner@heuristiclab.com>
2016-07-23 21:05:15 +0000

    #2526: Corrected post build events and config merging
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@14207 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/MergeConfigs.sh
+++ sources/MergeConfigs.sh
@@ -3,7 +3,7 @@ if [ "${TargetDir}" == "" ]; then
 fi
 
 echo Recreating HeuristicLab 3.3.exe.config...
-cp $TargetDir/app.config "$TargetDir/HeuristicLab 3.3.exe.config"
+cp $SolutionDir/HeuristicLab/3.3/app.config "$TargetDir/HeuristicLab 3.3.exe.config"
 
 echo Merging...
 for f in $(ls $TargetDir/*.dll.config); do 
