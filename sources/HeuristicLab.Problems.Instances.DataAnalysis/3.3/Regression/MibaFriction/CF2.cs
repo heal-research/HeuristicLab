a76493ea1122e18289c2ced3f632f8df194c92ba
Gabriel Kronberger <gabriel.kronberger@fh-hagenberg.at>
2017-04-12 08:19:05 +0000

    #2757: set factor variables as allowed for miba friction benchmark problems
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@14853 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Problems.Instances.DataAnalysis/3.3/Regression/MibaFriction/CF2.cs
+++ sources/HeuristicLab.Problems.Instances.DataAnalysis/3.3/Regression/MibaFriction/CF2.cs
@@ -37,11 +37,10 @@ namespace HeuristicLab.Problems.Instances.DataAnalysis {
       }
     }
 
-    // TODO: include string variables after factor branch integration
     protected override string[] AllowedInputVariables {
-      get { return new string[] { /* "Material_Cat", */
-        "Source1", "Source2", "x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10", "x11", "x12", "x13", "x14", "x15", "x16",
-        /* "Material", "Grooving", "Oil", */                                           
+      get { return new string[] { "Material_Cat",
+        "Source1", "x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10", "x11", "x12", "x13", "x14", "x15", "x16",
+        "Material", "Grooving", "Oil",
         "x17", "x18", "x19", "x20", "x21", "x22" }; }
     }
     protected override int TrainingPartitionStart { get { return 0; } }
