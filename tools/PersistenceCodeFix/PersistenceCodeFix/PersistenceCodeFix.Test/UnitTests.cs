f177d290ac95d5010058024c70ec933a598a34eb
Johannes Karder <johannes.karder@fh-hagenberg.at>
2017-05-16 14:56:40 +0000

    #2520: worked on persistence code fix   * added missing `UnitTests.cs`
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@14990 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ tools/PersistenceCodeFix/PersistenceCodeFix/PersistenceCodeFix.Test/UnitTests.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+
+namespace PersistenceCodeFix.Test {
+  [TestClass]
+  public class UnitTest : CodeFixVerifier {
+    [TestMethod]
+    public void TestMethod1() {
+      var test = @"";
+
+      VerifyCSharpDiagnostic(test);
+    }
+
+    [TestMethod]
+    public void TestMethod2() {
+      var test = @"
+namespace N {
+    class StorableTypeAttribute : System.Attribute
+    {
+        public StorableTypeAttribute(string s)
+        {
+        }
+    }
+    class StorableConstructorAttribute : System.Attribute
+    {
+    }
+
+    [StorableType(""3BD39A19-27AA-4CA6-A363-84A877648DC2"")]
+    class A
+    {
+        [StorableConstructor]
+        protected A(bool deserializing)
+        {
+        }
+    }
+
+    [StorableType(""254FCBA9-1E7F-4948-8140-52F998C47ADC"")]
+    class B : A
+    {
+    }
+}";
+
+
+      var fixtest = @"
+namespace N {
+    class StorableTypeAttribute : System.Attribute
+    {
+        public StorableTypeAttribute(string s)
+        {
+        }
+    }
+    class StorableConstructorAttribute : System.Attribute
+    {
+    }
+
+    [StorableType(""3BD39A19-27AA-4CA6-A363-84A877648DC2"")]
+    class A
+    {
+        [StorableConstructor]
+        protected A(bool deserializing)
+        {
+        }
+    }
+
+    [StorableType(""254FCBA9-1E7F-4948-8140-52F998C47ADC"")]
+    class B : A
+    {
+        [StorableConstructor]
+        protected B(bool deserializing) : base(deserializing)
+        {
+        }
+    }
+}";
+      VerifyCSharpFix(test, fixtest);
+    }
+
+    protected override CodeFixProvider GetCSharpCodeFixProvider() {
+      return new MissingStorableConstructorFix();
+    }
+
+    protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() {
+      return new MissingStorableConstructorAnalyzer();
+    }
+  }
+}
