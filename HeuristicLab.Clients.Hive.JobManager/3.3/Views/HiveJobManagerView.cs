a2746a163152e40ac2c916f29b97566f9ca9401f
Johannes Karder <johannes.karder@fh-hagenberg.at>
2017-12-06 11:33:15 +0000

    #2846: merged r15419 and r15478 into stable
    git-svn-id: https://src.heuristiclab.com/svn/core/stable@15495 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- HeuristicLab.Clients.Hive.JobManager/3.3/Views/HiveJobManagerView.cs
+++ HeuristicLab.Clients.Hive.JobManager/3.3/Views/HiveJobManagerView.cs
@@ -60,14 +60,17 @@ namespace HeuristicLab.Clients.Hive.JobManager.Views {
       base.DeregisterContentEvents();
     }
 
-    protected override void OnContentChanged() {
+    protected async override void OnContentChanged() {
       base.OnContentChanged();
       if (Content == null) {
         hiveExperimentListView.Content = null;
       } else {
         hiveExperimentListView.Content = Content.Jobs;
-        if (Content != null)
-          Content.RefreshAsync(new Action<Exception>((Exception ex) => HandleServiceException(ex)));
+        try {
+          await System.Threading.Tasks.Task.Run(() => Content.Refresh());
+        } catch (Exception ex) {
+          HandleServiceException(ex);
+        }
       }
     }
 
@@ -97,8 +100,12 @@ namespace HeuristicLab.Clients.Hive.JobManager.Views {
       }
     }
 
-    private void refreshButton_Click(object sender, EventArgs e) {
-      Content.RefreshAsync(new Action<Exception>((Exception ex) => HandleServiceException(ex)));
+    private async void refreshButton_Click(object sender, EventArgs e) {
+      try {
+        await System.Threading.Tasks.Task.Run(() => Content.Refresh());
+      } catch (Exception ex) {
+        HandleServiceException(ex);
+      }
     }
 
     private void HandleServiceException(Exception ex) {
