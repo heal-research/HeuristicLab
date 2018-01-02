1ed5b53a1aa72239f94ac5291ae4784185fdbd4e
Michael Kommenda <michael.kommenda@fh-ooe.at>
2018-01-02 16:45:15 +0000

    #2850: Merged r15532, r15545, r15548, r15551, r15560, r15570 into stable.
    git-svn-id: https://src.heuristiclab.com/svn/core/stable@15571 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- HeuristicLab.Algorithms.DataAnalysis/3.4/GaussianProcess/GaussianProcessRegression.cs
+++ HeuristicLab.Algorithms.DataAnalysis/3.4/GaussianProcess/GaussianProcessRegression.cs
@@ -38,7 +38,7 @@ namespace HeuristicLab.Algorithms.DataAnalysis {
   [Item("Gaussian Process Regression", "Gaussian process regression data analysis algorithm.")]
   [Creatable(CreatableAttribute.Categories.DataAnalysisRegression, Priority = 160)]
   [StorableClass]
-  public sealed class GaussianProcessRegression : GaussianProcessBase, IStorableContent {
+  public sealed class GaussianProcessRegression : GaussianProcessBase, IStorableContent, IDataAnalysisAlgorithm<IRegressionProblem> {
     public string Filename { get; set; }
 
     public override Type ProblemType { get { return typeof(IRegressionProblem); } }
