7c343657f275e646db5fa9a0f111328a03e316c2
Florian Holzinger <florian.holzinger@fh-hagenberg.at>
2018-01-30 12:14:25 +0000

    #2880: Content-check prefixed in method
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@15672 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- sources/HeuristicLab.Optimization.Views/3.3/RunCollectionViews/RunCollectionBoxPlotView.cs
+++ sources/HeuristicLab.Optimization.Views/3.3/RunCollectionViews/RunCollectionBoxPlotView.cs
@@ -400,6 +400,9 @@ namespace HeuristicLab.Optimization.Views {
     }
 
     private void SetCustomAxisLabels(Axis axis, int dimension) {
+      if (Content == null) { return; }
+      if (!Content.Any()) { return; }
+
       axis.CustomLabels.Clear();
       if (categoricalMapping.ContainsKey(dimension)) {
         int position = 1;
@@ -416,7 +419,7 @@ namespace HeuristicLab.Optimization.Views {
           axis.CustomLabels.Add(label);
           position++;
         }
-      } else if (dimension > 0 && Content != null && Content.Count > 0 && Content.GetValue(0, dimension) is TimeSpanValue) {
+      } else if (dimension > 0 && Content.GetValue(0, dimension) is TimeSpanValue) {
         this.chart.ChartAreas[0].RecalculateAxesScale();
         Axis correspondingAxis = this.chart.ChartAreas[0].Axes.Where(x => x.Name == axis.Name).SingleOrDefault();
         if (correspondingAxis == null)
