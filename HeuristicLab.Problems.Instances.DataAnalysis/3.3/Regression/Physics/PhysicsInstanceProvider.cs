553e7c57ecb446ec18fd96dcd7b9014f1c6b5727
Gabriel Kronberger <gabriel.kronberger@fh-hagenberg.at>
2018-11-01 17:26:53 +0000

    #2957: initial import of physics problems implemented by lkammere
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@16264 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ HeuristicLab.Problems.Instances.DataAnalysis/3.3/Regression/Physics/PhysicsInstanceProvider.cs
@@ -0,0 +1,47 @@
+﻿#region License Information
+/* HeuristicLab
+ * Copyright (C) 2002-2018 Heuristic and Evolutionary Algorithms Laboratory (HEAL)
+ *
+ * This file is part of HeuristicLab.
+ *
+ * HeuristicLab is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HeuristicLab is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HeuristicLab. If not, see <http://www.gnu.org/licenses/>.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+
+namespace HeuristicLab.Problems.Instances.DataAnalysis {
+  class PhysicsInstanceProvider : ArtificialRegressionInstanceProvider {
+    public override IEnumerable<IDataDescriptor> GetDataDescriptors() {
+      return new List<IDataDescriptor>()
+      {
+         new RocketFuelFlow(123),
+         new AircraftLift(456),
+         new FluidDynamics(789),
+         new AircraftMaximumLift(321)
+      };
+    }
+
+    public override string Name { get { return "Physics Benchmark Problems"; } }
+    public override string Description { get { return ""; } }
+    public override Uri WebLink { get { return new Uri(@"https://arxiv.org/abs/1706.02281"); } }
+    public override string ReferencePublication {
+      get {
+        return "Chen Chen, Changtong Luo, Zonglin Jiang, 2017 " +
+"\"A multilevel block search algorithm for fast modeling generalized separable systems.\" arXiv preprint arXiv:1706.02281, v3";
+      }
+    }
+  }
+}
