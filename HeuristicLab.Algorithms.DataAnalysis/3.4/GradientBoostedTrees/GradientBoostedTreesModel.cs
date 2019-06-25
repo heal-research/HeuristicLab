d519f1c9952eb5c8d142e4fc1045019d06b1534c
Michael Kommenda <michael.kommenda@fh-ooe.at>
2019-06-25 15:36:07 +0000

    #2883: Merged all changesets into trunk.
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@17030 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- HeuristicLab.Algorithms.DataAnalysis/3.4/GradientBoostedTrees/GradientBoostedTreesModel.cs
+++ HeuristicLab.Algorithms.DataAnalysis/3.4/GradientBoostedTrees/GradientBoostedTreesModel.cs
@@ -35,25 +35,22 @@ namespace HeuristicLab.Algorithms.DataAnalysis {
   public sealed class GradientBoostedTreesModel : RegressionModel, IGradientBoostedTreesModel {
     // BackwardsCompatibility3.4 for allowing deserialization & serialization of old models
     #region Backwards compatible code, remove with 3.5
-    private bool isCompatibilityLoaded = false; // only set to true if the model is deserialized from the old format, needed to make sure that information is serialized again if it was loaded from the old format
 
     [Storable(Name = "models")]
     private IList<IRegressionModel> __persistedModels {
       set {
-        this.isCompatibilityLoaded = true;
         this.models.Clear();
         foreach (var m in value) this.models.Add(m);
       }
-      get { if (this.isCompatibilityLoaded) return models; else return null; }
+      get { return models; }
     }
     [Storable(Name = "weights")]
     private IList<double> __persistedWeights {
       set {
-        this.isCompatibilityLoaded = true;
         this.weights.Clear();
         foreach (var w in value) this.weights.Add(w);
       }
-      get { if (this.isCompatibilityLoaded) return weights; else return null; }
+      get { return weights; }
     }
     #endregion
 
@@ -76,9 +73,8 @@ namespace HeuristicLab.Algorithms.DataAnalysis {
       : base(original, cloner) {
       this.weights = new List<double>(original.weights);
       this.models = new List<IRegressionModel>(original.models.Select(m => cloner.Clone(m)));
-      this.isCompatibilityLoaded = original.isCompatibilityLoaded;
     }
-    [Obsolete("The constructor of GBTModel should not be used directly anymore (use GBTModelSurrogate instead)")]
+
     internal GradientBoostedTreesModel(IEnumerable<IRegressionModel> models, IEnumerable<double> weights)
       : base(string.Empty, "Gradient boosted tree model", string.Empty) {
       this.models = new List<IRegressionModel>(models);
