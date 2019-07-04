541db8bb0645c0192a1b599ed5f8d3c9c0e02d7e
Michael Kommenda <michael.kommenda@fh-ooe.at>
2019-07-04 14:03:48 +0000

    #2958: Merged 16266, 16269, 16274, 16276, 16277, 16285, 16286, 16287, 16289, 16293, 16296, 16297, 16298, 16333, 16334  into stable.
    git-svn-id: https://src.heuristiclab.com/svn/core/stable@17071 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ HeuristicLab.ExtLibs/HeuristicLab.NativeInterpreter/0.1/NativeInterpreter-0.1/src/instruction.h
@@ -0,0 +1,45 @@
+#ifndef INSTRUCTION_H
+#define INSTRUCTION_H
+
+#include <cstdint>
+
+#include "vector_operations.h"
+
+enum OpCodes : uint8_t
+{
+    // same values as in OpCodes.cs
+    Add = 1,
+    Sub = 2,
+    Mul = 3,
+    Div = 4,
+    Sin = 5,
+    Cos = 6,
+    Tan = 7,
+    Log = 8,
+    Exp = 9,
+    Var = 18,
+    Const = 20,
+    Power = 22,
+    Root = 23,
+    Square = 28,
+    Sqrt = 29
+};
+
+struct instruction
+{
+    // from Instruction.cs
+    uint8_t opcode;
+    uint16_t narg;
+
+    int childIndex;
+
+    // from LinearInstruction.cs
+    double value;
+    double weight; // necessary for variables
+
+    // pointer to data
+    double *buf;
+    double *data;
+};
+
+#endif
