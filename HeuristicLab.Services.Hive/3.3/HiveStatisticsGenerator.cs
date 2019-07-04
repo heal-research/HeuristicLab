70ac2ea639738894bbf24a7aa02c085f1de8ef65
Michael Kommenda <michael.kommenda@fh-ooe.at>
2019-07-04 13:20:26 +0000

    #2839: Merged 16622, 16878 into stable.
    git-svn-id: https://src.heuristiclab.com/svn/core/stable@17067 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- HeuristicLab.Services.Hive/3.3/HiveStatisticsGenerator.cs
+++ HeuristicLab.Services.Hive/3.3/HiveStatisticsGenerator.cs
@@ -65,10 +65,10 @@ namespace HeuristicLab.Services.Hive {
           });
 
           pm.UseTransaction(() => {
-            UpdateFactTaskTable(pm);
             try {
-              pm.SubmitChanges();
+              UpdateFactTaskTable(pm);
               UpdateExistingDimJobs(pm);
+              FlagJobsForDeletion(pm);
               pm.SubmitChanges();
             }
             catch (DuplicateKeyException e) {
@@ -80,11 +80,6 @@ namespace HeuristicLab.Services.Hive {
             }
           });
         }
-
-        pm.UseTransaction(() => {
-          FlagJobsForDeletion(pm);
-          pm.SubmitChanges();
-        });
       }
     }
 
