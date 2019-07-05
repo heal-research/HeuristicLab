c596d66f51a527464d6126cefa8920f1477d05c5
Andreas Beham <andreas.beham@heuristiclab.com>
2019-07-05 13:15:48 +0000

    #2520: tested and simplified check
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@17088 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- HeuristicLab.Clients.Hive/3.3/Util/PersistenceUtil.cs
+++ HeuristicLab.Clients.Hive/3.3/Util/PersistenceUtil.cs
@@ -22,8 +22,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.IO.Compression;
-using Google.Protobuf;
 using HEAL.Attic;
 using HeuristicLab.Persistence.Default.Xml;
 
@@ -46,37 +44,11 @@ namespace HeuristicLab.Clients.Hive {
       try {
         return (T)ser.Deserialize(sjob);
       } catch (PersistenceException e) {
-        // Exceptions may arise because data in Hive was uploaded with former XML-based
-        // persistence or with Attic prior to 1.2.
         if (e.InnerException is InvalidDataException) {
-          // We assume the data was serialized with HEAL.Attic < 1.2 which did not use
-          // DeflateStream, but as of 1.2 uses DeflateStream by default
-          var compressedData = Compress(sjob);
-          try {
-            // retry with the job's data compressed
-            return (T)ser.Deserialize(compressedData);
-          } catch (PersistenceException e2) {
-            if (e2.InnerException is InvalidProtocolBufferException
-              || e2.InnerException is InvalidDataException) {
-              // retry deserialize original bytes with old persistence
-              return DeserializeWithXmlParser<T>(sjob);
-            } else throw;
-          }
-        }
-        if (e.InnerException is InvalidProtocolBufferException) {
           // We assume the data was not serialized with HEAL.Attic, but with the former
           // XML-based persistence
           return DeserializeWithXmlParser<T>(sjob);
         } else throw;
-      }
-    }
-
-    private static byte[] Compress(byte[] sjob) {
-      using (var memStream = new MemoryStream(sjob.Length)) {
-        using (var deflateStream = new DeflateStream(memStream, CompressionMode.Compress)) {
-          deflateStream.Write(sjob, 0, sjob.Length);
-        }
-        return memStream.ToArray();
       }
     }
 
