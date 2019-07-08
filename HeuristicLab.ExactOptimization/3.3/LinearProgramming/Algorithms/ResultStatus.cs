4cfa2fa1feaca131802bea34de413b43f1e1035c
Andreas Beham <andreas.beham@heuristiclab.com>
2019-07-08 13:52:20 +0000

    #2931: merged to stable (16910, 16939, 16944, 17095)
    git-svn-id: https://src.heuristiclab.com/svn/core/stable@17117 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ HeuristicLab.ExactOptimization/3.3/LinearProgramming/Algorithms/ResultStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace HeuristicLab.ExactOptimization.LinearProgramming {
+
+  public enum ResultStatus {
+    Optimal,        // optimal.
+    Feasible,       // feasible, or stopped by limit.
+    Infeasible,     // proven infeasible.
+    Unbounded,      // proven unbounded.
+    Abnormal,       // abnormal, i.e., error of some kind.
+    ModelInvalid,   // the model is trivially invalid (NaN coefficients, etc).
+    NotSolved = 6,  // not been solved yet.
+    OptimalWithinTolerance = int.MaxValue  // optimal within gap tolerance but objective != bound
+  }
+}
