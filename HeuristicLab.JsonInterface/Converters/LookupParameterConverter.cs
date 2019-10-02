6b13dad0edc3e4a395f9f50cf18124b5e84f387b
David Piringer <david.piringer@fh-hagenberg.at>
2019-10-02 10:44:44 +0000

    #3026 - added class for constants -> `Constants` - added new converter -> `ValueLookupParameterConverter` - fixed a bug with type extension `IsEqualTo` - fixed converter priorities
    git-svn-id: https://src.heuristiclab.com/svn/core/branches/3026_IntegrationIntoSymSpace@17287 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- HeuristicLab.JsonInterface/Converters/LookupParameterConverter.cs
+++ HeuristicLab.JsonInterface/Converters/LookupParameterConverter.cs
@@ -7,12 +7,14 @@ using HeuristicLab.Core;
 
 namespace HeuristicLab.JsonInterface {
   public class LookupParameterConverter : ParameterBaseConverter {
-    public override JsonItem ExtractData(IParameter value) {
-      return new JsonItem() { Name = value.Name };
-    }
+    public override JsonItem ExtractData(IParameter value) => 
+      new JsonItem() { 
+        Name = value.Name,
+        Default = value.Cast<ILookupParameter>().ActualName
+      };
 
-    public override void InjectData(IParameter parameter, JsonItem data) {
-      
-    }
+    public override void InjectData(IParameter parameter, JsonItem data) =>
+      parameter.Cast<ILookupParameter>().ActualName = data.Default.Cast<string>();
+    
   }
 }
---
+++ HeuristicLab.JsonInterface/Converters/ValueLookupParameterConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HeuristicLab.Core;
+
+namespace HeuristicLab.JsonInterface {
+  public class ValueLookupParameterConverter : ParameterBaseConverter {
+    public override JsonItem ExtractData(IParameter value) {
+      IValueLookupParameter param = value.Cast<IValueLookupParameter>();
+      return new JsonItem() {
+        Name = value.Name,
+        Default = param.ActualName,
+        Reference = param.Value != null ? JsonItemConverter.Extract(param.Value) : null
+      };
+    }
+
+    public override void InjectData(IParameter parameter, JsonItem data) {
+      IValueLookupParameter param = parameter.Cast<IValueLookupParameter>();
+      param.ActualName = CastValue<string>(data.Default);
+      if (param.Value != null && data.Reference != null)
+        JsonItemConverter.Inject(param.Value, data.Reference);
+    }
+  }
+}
