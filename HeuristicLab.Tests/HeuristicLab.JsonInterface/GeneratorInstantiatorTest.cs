fbb4074765178fd1a5c74c1259053677c3efc693
David Piringer <david.piringer@fh-hagenberg.at>
2019-10-08 12:16:31 +0000

    #3026 added unit tests for `HeuristicLab.JsonInterface`
    git-svn-id: https://src.heuristiclab.com/svn/core/branches/3026_IntegrationIntoSymSpace@17323 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ HeuristicLab.Tests/HeuristicLab.JsonInterface/GeneratorInstantiatorTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using HeuristicLab.Algorithms.GeneticAlgorithm;
+using HeuristicLab.Encodings.PermutationEncoding;
+using HeuristicLab.JsonInterface;
+using HeuristicLab.Operators;
+using HeuristicLab.Optimization;
+using HeuristicLab.Problems.TravelingSalesman;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HeuristicLab.JsonInterface.Tests {
+  [TestClass]
+  public class GeneratorInstantiatorTest {
+    private string templateFilePath = Directory.GetCurrentDirectory()+"\\Template.json";
+    private string configFilePath = Directory.GetCurrentDirectory() + "\\Config.json";
+
+    [TestInitialize()]
+    public void CreateTempFiles() {
+      GeneticAlgorithm alg = new GeneticAlgorithm();
+      TravelingSalesmanProblem tsp = new TravelingSalesmanProblem();
+      JCGenerator gen = new JCGenerator();
+      File.WriteAllText(templateFilePath, gen.GenerateTemplate(alg, tsp));
+      File.WriteAllText(configFilePath, "["+
+        "{\"Name\": \"Seed\",\"Default\": 55555,\"Path\": \"Genetic Algorithm (GA).Seed\"},"+
+        "{\"Name\": \"Crossover\", \"Path\": \"Genetic Algorithm (GA).Crossover\", \"Default\": \"MultiPermutationCrossover\"}," +
+        "{\"Name\": \"Elites\", \"Path\": \"Genetic Algorithm (GA).Elites\", \"Default\": 5,\"Range\":[-2147483648,2147483647]}" +
+        "]");
+    }
+    
+    [TestCleanup()]
+    public void ClearTempFiles() {
+      File.Delete(templateFilePath);
+      File.Delete(configFilePath);
+    }
+
+    [TestMethod]
+    public void TestInstantiator() {
+      JCInstantiator configurator = new JCInstantiator();
+      GeneticAlgorithm alg = (GeneticAlgorithm)configurator.Instantiate(templateFilePath, configFilePath);
+
+      Assert.AreEqual(55555, alg.Seed.Value);
+      Assert.IsTrue(alg.Crossover is MultiPermutationCrossover);
+      Assert.AreEqual(5, alg.Elites.Value);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentOutOfRangeException))]
+    public void TestRangeChangeWithConfig() {
+      File.WriteAllText(configFilePath, "[{\"Name\": \"MutationProbability\", \"Path\": \"Genetic Algorithm (GA).MutationProbability\", \"Default\": 2.0,\"Range\":[0.0,2.0]}]");
+      JCInstantiator configurator = new JCInstantiator();
+      GeneticAlgorithm alg = (GeneticAlgorithm)configurator.Instantiate(templateFilePath, configFilePath);
+    }
+  }
+}
