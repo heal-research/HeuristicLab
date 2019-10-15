db5936c4b777c42011d02fe14ef12aa968db09f3
David Piringer <david.piringer@fh-hagenberg.at>
2019-10-15 13:42:39 +0000

    #3026 - added new project: `HeuristicLab.JsonInterface.OptimizerIntegration` -> to generate/load .json templates in `HeuristicLab.Optimizer` - added new menu dropdown items (Import/Export) in `HeuristicLab.Optimizer` -> they are only active when they have items - added item for Import/Export menu items (Json-Template), logic in file `FileManager.cs` in project `HeuristicLab.JsonInterface.OptimizerIntegration` - repositioned `CloseMenuItem`, `CloseAllMenuItem` and added new seperator in menu `File` (to separate import/export from save/open and close/closeAll)
    git-svn-id: https://src.heuristiclab.com/svn/core/branches/3026_IntegrationIntoSymSpace@17331 2abd9481-f8db-48e9-bd25-06bc13291c1b

---
+++ HeuristicLab.Optimizer/3.3/MenuItems/ImportMenuItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HeuristicLab.Common;
+using HeuristicLab.MainForm;
+
+namespace HeuristicLab.Optimizer.MenuItems {
+  internal class ImportMenuItem : HeuristicLab.MainForm.WindowsForms.MenuItem, IOptimizerUserInterfaceItemProvider {
+    public override string Name {
+      get { return "&Import"; }
+    }
+    public override IEnumerable<string> Structure {
+      get { return new string[] { "&File" }; }
+    }
+    public override int Position {
+      get { return 1500; }
+    }
+    public override Keys ShortCutKeys {
+      get { return Keys.Control | Keys.Shift | Keys.S; }
+    }
+
+    protected override void OnToolStripItemSet(EventArgs e) =>
+      ToolStripItem.Enabled = IsUseable();
+    
+    protected override void OnActiveViewChanged(object sender, EventArgs e) =>
+      ToolStripItem.Enabled = IsUseable();
+
+    private bool IsUseable() => ToolStripItem.IsOnDropDown && 
+      ((ToolStripDropDownItem)ToolStripItem).DropDownItems.Count > 0;
+
+    public override void Execute() {}
+  }
+}
