8554a2b3223c797da2bf80c06c7eaea542d3609f
Michael Kommenda <michael.kommenda@fh-ooe.at>
2021-03-03 19:42:06 +0000

    #3109: Corrected handling of empty data points when determining the y-axis range.
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@17858 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- HeuristicLab.Problems.DataAnalysis.Views/3.4/Regression/RegressionSolutionLineChartViewBase.cs
+++ HeuristicLab.Problems.DataAnalysis.Views/3.4/Regression/RegressionSolutionLineChartViewBase.cs
@@ -118,7 +118,7 @@ namespace HeuristicLab.Problems.DataAnalysis.Views {
         axisY.Title = Content.ProblemData.TargetVariable;
         double min = double.MaxValue, max = double.MinValue;
         foreach (var point in chart.Series.SelectMany(x => x.Points)) {
-          if (!point.YValues.Any() || double.IsInfinity(point.YValues[0]) || double.IsNaN(point.YValues[0]))
+          if (point.IsEmpty || !point.YValues.Any() || double.IsInfinity(point.YValues[0]) || double.IsNaN(point.YValues[0]))
             continue;
           var y = point.YValues[0];
           if (y < min)
