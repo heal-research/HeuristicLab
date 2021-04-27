11f27c3390bf962fea8af1aa2c8dc3e6fa32578b
Christian Haider <christian.haider@fh-hagenberg.at>
2021-04-27 08:41:43 +0000

    #3073 Added constraints for ShapeConstraintsSampleTest and fixed values
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@17965 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- HeuristicLab.Tests/HeuristicLab-3.3/Samples/ShapeConstrainedRegressionSampleTest.cs
+++ HeuristicLab.Tests/HeuristicLab-3.3/Samples/ShapeConstrainedRegressionSampleTest.cs
@@ -18,11 +18,14 @@
  * along with HeuristicLab. If not, see <http://www.gnu.org/licenses/>.
  */
 #endregion
+
+using System;
 using System.IO;
 using System.Linq;
 using HEAL.Attic;
 using HeuristicLab.Algorithms.GeneticAlgorithm;
 using HeuristicLab.Encodings.SymbolicExpressionTreeEncoding;
+using HeuristicLab.Problems.DataAnalysis;
 using HeuristicLab.Problems.DataAnalysis.Symbolic.Regression;
 using HeuristicLab.Problems.Instances.DataAnalysis;
 using HeuristicLab.Selection;
@@ -35,14 +38,6 @@ namespace HeuristicLab.Tests {
     private static readonly ProtoBufSerializer serializer = new ProtoBufSerializer();
 
     [TestMethod]
-    [TestCategory("Samples.Create")]
-    [TestProperty("Time", "medium")]
-    public void CreateShapeConstrainedRegressionSampleTest() {
-      var ga = CreateShapeConstrainedRegressionSample();
-      string path = Path.Combine(SamplesUtils.SamplesDirectory, SampleFileName + SamplesUtils.SampleFileExtension);
-      serializer.Serialize(ga, path);
-    }
-    [TestMethod]
     [TestCategory("Samples.Execute")]
     [TestProperty("Time", "long")]
     public void RunShapeConstrainedRegressionSampleTest() {
@@ -50,9 +45,9 @@ namespace HeuristicLab.Tests {
       ga.SetSeedRandomly.Value = false;
       SamplesUtils.RunAlgorithm(ga);
 
-      Assert.AreEqual(0.0780908301423225, SamplesUtils.GetDoubleResult(ga, "BestQuality"), 1E-8);
-      Assert.AreEqual(0.381314019205066, SamplesUtils.GetDoubleResult(ga, "CurrentAverageQuality"), 1E-8);
-      Assert.AreEqual(1.0000000000000002, SamplesUtils.GetDoubleResult(ga, "CurrentWorstQuality"), 1E-8);
+      Assert.AreEqual(0.317642788600248, SamplesUtils.GetDoubleResult(ga, "BestQuality"), 1E-8);
+      Assert.AreEqual(40.9805778810063, SamplesUtils.GetDoubleResult(ga, "CurrentAverageQuality"), 1E-8);
+      Assert.AreEqual(3359.91748220025, SamplesUtils.GetDoubleResult(ga, "CurrentWorstQuality"), 1E-8);
       Assert.AreEqual(150200, SamplesUtils.GetIntResult(ga, "EvaluatedSolutions"));
     }
 
@@ -62,6 +57,21 @@ namespace HeuristicLab.Tests {
       var instance = provider.GetDataDescriptors().Where(x => x.Name.Contains("Radiated gravitational wave power: -32/5*G**4/c**5*(m1*m2)**2*(m1+m2)/r**5 | no noise")).Single();
       var problem = new ShapeConstrainedRegressionSingleObjectiveProblem();
       problem.Load(provider.LoadData(instance));
+      var problemData = (IShapeConstrainedRegressionProblemData)problem.ProblemData;
+      problemData.ShapeConstraints.Add(new ShapeConstraint(new Interval(double.NegativeInfinity, 0), 1.0));
+      problemData.ShapeConstraints.Add(new ShapeConstraint("G", 1, new Interval(double.NegativeInfinity, 0), 1.0));
+      problemData.ShapeConstraints.Add(new ShapeConstraint("c", 1, new Interval(0, double.PositiveInfinity), 1.0));
+      problemData.ShapeConstraints.Add(new ShapeConstraint("m1", 1, new Interval(double.NegativeInfinity, 0), 1.0));
+      problemData.ShapeConstraints.Add(new ShapeConstraint("m2", 1, new Interval(double.NegativeInfinity, 0), 1.0));
+      problemData.ShapeConstraints.Add(new ShapeConstraint("r", 1, new Interval(0, double.PositiveInfinity), 1.0));
+
+      problemData.VariableRanges.SetInterval("G", new Interval(1, 2));
+      problemData.VariableRanges.SetInterval("c", new Interval(1, 2));
+      problemData.VariableRanges.SetInterval("m1", new Interval(1, 5));
+      problemData.VariableRanges.SetInterval("m2", new Interval(1, 5));
+      problemData.VariableRanges.SetInterval("r", new Interval(1, 2));
+      problem.ProblemData = problemData;
+
 
       #region Algorithm Configuration
       alg.Name = "Genetic Programming - Shape constrained Regression";
