f2880b2e3774c3f9064d79741136264df55e2338
Gabriel Kronberger <gabriel.kronberger@fh-hagenberg.at>
2021-12-08 17:34:19 +0000

    #3073 actually use variable ranges specified in the constructor
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@18108 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- HeuristicLab.Problems.DataAnalysis/3.4/Implementation/Regression/ShapeConstrainedRegressionProblemData.cs
+++ HeuristicLab.Problems.DataAnalysis/3.4/Implementation/Regression/ShapeConstrainedRegressionProblemData.cs
@@ -144,7 +144,7 @@ namespace HeuristicLab.Problems.DataAnalysis {
     public ShapeConstrainedRegressionProblemData(IRegressionProblemData regressionProblemData)
       : this(regressionProblemData.Dataset, regressionProblemData.AllowedInputVariables, regressionProblemData.TargetVariable,
           regressionProblemData.TrainingPartition, regressionProblemData.TestPartition, regressionProblemData.Transformations,
-          (regressionProblemData is ShapeConstrainedRegressionProblemData) ? ((ShapeConstrainedRegressionProblemData)regressionProblemData).ShapeConstraints : null, 
+          (regressionProblemData is ShapeConstrainedRegressionProblemData scProblemData) ? scProblemData.ShapeConstraints : null, 
           regressionProblemData.VariableRanges) {
     }
 
@@ -158,7 +158,8 @@ namespace HeuristicLab.Problems.DataAnalysis {
       TestPartition.End = testPartition.End;
       if (sc == null) sc = new ShapeConstraints();
       Parameters.Add(new FixedValueParameter<ShapeConstraints>(ShapeConstraintsParameterName, "Specifies the shape constraints for the regression problem.", (ShapeConstraints)sc.Clone()));
-
+      foreach (var entry in variableRanges.GetVariableIntervals())
+        VariableRanges.SetInterval(entry.Item1, entry.Item2);
       RegisterEventHandlers();
     }
 
