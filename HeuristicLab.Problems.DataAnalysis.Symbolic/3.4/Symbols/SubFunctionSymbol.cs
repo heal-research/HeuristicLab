f35503626d2f4acc643302453c4f82657e988053
David Piringer <david.piringer@fh-hagenberg.at>
2021-12-16 09:10:21 +0000

    #3136 - changed the item name of `SubFunctionSymbol` from `SubFunctionSymbol` to `SubFunction`
    git-svn-id: https://src.heuristiclab.com/svn/core/branches/3136_Structural_GP@18139 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- HeuristicLab.Problems.DataAnalysis.Symbolic/3.4/Symbols/SubFunctionSymbol.cs
+++ HeuristicLab.Problems.DataAnalysis.Symbolic/3.4/Symbols/SubFunctionSymbol.cs
@@ -6,12 +6,12 @@ using HeuristicLab.Encodings.SymbolicExpressionTreeEncoding;
 
 namespace HeuristicLab.Problems.DataAnalysis.Symbolic {
   [StorableType("C6C2FBEE-8F4B-4FF2-80F5-D566BE55ED69")]
-  [Item("SubFunctionSymbol", "Symbol that represents a sub function.")]
+  [Item("SubFunction", "Symbol that represents a sub function.")]
   public class SubFunctionSymbol : Symbol {
     public override int MinimumArity => 0;
     public override int MaximumArity => 1;
 
-    public SubFunctionSymbol() : base("SubFunctionSymbol", "Symbol that represents a sub function.") { }
+    public SubFunctionSymbol() : base("SubFunction", "Symbol that represents a sub function.") { }
 
     private SubFunctionSymbol(SubFunctionSymbol original, Cloner cloner) : base(original, cloner) { }
 
