7c0ece24c25c44975330a0278c6757d48bde0af8
Gabriel Kronberger <gabriel.kronberger@fh-hagenberg.at>
2021-12-27 08:57:49 +0000

    #3145 changed unit tests to check new expected output (unnecessary parenthesis removed) --> unit tests fail
    git-svn-id: https://src.heuristiclab.com/svn/core/trunk@18170 2abd9481-f8db-48e9-bd25-06bc13291c1b

--- HeuristicLab.Tests/HeuristicLab.Problems.DataAnalysis.Symbolic-3.4/InfixExpressionParserTest.cs
+++ HeuristicLab.Tests/HeuristicLab.Problems.DataAnalysis.Symbolic-3.4/InfixExpressionParserTest.cs
@@ -34,8 +34,8 @@ namespace HeuristicLab.Problems.DataAnalysis.Symbolic.Tests {
       var formatter = new InfixExpressionFormatter();
       var parser = new InfixExpressionParser();
       Assert.AreEqual("3", formatter.Format(parser.Parse("3")));
-      Assert.AreEqual("(3 * 3)", formatter.Format(parser.Parse("3*3")));
-      Assert.AreEqual("(3 * 4)",formatter.Format(parser.Parse("3 * 4")));
+      Assert.AreEqual("3 * 3", formatter.Format(parser.Parse("3*3")));
+      Assert.AreEqual("3 * 4",formatter.Format(parser.Parse("3 * 4")));
       Assert.AreEqual("0.123",formatter.Format(parser.Parse("123E-03")));
       Assert.AreEqual("0.123",formatter.Format(parser.Parse("123e-03")));
       Assert.AreEqual("123000",formatter.Format(parser.Parse("123e+03")));
@@ -49,40 +49,40 @@ namespace HeuristicLab.Problems.DataAnalysis.Symbolic.Tests {
       Assert.AreEqual("123000",formatter.Format(parser.Parse("123.0e+3")));
       Assert.AreEqual("123000",formatter.Format(parser.Parse("123.0E+3")));
 
-      Assert.AreEqual("(3.1415 + 2)",formatter.Format(parser.Parse("3.1415+2.0")));
-      Assert.AreEqual("(3.1415 / 2)",formatter.Format(parser.Parse("3.1415/2.0")));
-      Assert.AreEqual("(3.1415 * 2)",formatter.Format(parser.Parse("3.1415*2.0")));
-      Assert.AreEqual("(3.1415 - 2)", formatter.Format(parser.Parse("3.1415-2.0")));
+      Assert.AreEqual("3.1415 + 2",formatter.Format(parser.Parse("3.1415+2.0")));
+      Assert.AreEqual("3.1415 / 2",formatter.Format(parser.Parse("3.1415/2.0")));
+      Assert.AreEqual("3.1415 * 2",formatter.Format(parser.Parse("3.1415*2.0")));
+      Assert.AreEqual("3.1415 - 2", formatter.Format(parser.Parse("3.1415-2.0")));
       // round-trip
-      Assert.AreEqual("(3.1415 - 2)",formatter.Format(parser.Parse(formatter.Format(parser.Parse("3.1415-2.0")))));
-      Assert.AreEqual("(3.1415 + 2)",formatter.Format(parser.Parse("3.1415+(2.0)")));
-      Assert.AreEqual("(3.1415 + 2)", formatter.Format(parser.Parse("(3.1415+(2.0))")));
+      Assert.AreEqual("3.1415 - 2",formatter.Format(parser.Parse(formatter.Format(parser.Parse("3.1415-2.0")))));
+      Assert.AreEqual("3.1415 + 2",formatter.Format(parser.Parse("3.1415+(2.0)")));
+      Assert.AreEqual("3.1415 + 2", formatter.Format(parser.Parse("(3.1415+(2.0))")));
 
 
       Assert.AreEqual("LOG(3)",formatter.Format(parser.Parse("log(3)")));
-      Assert.AreEqual("LOG((-3))",formatter.Format(parser.Parse("log(-3)")));
+      Assert.AreEqual("LOG(-3)",formatter.Format(parser.Parse("log(-3)")));
       Assert.AreEqual("EXP(3)",formatter.Format(parser.Parse("exp(3)")));
-      Assert.AreEqual("EXP((-3))",formatter.Format(parser.Parse("exp(-3)")));
+      Assert.AreEqual("EXP(-3)",formatter.Format(parser.Parse("exp(-3)")));
       Assert.AreEqual("SQRT(3)", formatter.Format(parser.Parse("sqrt(3)")));
 
-      Assert.AreEqual("SQR((-3))", formatter.Format(parser.Parse("sqr((-3))")));
+      Assert.AreEqual("SQR(-3)", formatter.Format(parser.Parse("sqr((-3))")));
 
-      Assert.AreEqual("((3 / 3) + (2 / 2) + (1 / 1))",formatter.Format(parser.Parse("3/3+2/2+1/1")));
-      Assert.AreEqual("((((((-3) + 30) - 2) + 20) - 1) + 10)", formatter.Format(parser.Parse("-3+30-2+20-1+10")));
+      Assert.AreEqual("3 / 3 + 2 / 2 + 1 / 1",formatter.Format(parser.Parse("3/3+2/2+1/1")));
+      Assert.AreEqual("-3 + 30 - 2 + 20 - 1 + 10", formatter.Format(parser.Parse("-3+30-2+20-1+10")));
 
       // 'flattening' of nested addition, subtraction, multiplication, or division
-      Assert.AreEqual("(1 + 2 + 3 + 4)", formatter.Format(parser.Parse("1 + 2 + 3 + 4")));
-      Assert.AreEqual("(1 - 2 - 3 - 4)", formatter.Format(parser.Parse("1 - 2 - 3 - 4")));
-      Assert.AreEqual("(1 * 2 * 3 * 4)", formatter.Format(parser.Parse("1 * 2 * 3 * 4")));
-      Assert.AreEqual("(1 / 2 / 3 / 4)", formatter.Format(parser.Parse("1 / 2 / 3 / 4")));
+      Assert.AreEqual("1 + 2 + 3 + 4", formatter.Format(parser.Parse("1 + 2 + 3 + 4")));
+      Assert.AreEqual("1 - 2 - 3 - 4", formatter.Format(parser.Parse("1 - 2 - 3 - 4")));
+      Assert.AreEqual("1 * 2 * 3 * 4", formatter.Format(parser.Parse("1 * 2 * 3 * 4")));
+      Assert.AreEqual("1 / 2 / 3 / 4", formatter.Format(parser.Parse("1 / 2 / 3 / 4")));
 
       // signed variables / constants
-      Assert.AreEqual("(-1*'x1')", formatter.Format(parser.Parse("-x1")));
+      Assert.AreEqual("-1*'x1'", formatter.Format(parser.Parse("-x1")));
       Assert.AreEqual("1", formatter.Format(parser.Parse("--1.0")));
       Assert.AreEqual("1", formatter.Format(parser.Parse("----1.0")));
       Assert.AreEqual("1", formatter.Format(parser.Parse("-+-1.0")));
       Assert.AreEqual("1", formatter.Format(parser.Parse("+-+-1.0")));
-      Assert.AreEqual("((-3) + (-1))", formatter.Format(parser.Parse("-3 + -1.0")));
+      Assert.AreEqual("-3 + -1", formatter.Format(parser.Parse("-3 + -1.0")));
 
 
       Assert.AreEqual("'x1'",formatter.Format(parser.Parse("\"x1\"")));
@@ -94,53 +94,53 @@ namespace HeuristicLab.Problems.DataAnalysis.Symbolic.Tests {
       Assert.AreEqual("\"var ' name\"", formatter.Format(parser.Parse("\"var \' name\"")));
 
 
-      Assert.AreEqual("('x1' * 'x2')",formatter.Format(parser.Parse("\"x1\"*\"x2\"")));
-      Assert.AreEqual("(('x1' * 'x2') + ('x3' * 'x4'))",formatter.Format(parser.Parse("\"x1\"*\"x2\"+\"x3\"*\"x4\"")));
-      Assert.AreEqual("(('x1' * 'x2') + ('x3' * 'x4'))", formatter.Format(parser.Parse("x1*x2+x3*x4")));
+      Assert.AreEqual("'x1' * 'x2'",formatter.Format(parser.Parse("\"x1\"*\"x2\"")));
+      Assert.AreEqual("'x1' * 'x2' + 'x3' * 'x4'",formatter.Format(parser.Parse("\"x1\"*\"x2\"+\"x3\"*\"x4\"")));
+      Assert.AreEqual("'x1' * 'x2' + 'x3' * 'x4'", formatter.Format(parser.Parse("x1*x2+x3*x4")));
 
 
-      Assert.AreEqual("(3 ^ 2)",formatter.Format(parser.Parse("POW(3, 2)")));
-      Assert.AreEqual("(3.1 ^ 2.1)",formatter.Format(parser.Parse("POW(3.1, 2.1)")));
-      Assert.AreEqual("(3.1 ^ 2.1)",formatter.Format(parser.Parse("POW(3.1 , 2.1)")));
-      Assert.AreEqual("(3.1 ^ 2.1)",formatter.Format(parser.Parse("POW(3.1 ,2.1)")));
-      Assert.AreEqual("((-3.1) ^ (-2.1))",formatter.Format(parser.Parse("POW(-3.1 , - 2.1)")));
+      Assert.AreEqual("3 ^ 2",formatter.Format(parser.Parse("POW(3, 2)")));
+      Assert.AreEqual("3.1 ^ 2.1",formatter.Format(parser.Parse("POW(3.1, 2.1)")));
+      Assert.AreEqual("3.1 ^ 2.1",formatter.Format(parser.Parse("POW(3.1 , 2.1)")));
+      Assert.AreEqual("3.1 ^ 2.1",formatter.Format(parser.Parse("POW(3.1 ,2.1)")));
+      Assert.AreEqual("-3.1 ^ -2.1",formatter.Format(parser.Parse("POW(-3.1 , - 2.1)")));
       Assert.AreEqual("ROOT(3, 2)",formatter.Format(parser.Parse("ROOT(3, 2)")));
       Assert.AreEqual("ROOT(3.1, 2.1)",formatter.Format(parser.Parse("ROOT(3.1, 2.1)")));
       Assert.AreEqual("ROOT(3.1, 2.1)",formatter.Format(parser.Parse("ROOT(3.1 , 2.1)")));
       Assert.AreEqual("ROOT(3.1, 2.1)",formatter.Format(parser.Parse("ROOT(3.1 ,2.1)")));
-      Assert.AreEqual("ROOT((-3.1), (-2.1))", formatter.Format(parser.Parse("ROOT(-3.1 , -2.1)")));
+      Assert.AreEqual("ROOT(-3.1, -2.1)", formatter.Format(parser.Parse("ROOT(-3.1 , -2.1)")));
 
       Assert.AreEqual("IF(GT(0, 1), 1, 0)",formatter.Format(parser.Parse("IF(GT( 0, 1), 1, 0)")));
       Assert.AreEqual("IF(LT(0, 1), 1, 0)",formatter.Format(parser.Parse("IF(LT(0,1), 1 , 0)")));
       Assert.AreEqual("LAG('x', 1)",formatter.Format(parser.Parse("LAG(x, 1)")));
       Assert.AreEqual("LAG('x', -1)",formatter.Format(parser.Parse("LAG(x, -1)")));
       Assert.AreEqual("LAG('x', 1)",formatter.Format(parser.Parse("LAG(x, +1)")));
-      Assert.AreEqual("('x' * LAG('x', 1))", formatter.Format(parser.Parse("x * LAG('x', +1)")));
+      Assert.AreEqual("'x' * LAG('x', 1)", formatter.Format(parser.Parse("x * LAG('x', +1)")));
 
       // factor variables
-      Assert.AreEqual("('x'[1] * 'y')",formatter.Format(parser.Parse("x [1.0] * y")));
-      Assert.AreEqual("('x'[1, 2] * 'y'[1, 2])",formatter.Format(parser.Parse("x [1.0, 2.0] * y [1.0, 2.0]")));
-      Assert.AreEqual("('x'[1] * 'y')",formatter.Format(parser.Parse("x[1] * y")));
-      Assert.AreEqual("('x'[1, 2] * 'y'[1, 2])",formatter.Format(parser.Parse("x[1, 2] * y [1, 2]")));
-      Assert.AreEqual("('x'[1] * 'y')",formatter.Format(parser.Parse("x [+1.0] * y")));
-      Assert.AreEqual("('x'[-1] * 'y')",formatter.Format(parser.Parse("x [-1.0] * y")));
-      Assert.AreEqual("('x'[-1, -2] * 'y'[1, 2])", formatter.Format(parser.Parse("x [-1.0, -2.0] * y [+1.0, +2.0]")));
+      Assert.AreEqual("'x'[1] * 'y'",formatter.Format(parser.Parse("x [1.0] * y")));
+      Assert.AreEqual("'x'[1, 2] * 'y'[1, 2]",formatter.Format(parser.Parse("x [1.0, 2.0] * y [1.0, 2.0]")));
+      Assert.AreEqual("'x'[1] * 'y'",formatter.Format(parser.Parse("x[1] * y")));
+      Assert.AreEqual("'x'[1, 2] * 'y'[1, 2]",formatter.Format(parser.Parse("x[1, 2] * y [1, 2]")));
+      Assert.AreEqual("'x'[1] * 'y'",formatter.Format(parser.Parse("x [+1.0] * y")));
+      Assert.AreEqual("'x'[-1] * 'y'",formatter.Format(parser.Parse("x [-1.0] * y")));
+      Assert.AreEqual("'x'[-1, -2] * 'y'[1, 2]", formatter.Format(parser.Parse("x [-1.0, -2.0] * y [+1.0, +2.0]")));
 
       // one-hot for factor
-      Assert.AreEqual("('x' = 'val' * 'y')",formatter.Format(parser.Parse("x='val' * y")));
+      Assert.AreEqual("'x' = 'val' * 'y'",formatter.Format(parser.Parse("x='val' * y")));
       Assert.AreEqual("'x' = 'val'",formatter.Format(parser.Parse("x = 'val'")));
       Assert.AreEqual("'x' = 'val'",formatter.Format(parser.Parse("x = \"val\"")));
-      Assert.AreEqual("(1 * 'x' = 'val')",formatter.Format(parser.Parse("1.0 * x = val")));
-      Assert.AreEqual("((-1) * 'x' = 'val')",formatter.Format(parser.Parse("-1.0 * x = val")));
-      Assert.AreEqual("((1 * 'x' = 'val1') + 'y' = 'val2')", formatter.Format(parser.Parse("+1.0 * \"x\" = val1 + y = \"val2\"")));
+      Assert.AreEqual("1 * 'x' = 'val'",formatter.Format(parser.Parse("1.0 * x = val")));
+      Assert.AreEqual("-1 * 'x' = 'val'",formatter.Format(parser.Parse("-1.0 * x = val")));
+      Assert.AreEqual("1 * 'x' = 'val1' + 'y' = 'val2'", formatter.Format(parser.Parse("+1.0 * \"x\" = val1 + y = \"val2\"")));
 
       // numeric parameters
       Assert.AreEqual("0", formatter.Format(parser.Parse("<num>"))); // default initial value is zero
       Assert.AreEqual("0", formatter.Format(parser.Parse("< num >")));
       Assert.AreEqual("1", formatter.Format(parser.Parse("< num=1.0>")));
       Assert.AreEqual("1", formatter.Format(parser.Parse("< num = 1.0>")));
-      Assert.AreEqual("(-1)", formatter.Format(parser.Parse("< num =-1.0>")));
-      Assert.AreEqual("(-1)", formatter.Format(parser.Parse("< num = - 1.0>")));
+      Assert.AreEqual("-1", formatter.Format(parser.Parse("< num =-1.0>")));
+      Assert.AreEqual("-1", formatter.Format(parser.Parse("< num = - 1.0>")));
       
       // numeric parameter with sign
       Assert.AreEqual("1", formatter.Format(parser.Parse("-<num=-1.0>")));
