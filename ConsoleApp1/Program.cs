3528d5628107631ebcbe124dca8f34f2625eb62b
David Piringer <david.piringer@fh-hagenberg.at>
2022-03-31 16:09:54 +0200

    #3026 Add test application

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using HeuristicLab.Algorithms.GeneticAlgorithm;
+using HeuristicLab.Problems.DataAnalysis.Symbolic.Regression;
+using HeuristicLab.SequentialEngine;
+using HeuristicLab.Problems.DataAnalysis;
+using HeuristicLab.Problems.DataAnalysis.Symbolic;
+using HeuristicLab.JsonInterface;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      GeneticAlgorithm ga = new GeneticAlgorithm();
+      ga.Engine = new SequentialEngine();
+      ga.Problem = new SymbolicRegressionSingleObjectiveProblem();
+
+      var converter = new JsonItemConverter();
+      JsonItem item = converter.ConvertToJson(ga);
+
+      JsonTemplateGenerator.GenerateTemplate(@"D:\test", ga);
+    }
+  }
+}
