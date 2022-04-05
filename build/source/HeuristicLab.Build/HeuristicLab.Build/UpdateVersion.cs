f2295f8dd233320adf49bff8584f6b35463acaca
fHolz <vanmodeus@gmail.com>
2022-04-05 22:01:54 +0200

    Added new .netstandard20 project for custom MSBuild-Task utilizing GitInfo

---
+++ build/source/HeuristicLab.Build/HeuristicLab.Build/UpdateVersion.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace HeuristicLab.Build {
+  public class UpdateVersion : Task {
+
+    [Required]
+    public string ProjectDir { get; set; }
+
+    public override bool Execute() {
+
+      string commitCount = ThisAssembly.Git.Commits;
+      Log.LogMessage(MessageImportance.High, $"Updating {Path.Combine(ProjectDir, "Properties", "AssemblyInfo.cs")}");
+      var assemblyInfoFrameContent = File.ReadAllText(Path.Combine(ProjectDir, "Properties", "AssemblyInfo.cs.frame"));
+      File.WriteAllText(Path.Combine(ProjectDir, "Properties", "AssemblyInfo.cs"), assemblyInfoFrameContent.Replace("$WCREV$", commitCount));
+
+      Log.LogMessage(MessageImportance.High, $"Updating {Path.Combine(ProjectDir, "Plugin.cs")}");
+      var pluginFrameContent = File.ReadAllText(Path.Combine(ProjectDir, "Plugin.cs.frame"));
+      File.WriteAllText(Path.Combine(ProjectDir, "Plugin.cs"), pluginFrameContent.Replace("$WCREV$", commitCount));
+
+      return true;
+    }
+  }
+}
