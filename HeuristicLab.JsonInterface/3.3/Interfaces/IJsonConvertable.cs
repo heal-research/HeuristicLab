53103dcd7a0d67e6786a0ef584bad6d4e15a6f29
David Piringer <david.piringer@fh-hagenberg.at>
2022-04-07 16:55:40 +0200

    Rename a inject parameter and fix documentation

--- HeuristicLab.JsonInterface/3.3/Interfaces/IJsonConvertable.cs
+++ HeuristicLab.JsonInterface/3.3/Interfaces/IJsonConvertable.cs
@@ -10,15 +10,16 @@ namespace HeuristicLab.JsonInterface {
     /// Injects the saved infos from the JsonItem into the IItem.
     /// (Sets the necessary values.)
     /// </summary>
-    /// <param name="item">The IItem which get the data injected.</param>
-    /// <param name="data">The JsonItem with the saved values.</param>
-    void Inject(JsonItem data, JsonItemConverter converter);
+    /// <param name="item">The JsonItem with the saved values.</param>
+    /// <param name="converter">The convorter which started the 
+    /// injection (necessary to prevent loops).</param>
+    void Inject(JsonItem item, JsonItemConverter converter);
 
     /// <summary>
     /// Extracts all infos out of an IItem to create a JsonItem. 
     /// (For template generation.)
     /// </summary>
-    /// <param name="value">The IItem to extract infos.</param>
+    /// <param name="converter">The converter which started the extraction (necessary to prevent loops).</param>
     /// <returns>JsonItem with infos to reinitialise the IItem.</returns>
     JsonItem Extract(JsonItemConverter converter);
   }
