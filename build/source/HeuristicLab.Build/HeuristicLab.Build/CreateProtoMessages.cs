2e3259655e48f7af977d4ae2e85e9b1ca0e3ea20
P41847 (Holzinger Florian) <florian.holzinger@fh-hagenberg.at>
2022-04-12 17:15:40 +0200

    Added new Task for protoc generation in build script (including required projects to be self-contained)

---
+++ build/source/HeuristicLab.Build/HeuristicLab.Build/CreateProtoMessages.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using Google.ProtocolBuffers.ProtoGen;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace HeuristicLab.Build {
+  public class CreateProtoMessages : Task {
+    [Required]
+    public string ProtosDir { get; set; }
+
+    public override bool Execute() {
+      if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) { return true; }
+
+      var protoFiles = Directory.EnumerateFiles(ProtosDir, "*.proto");
+      foreach (var protoFile in protoFiles) {
+        //ProtoGen --proto_path="%ProjectDir%\Protos" "%%i" --include_imports -output_directory="%ProjectDir%Protos"
+        ProgramPreprocess.Run($"--proto_path={ProtosDir}", protoFile, "--include_imports", $"-output_directory={ProtosDir}");
+      }
+      return true;
+    }
+  }
+}
