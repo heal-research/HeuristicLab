f5bab6cf4ec41f0a6a1331475370e66cbcdc8243
David Piringer <david.piringer@fh-hagenberg.at>
2022-04-20 17:56:08 +0200

    Remove unused ViewModels

--- HeuristicLab.JsonInterface.OptimizerIntegration/3.3/ViewModels/LookupJsonItemVM.cs
+++
@@ -1,7 +0,0 @@
-﻿//using System.Windows.Forms;
-
-//namespace HeuristicLab.JsonInterface.OptimizerIntegration {
-//  public class LookupJsonItemVM : JsonItemVMBase<LookupJsonItem> {
-//    public override UserControl Control => null;
-//  }
-//}
--- HeuristicLab.JsonInterface.OptimizerIntegration/3.3/ViewModels/RangeVM.cs
+++
@@ -1,7 +0,0 @@
-﻿//using System;
-//namespace HeuristicLab.JsonInterface.OptimizerIntegration {
-//  public abstract class RangeVM<T, JsonItemType> : RangedValueBaseVM<T, JsonItemType>
-//    where T : IComparable 
-//    where JsonItemType : RangeJsonItem<T>
-//  { }
-//}
