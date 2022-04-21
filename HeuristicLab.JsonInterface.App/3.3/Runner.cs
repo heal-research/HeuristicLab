3c2617d35a6922be1871040697b417d5fc1f3416
David Piringer <david.piringer@fh-hagenberg.at>
2022-04-21 16:09:37 +0200

    Refactor JsonInterface App project

--- HeuristicLab.JsonInterface.App/3.3/Runner.cs
+++ HeuristicLab.JsonInterface.App/3.3/Runner.cs
@@ -11,41 +11,44 @@ using Newtonsoft.Json.Linq;
 namespace HeuristicLab.JsonInterface.App {
   internal static class Runner {
     internal static void Run(string template, string config, string outputFile, TimeSpan outputInterval) {
-      //InstantiatorResult instantiatorResult = JsonTemplateInstantiator.Instantiate(template, config);
-      //IJsonConvertable convertable = instantiatorResult.Convertable;
+      var convertable = JsonTemplateInstantiator.Instantiate(template, config);
 
-      //convertable.Runs.Clear();
-      //if (convertable is EngineAlgorithm e)
-      //  e.Engine = new ParallelEngine.ParallelEngine();
+      if (convertable is EngineAlgorithm e)
+        e.Engine = new ParallelEngine.ParallelEngine();
 
-      //Task task = convertable.StartAsync();
-      //while (!task.IsCompleted) {
-      //  WriteResultsToFile(outputFile, convertable, instantiatorResult.RunCollectionModifiers);
-      //  Thread.Sleep(outputInterval);
-      //}
-
-      //WriteResultsToFile(outputFile, convertable, instantiatorResult.RunCollectionModifiers);
+      if (convertable is IOptimizer optimizer) {
+        optimizer.Runs.Clear();
+        var modifiers = optimizer.Runs.Modifiers;
+        Task task = optimizer.StartAsync();
+        while (!task.IsCompleted) {
+          WriteResultsToFile(outputFile, optimizer, modifiers);
+          Thread.Sleep(outputInterval);
+        }
+        WriteResultsToFile(outputFile, optimizer, modifiers);
+      } else {
+        throw new ArgumentException("The convertable is not an IOptimizer.");
+      }
     }
 
-    //private static void WriteResultsToFile(string file, IOptimizer optimizer, IEnumerable<IRunCollectionModifier> runCollectionModifiers) {
-    //  if (optimizer.Runs.Count > 0) 
-    //    File.WriteAllText(file, FetchResults(optimizer, runCollectionModifiers));
-    //}
+    private static void WriteResultsToFile(string file, IOptimizer optimizer, IEnumerable<IRunCollectionModifier> runCollectionModifiers) {
+      if (optimizer.Runs.Count > 0)
+        File.WriteAllText(file, FetchResults(optimizer, runCollectionModifiers));
+    }
 
-    //private static string FetchResults(IOptimizer optimizer, IEnumerable<IRunCollectionModifier> runCollectionModifiers) {
-    //  JArray arr = new JArray();
-      
-    //  foreach (var modifier in runCollectionModifiers)
-    //    modifier.Modify(optimizer.Runs.ToList());
-      
-    //  foreach (var run in optimizer.Runs) {
-    //    JObject obj = new JObject();
-    //    arr.Add(obj);
-    //    obj.Add("Run", JToken.FromObject(run.ToString()));
-    //    foreach (var result in run.Results)
-    //      obj.Add(result.Key, result.Value.ToString());
-    //  }
-    //  return SingleLineArrayJsonWriter.Serialize(arr);
-    //}
+    private static string FetchResults(IOptimizer optimizer, IEnumerable<IRunCollectionModifier> runCollectionModifiers) {
+      JArray arr = new JArray();
+
+      foreach (var modifier in runCollectionModifiers)
+        modifier.Modify(optimizer.Runs.ToList());
+
+      foreach (var run in optimizer.Runs) {
+        JObject obj = new JObject();
+        arr.Add(obj);
+        obj.Add("Run", JToken.FromObject(run.ToString()));
+        foreach (var result in run.Results)
+          obj.Add(result.Key, result.Value.ToString());
+      }
+      return SingleLineArrayJsonWriter.Serialize(arr);
+    }
   }
 }
