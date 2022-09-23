604a40788d88941881ae461470463b48bc3a8613
David Piringer <david.piringer.1996@gmail.com>
2022-09-23 12:40:46 +0200

    Add CLI project and headless solutions

---
+++ HeuristicLab.CommandLineInterface/3.3/Runner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HeuristicLab.PluginInfrastructure;
+
+namespace HeuristicLab.CommandLineInterface
+{
+  public class Runner
+  {
+    public static void Run(ICommandLineArgument[] args) {
+      Console.WriteLine("Hello World");
+      Console.WriteLine(args.Count());
+      foreach(var arg in args)
+        Console.WriteLine(arg.Value);
+    }
+  }
+}
