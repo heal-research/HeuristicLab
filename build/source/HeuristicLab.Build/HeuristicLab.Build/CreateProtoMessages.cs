cd40ca9144b3b0eeb26e9a70c15e8638da2afbd9
David Piringer <david.piringer@fh-hagenberg.at>
2022-09-27 15:50:49 +0200

    Add build task from vanmodeus

---
+++ build/source/HeuristicLab.Build/HeuristicLab.Build/CreateProtoMessages.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using Google.ProtocolBuffers.ProtoGen;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace HeuristicLab.Build {
+  public class CreateProtoMessages : Task {
+    [Required]
+    public string ProtosDir { get; set; }
+    [Required]
+    public string SolutionDir { get; set; }
+
+    public override bool Execute() {
+      if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) { return true; }
+
+      var protoFiles = Directory.EnumerateFiles(ProtosDir, "*.proto");
+      foreach (var protoFile in protoFiles) {
+        ProgramPreprocess.Run($"--proto_path={ProtosDir}", protoFile, "--include_imports", $"-output_directory={ProtosDir}", $"--protoc_dir={Path.Combine(SolutionDir, "build", "bin")}");
+      }
+      return true;
+    }
+  }
+}
