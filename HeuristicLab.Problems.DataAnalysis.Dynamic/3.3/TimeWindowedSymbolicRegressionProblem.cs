972c48a24da7a9120889fa49084fd86ae4e6b81e
Philipp Fleck <philipp.fleck@fh-hagenberg.at>
2023-08-28 09:57:44 +0200

    Added WIP version for dynamic sym reg problem.

---
+++ HeuristicLab.Problems.DataAnalysis.Dynamic/3.3/TimeWindowedSymbolicRegressionProblem.cs
@@ -0,0 +1,73 @@
+﻿#region License Information
+/* HeuristicLab
+ * Copyright (C) Heuristic and Evolutionary Algorithms Laboratory (HEAL)
+ *
+ * This file is part of HeuristicLab.
+ *
+ * HeuristicLab is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HeuristicLab is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HeuristicLab. If not, see <http://www.gnu.org/licenses/>.
+ */
+#endregion
+
+using HEAL.Attic;
+using HeuristicLab.Common;
+using HeuristicLab.Core;
+using HeuristicLab.Encodings.SymbolicExpressionTreeEncoding;
+using HeuristicLab.Optimization;
+
+namespace HeuristicLab.Problems.Dynamic {
+  [Item("Time Windowed Symbolic Regression Problem", "Applies a dynamic time window on a regression problem")]
+  [Creatable(CreatableAttribute.Categories.Problems, Priority = 210)]
+  [StorableType("B8C7B816-9332-4786-8C5A-187B2A4AAAC6")]
+  public class TimeWindowedSymbolicRegressionProblem : SingleObjectiveStatefulDynamicProblem<SymbolicExpressionTreeEncoding, SymbolicExpressionTree, TimeWindowedSymbolicRegressionProblemState> {
+
+    #region Propeties
+    public override bool Maximization => !Parameters.ContainsKey(StateParameterName) || (State?.Maximization??true);
+    #endregion
+
+    #region Constructors and Cloning
+    public TimeWindowedSymbolicRegressionProblem() {
+      InitialState = new TimeWindowedSymbolicRegressionProblemState();
+      Encoding = InitialState.Encoding;
+      SolutionCreator = InitialState.SolutionCreator;
+      RegisterProblemEventHandlers();
+    }
+
+    private void RegisterProblemEventHandlers() {
+      InitialState.SolutionCreatorChanged+= (a,b)=> SolutionCreator = InitialState.SolutionCreator;
+    }
+
+    [StorableConstructor]
+    protected TimeWindowedSymbolicRegressionProblem(StorableConstructorFlag _) : base(_) { }
+
+    
+    [StorableHook(HookType.AfterDeserialization)]
+    private void AfterDeserialization() {
+      RegisterProblemEventHandlers();
+    }
+    
+
+    protected TimeWindowedSymbolicRegressionProblem(TimeWindowedSymbolicRegressionProblem original, Cloner cloner) : base(original, cloner) { }
+
+    public override IDeepCloneable Clone(Cloner cloner) {
+      return new TimeWindowedSymbolicRegressionProblem(this, cloner);
+    }
+    #endregion
+
+    #region ProblemMethods
+    protected override double Evaluate(Individual individual, IRandom random, bool dummy) {
+      return State.Evaluate(individual, random);
+    }
+    #endregion
+  }
+}
---
+++ HeuristicLab.Problems.DataAnalysis.Dynamic/3.3/TimeWindowedSymbolicRegressionProblemState.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HEAL.Attic;
+using HeuristicLab.Common;
+using HeuristicLab.Core;
+using HeuristicLab.Data;
+using HeuristicLab.Encodings.SymbolicExpressionTreeEncoding;
+using HeuristicLab.Optimization;
+using HeuristicLab.Parameters;
+using HeuristicLab.Problems.DataAnalysis;
+using HeuristicLab.Problems.DataAnalysis.Symbolic.Regression;
+
+namespace HeuristicLab.Problems.Dynamic {
+  [StorableType("A1A271EC-8B81-452A-A9FB-7BDF223C18E4")]
+  public class TimeWindowedSymbolicRegressionProblemState 
+    : SingleObjectiveBasicProblem<SymbolicExpressionTreeEncoding>, IDynamicProblemState<TimeWindowedSymbolicRegressionProblemState>
+  {
+    public const string ProblemParameterName = "InnerProblem";
+    public const string WindowStartParameterName = "WindowStart";
+    public const string WindowSizeParameterName = "WindowSize";
+    public const string MoveSizeParameterName = "MoveSize";
+
+    public IFixedValueParameter<IntValue> WindowStartParameter => (IFixedValueParameter<IntValue>)Parameters[WindowStartParameterName];
+    public IFixedValueParameter<IntValue> WindowSizeParameter => (IFixedValueParameter<IntValue>)Parameters[WindowSizeParameterName];
+    public IFixedValueParameter<IntValue> MoveSizeParameter => (IFixedValueParameter<IntValue>)Parameters[MoveSizeParameterName];
+    public IValueParameter<SymbolicRegressionSingleObjectiveProblem> ProblemParameter => (IValueParameter<SymbolicRegressionSingleObjectiveProblem>)Parameters[ProblemParameterName];
+
+    public int WindowStart {
+      get {
+        return WindowStartParameter.Value.Value;
+      }
+      set {
+        WindowStartParameter.Value.Value = value;
+      }
+    }
+    public int WindowSize {
+      get {
+        return WindowSizeParameter.Value.Value;
+      }
+      set {
+        WindowSizeParameter.Value.Value = value;
+      }
+    }
+    public int MoveSize {
+      get {
+        return MoveSizeParameter.Value.Value;
+      }
+      set {
+        MoveSizeParameter.Value.Value = value;
+      }
+    }
+    public SymbolicRegressionSingleObjectiveProblem Problem => ProblemParameter.Value;
+    public IRegressionProblemData ProblemData {
+      get {
+        return Problem.ProblemData;
+      }
+      set {
+        Problem.ProblemData = value;
+      }
+    }
+
+    [StorableConstructor]
+    protected TimeWindowedSymbolicRegressionProblemState(StorableConstructorFlag _) : base(_) { }
+    protected TimeWindowedSymbolicRegressionProblemState(TimeWindowedSymbolicRegressionProblemState original, Cloner cloner) : base(original, cloner) { }
+
+    public TimeWindowedSymbolicRegressionProblemState() {
+      Parameters.Add(new ValueParameter<SymbolicRegressionSingleObjectiveProblem>(ProblemParameterName, new SymbolicRegressionSingleObjectiveProblem()));
+      Parameters.Add(new FixedValueParameter<IntValue>(WindowStartParameterName, new IntValue(0)));
+      Parameters.Add(new FixedValueParameter<IntValue>(WindowSizeParameterName, new IntValue(100)));
+      Parameters.Add(new FixedValueParameter<IntValue>(MoveSizeParameterName, new IntValue(1)));
+
+      ConfigureEncoding();
+    }
+
+    private void ConfigureEncoding() {
+      Encoding.TreeLength = Problem.MaximumSymbolicExpressionTreeLength.Value;
+      Encoding.TreeDepth = Problem.MaximumSymbolicExpressionTreeDepth.Value;
+      Encoding.Grammar = Problem.SymbolicExpressionTreeGrammar;
+      Encoding.SolutionCreator = Problem.SolutionCreator;
+      Encoding.ConfigureOperators(Problem.OperatorsParameter.Value.OfType<IOperator>());
+      Operators.AddRange(Problem.OperatorsParameter.Value.OfType<IOperator>());
+    }
+
+    public override IDeepCloneable Clone(Cloner cloner) {
+      return new TimeWindowedSymbolicRegressionProblemState(this, cloner);
+    }
+
+    public void Initialize(IRandom random) {
+      ConfigureEncoding();
+    }
+
+    public void Update(IRandom random, long version) {
+      WindowStart += MoveSize;
+    }
+
+    public void MergeFrom(TimeWindowedSymbolicRegressionProblemState original) {
+      // todo?
+    }
+
+    public override bool Maximization => !Parameters.ContainsKey(ProblemParameterName) || (Problem?.Maximization.Value??true);
+    public override double Evaluate(Individual individual, IRandom random) {
+      return Problem.Evaluator.Evaluate(
+        individual.SymbolicExpressionTree(Encoding.Name), 
+        ProblemData, 
+        GetWindow().ToArray(), 
+        Problem.SymbolicExpressionTreeInterpreter, 
+        Problem.ApplyLinearScaling.Value, 
+        Problem.EstimationLimits?.Lower ?? 0, Problem.EstimationLimits?.Upper ?? 1);
+    }
+
+    private IEnumerable<int> GetWindow() {
+      var min = ProblemData.TrainingPartition.Start;
+      var size = ProblemData.TrainingPartition.Size;
+      var windowEnd = WindowStart + WindowSize;
+      for (var i = WindowStart; i < windowEnd; i++) {
+        yield return i % size + min;
+      }
+
+    }
+  }
+}
